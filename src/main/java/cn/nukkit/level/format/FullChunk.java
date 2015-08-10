051af0f79076c896f40a4ca29668e0da2a3b43b6
MagicDroidX <1323471525@qq.com>
2015-08-10 11:38:58 +0800

    FullChunk

--- src/main/java/cn/nukkit/level/format/FullChunk.java
+++ src/main/java/cn/nukkit/level/format/FullChunk.java
@@ -1,5 +1,8 @@
 package cn.nukkit.level.format;
 
+import cn.nukkit.entity.Entity;
+import cn.nukkit.tile.Tile;
+
 /**
  * author: MagicDroidX
  * Nukkit Project
@@ -19,4 +22,135 @@ public abstract class FullChunk {
 
     public abstract void setProvider(LevelProvider provider);
 
+    public abstract int getFullBlock(int x, int y, int z);
+
+    public abstract void setBlock(int x, int y, int z);
+
+    public abstract void setBlock(int x, int y, int z, int blockId);
+
+    public abstract void setBlock(int x, int y, int z, int blockId, int meta);
+
+    public abstract int getBlockId(int x, int y, int z);
+
+    public abstract void setBlockId(int x, int y, int z, int id);
+
+    public abstract int getBlockData(int x, int y, int z);
+
+    public abstract void setBlockData(int x, int y, int z, int data);
+
+    public abstract int getBlockSkyLight(int x, int y, int z);
+
+    public abstract void setBlockSkyLight(int x, int y, int z, int level);
+
+    public abstract int getBlockLight(int x, int y, int z);
+
+    public abstract void setBlockLight(int x, int y, int z, int level);
+
+    public abstract int getHighestBlockAt(int x, int z);
+
+    public abstract int getHeightMap(int x, int z);
+
+    public abstract void setHeightMap(int x, int z, int value);
+
+    public abstract void recalculateHeightMap();
+
+    public abstract void populateSkyLight();
+
+    public abstract int getBiomeId(int x, int z);
+
+    public abstract void setBiomeId(int x, int z, int biomeId);
+
+    public abstract int[] getBiomeColor(int x, int z);
+
+    public abstract String[] getBlockIdColunm(int x, int z);
+
+    public abstract String[] getBlockDataColumn(int x, int z);
+
+    public abstract String[] getBlockSkyLightColunm(int x, int z);
+
+    public abstract String[] getBlockLightColunm(int x, int z);
+
+    public abstract void setBiomeColor(int x, int z, int R, int G, int B);
+
+    public abstract boolean isLightPopulated();
+
+    public abstract void setLightPopulated();
+
+    public abstract void setLightPopulated(boolean value);
+
+    public abstract boolean isPopulated();
+
+    public abstract void setPopulated();
+
+    public abstract void setPopulated(boolean value);
+
+    public abstract boolean isGenerated();
+
+    public abstract void setGenerated();
+
+    public abstract void setGenerated(boolean value);
+
+    public abstract void addEntity(Entity entity);
+
+    public abstract void removeEntity(Entity entity);
+
+    public abstract void addTile(Tile tile);
+
+    public abstract void removeTile(Tile tile);
+
+    public abstract Entity[] getEntities();
+
+    public abstract Tile[] getTiles();
+
+    public abstract Tile getTile(int x, int y, int z);
+
+    public abstract boolean isLoaded();
+
+    public abstract boolean load();
+
+    public abstract boolean load(boolean generate);
+
+    public abstract boolean unload();
+
+    public abstract boolean unload(boolean save);
+
+    public abstract boolean unload(boolean save, boolean safe);
+
+    public abstract void initChunk();
+
+    public abstract String[] getBiomeIdArray();
+
+    public abstract int[] getBiomeColorArray();
+
+    public abstract int[] getHeightMapArray();
+
+    public abstract String[] getBlockIdArray();
+
+    public abstract byte[] getBlockDataArray();
+
+    public abstract int[] getBlockSkyLightArray();
+
+    public abstract int[] getBlockLightArray();
+
+    public abstract void toBinary();
+
+    public abstract byte[] toFastBinary();
+
+    public abstract boolean hasChanged();
+
+    public abstract void setChanged();
+
+    public abstract void setChanged(boolean changed);
+
+    public abstract FullChunk fromBinary(byte[] data);
+
+    public abstract FullChunk fromBinary(byte[] data, LevelProvider provider);
+
+    public abstract FullChunk fromFastBinary(byte[] data);
+
+    public abstract FullChunk fromFastBinary(byte[] data, LevelProvider provider);
+
+    public abstract FullChunk getEmptyChunk(int chunkX, int chunkZ);
+
+    public abstract FullChunk getEmptyChunk(int chunkX, int chunkZ, LevelProvider provider);
 }
---
+++ src/main/java/cn/nukkit/tile/Tile.java
@@ -0,0 +1,10 @@
+package cn.nukkit.tile;
+
+import cn.nukkit.level.Position;
+
+/**
+ * author: MagicDroidX
+ * Nukkit Project
+ */
+public abstract class Tile extends Position {
+}
