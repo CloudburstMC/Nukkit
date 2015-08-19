249b3bf978c0ea1f2a72b3e8b7996b3933b6b290
Nevet <inevet@163.com>
2015-08-19 09:23:05 +0800

    Update PluginDescription.java

--- src/main/java/cn/nukkit/plugin/PluginDescription.java
+++ src/main/java/cn/nukkit/plugin/PluginDescription.java
@@ -4,4 +4,16 @@ package cn.nukkit.plugin;
  * Created by Nukkit Team.
  */
 public class PluginDescription {
+  
+  private String name;
+  private String main;
+  private String apiVersion;
+  private String[] depend;
+  private String[] softDepend;
+  private String version;
+  private String[] commands;
+  private String description;
+  private String[] authors;
+  private String website;
+  
 }
