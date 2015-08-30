79129415f997df4a49f693ae9694d7a913c0e055
MagicDroidX <1323471525@qq.com>
2015-08-30 15:15:16 +0800

    Revert "Update PluginDescription.java"
    This reverts commit 249b3bf978c0ea1f2a72b3e8b7996b3933b6b290.

--- src/main/java/cn/nukkit/plugin/PluginDescription.java
+++ src/main/java/cn/nukkit/plugin/PluginDescription.java
@@ -4,16 +4,4 @@ package cn.nukkit.plugin;
  * Created by Nukkit Team.
  */
 public class PluginDescription {
-  
-  private String name;
-  private String main;
-  private String apiVersion;
-  private String[] depend;
-  private String[] softDepend;
-  private String version;
-  private String[] commands;
-  private String description;
-  private String[] authors;
-  private String website;
-  
 }
