200b02853134db89e32d7e03f17b2c7c63cae113
MagicDroidX <1323471525@qq.com>
2015-09-13 21:48:57 +0800

    ServerInstance interface

---
+++ src/main/java/cn/nukkit/raknet/server/ServerInstance.java
@@ -0,0 +1,22 @@
+package cn.nukkit.raknet.server;
+
+import cn.nukkit.raknet.protocol.EncapsulatedPacket;
+
+/**
+ * author: MagicDroidX
+ * Nukkit Project
+ */
+public interface ServerInstance {
+
+    void openSession(String identifier, String address, int port, long clientID);
+
+    void closeSession(String identifier, String reason);
+
+    void handleEncapsulated(String identifier, EncapsulatedPacket packet, int flags);
+
+    void handleRaw(String address, int port, byte[] payload);
+
+    void notifyACK(String identifier, int identifierACK);
+
+    void handleOption(String option, String value);
+}
