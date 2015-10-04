fe20c9524269059f95aeab7aca2d99680c2a056a
MagicDroidX <1323471525@qq.com>
2015-10-04 15:21:10 +0800

    More events

---
+++ src/main/java/cn/nukkit/event/server/DataPacketSendEvent.java
@@ -0,0 +1,35 @@
+package cn.nukkit.event.server;
+
+import cn.nukkit.Player;
+import cn.nukkit.event.Cancellable;
+import cn.nukkit.event.HandlerList;
+import cn.nukkit.network.protocol.DataPacket;
+
+/**
+ * author: MagicDroidX
+ * Nukkit Project
+ */
+public class DataPacketSendEvent extends ServerEvent implements Cancellable {
+
+    private static final HandlerList handlers = new HandlerList();
+
+    private DataPacket packet;
+    private Player player;
+
+    public DataPacketSendEvent(Player player, DataPacket packet) {
+        this.packet = packet;
+        this.player = player;
+    }
+
+    public DataPacket getPacket() {
+        return packet;
+    }
+
+    public Player getPlayer() {
+        return player;
+    }
+
+    public static HandlerList getHandlers() {
+        return handlers;
+    }
+}
