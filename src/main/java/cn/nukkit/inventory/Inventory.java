8e3f2d825f85c4a33beec903be326712d13cc9e7
MagicDroidX <1323471525@qq.com>
2015-10-07 12:46:59 +0800

    Start working on Inventory part!

--- src/main/java/cn/nukkit/inventory/Inventory.java
+++ src/main/java/cn/nukkit/inventory/Inventory.java
@@ -1,8 +1,11 @@
 package cn.nukkit.inventory;
 
+import cn.nukkit.Player;
 import cn.nukkit.item.Item;
 
+import java.util.Collection;
 import java.util.Map;
+import java.util.Set;
 
 /**
  * author: MagicDroidX
@@ -10,5 +13,71 @@ import java.util.Map;
  */
 public interface Inventory {
 
+    int MAX_STACK = 64;
+
+    int getSize();
+
+    int getMaxStackSize();
+
+    void setMaxStackSize(int size);
+
+    String getName();
+
+    String getTitle();
+
+    Item getItem(int index);
+
+    boolean setItem(int index, Item item);
+
+    Item[] addItem(Item... slots);
+
+    boolean canAddItem(Item item);
+
+    public Item[] removeItem(Item... slots);
+
     Map<Integer, Item> getContents();
+
+    void setContents(Map<Integer, Item> items);
+
+    void sendContents(Player player);
+
+    void sendContents(Player[] players);
+
+    void sendContents(Collection<Player> players);
+
+    void sendSlot(int index, Player player);
+
+    void sendSlot(int index, Player[] players);
+
+    void sendSlot(int index, Collection<Player> players);
+
+    boolean contains(Item item);
+
+    Map<Integer, Item> all(Item item);
+
+    int first(Item item);
+
+    int firstEmpty(Item item);
+
+    void remove(Item item);
+
+    boolean clear(int index);
+
+    void clearAll();
+
+    public Set<Player> getViewers();
+
+    InventoryType getType();
+
+    InventoryHolder getHolder();
+
+    void onOpen(Player who);
+
+    boolean open(Player who);
+
+    void close(Player who);
+
+    void onClose(Player who);
+
+    void onSlotChange(int index, Item before);
 }
---
+++ src/main/java/cn/nukkit/inventory/InventoryType.java
@@ -0,0 +1,60 @@
+package cn.nukkit.inventory;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * author: MagicDroidX
+ * Nukkit Project
+ */
+public class InventoryType {
+    public static final int CHEST = 0;
+    public static final int DOUBLE_CHEST = 1;
+    public static final int PLAYER = 2;
+    public static final int FURNACE = 3;
+    public static final int CRAFTING = 4;
+    public static final int WORKBENCH = 5;
+    public static final int STONECUTTER = 6;
+
+    private static Map<Integer, InventoryType> defaults = new HashMap<>();
+
+    private int size;
+    private String title;
+    private int typeId;
+
+    public static InventoryType get(int index) {
+        return defaults.containsKey(index) ? defaults.get(index) : null;
+    }
+
+    public static void init() {
+        if (!defaults.isEmpty()) {
+            return;
+        }
+
+        defaults.put(CHEST, new InventoryType(27, "Chest", 0));
+        defaults.put(DOUBLE_CHEST, new InventoryType(27 + 27, "Double Chest", 0));
+        defaults.put(PLAYER, new InventoryType(40, "Player", 0)); //27 CONTAINER, 4 ARMOR (9 reference HOTBAR slots)
+        defaults.put(FURNACE, new InventoryType(3, "Furnace", 2));
+        defaults.put(CRAFTING, new InventoryType(5, "Crafting", 1)); //4 CRAFTING slots, 1 RESULT
+        defaults.put(WORKBENCH, new InventoryType(10, "Crafting", 1)); //9 CRAFTING slots, 1 RESULT
+        defaults.put(STONECUTTER, new InventoryType(10, "Crafting", 3)); //9 CRAFTING slots, 1 RESULT
+    }
+
+    public InventoryType(int defaultSize, String defaultTile, int typeId) {
+        this.size = defaultSize;
+        this.title = defaultTile;
+        this.typeId = typeId;
+    }
+
+    public int getDefaultSize() {
+        return size;
+    }
+
+    public String getDefaultTitle() {
+        return title;
+    }
+
+    public int getNetworkType() {
+        return typeId;
+    }
+}
