d57e039ca7072dbe11bad834037671c8646cb922
MagicDroidX <1323471525@qq.com>
2015-10-11 12:43:42 +0800

    More Inventory

---
+++ src/main/java/cn/nukkit/tile/Furnace.java
@@ -0,0 +1,10 @@
+package cn.nukkit.tile;
+
+import cn.nukkit.inventory.InventoryHolder;
+
+/**
+ * author: MagicDroidX
+ * Nukkit Project
+ */
+public class Furnace extends Tile implements InventoryHolder, Container {
+}
