f591dfab59235d0091ccaee0f73994b141d98ba1
Snake1999 <237356812@qq.com>
2015-10-11 20:33:50 +0800

    Javadoc Part 4 : Fixes on PluginLoadOrder

--- src/main/java/cn/nukkit/plugin/PluginLoadOrder.java
+++ src/main/java/cn/nukkit/plugin/PluginLoadOrder.java
@@ -4,12 +4,10 @@ package cn.nukkit.plugin;
  * 描述一个Nukkit插件加载顺序的类。<br />
  * Describes a Nukkit plugin load order.
  *
- * <p>Nukkit插件的加载顺序有两个:{@code STARTUP}和{@code POSTWORLD}。
- * {@code STARTUP}表示这个插件在服务器启动时就开始加载。
- * {@code POSTWORLD}表示这个插件在第一个世界加载完成后开始加载。<br />
- * The load order of a Nukkit plugin can be {@code STARTUP} or {@code POSTWORLD}.
- * {@code STARTUP} indicates that the plugin will be loaded at startup, and
- * {@code POSTWORLD} indicates that the plugin will be loaded after the first/default world was created.</p>
+ * <p>Nukkit插件的加载顺序有两个:{@link cn.nukkit.plugin.PluginLoadOrder#STARTUP}
+ * 和 {@link cn.nukkit.plugin.PluginLoadOrder#POSTWORLD}。<br />
+ * The load order of a Nukkit plugin can be {@link cn.nukkit.plugin.PluginLoadOrder#STARTUP}
+ * or {@link cn.nukkit.plugin.PluginLoadOrder#POSTWORLD}.</p>
  *
  * @author MagicDroidX(code) @ Nukkit Project
  * @author iNevet(code) @ Nukkit Project
@@ -18,16 +16,16 @@ package cn.nukkit.plugin;
  */
 public enum PluginLoadOrder {
     /**
-     * {@code STARTUP}表示这个插件在服务器启动时就开始加载。<br />
-     * {@code STARTUP} indicates that the plugin will be loaded at startup.
+     * 表示这个插件在服务器启动时就开始加载。<br />
+     * Indicates that the plugin will be loaded at startup.
      *
      * @see cn.nukkit.plugin.PluginLoadOrder
      * @since Nukkit 1.0 | Nukkit API 1.0.0
      */
     STARTUP,
     /**
-     * {@code POSTWORLD}表示这个插件在第一个世界加载完成后开始加载。<br />
-     * {@code POSTWORLD} indicates that the plugin will be loaded after the first/default world was created.
+     * 表示这个插件在第一个世界加载完成后开始加载。<br />
+     * Indicates that the plugin will be loaded after the first/default world was created.
      *
      * @see cn.nukkit.plugin.PluginLoadOrder
      * @since Nukkit 1.0 | Nukkit API 1.0.0
