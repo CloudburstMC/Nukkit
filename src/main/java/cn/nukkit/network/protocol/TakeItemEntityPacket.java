85f1772bdcc893d33b82531da436a664b9f342a9
caoli5288 <caoli5288@gmail.com>
2015-10-14 21:22:22 +0800

    Implements TakeItemEntityPacket

---
+++ src/main/java/cn/nukkit/network/protocol/TakeItemEntityPacket.java
@@ -0,0 +1,30 @@
+package cn.nukkit.network.protocol;
+
+/**
+ * Created on 15-10-14.
+ */
+public class TakeItemEntityPacket extends DataPacket {
+
+    public static final byte NETWORK_ID = Info.TAKE_ITEM_ENTITY_PACKET;
+
+    public long entityId;
+    public long target;
+
+    @Override
+    public void decode() {
+        ;
+    }
+
+    @Override
+    public void encode() {
+        reset();
+        putLong(entityId);
+        putLong(target);
+    }
+
+    @Override
+    public byte pid() {
+        return NETWORK_ID;
+    }
+
+}
