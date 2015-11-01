70b53f297fbae5feeb1329637e8b42acc2464593
MagicDroidX <1323471525@qq.com>
2015-11-01 16:53:53 +0800

    ?

--- src/main/java/cn/nukkit/event/entity/EntityShootBowEvent.java
+++ src/main/java/cn/nukkit/event/entity/EntityShootBowEvent.java
@@ -22,32 +22,44 @@ public class EntityShootBowEvent extends EntityEvent implements Cancellable {
 
     private Projectile projectile;
 
+    private double force;
 
+    public void __construct(Living shooter, Item bow, Projectile projectile, double force) {
         this.entity = shooter;
         this.bow = bow;
         this.projectile = projectile;
         this.force = force;
     }
 
+    public Living getEntity() {
+        return (Living) this.entity;
     }
 
 
+    public Item getBow() {
         return this.bow;
     }
 
 
+    public Entity getProjectile() {
         return this.projectile;
     }
 
+    public void setProjectile(Entity projectile) {
+        if (projectile != this.projectile) {
+            if (this.projectile.getViewers().size() == 0) {
                 this.projectile.kill();
                 this.projectile.close();
             }
+            this.projectile = (Projectile) projectile;
         }
     }
 
+    public double getForce() {
         return this.force;
     }
 
+    public void setForce(double force) {
         this.force = force;
     }
 }
