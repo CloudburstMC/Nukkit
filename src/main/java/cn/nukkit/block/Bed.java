4ba08be55ea0ee3dde43f9637893a8be5bdd0607
MagicDroidX <1323471525@qq.com>
2015-11-02 22:14:31 +0800

    Have a nice sleep!

--- src/main/java/cn/nukkit/block/Bed.java
+++ src/main/java/cn/nukkit/block/Bed.java
@@ -2,7 +2,9 @@ package cn.nukkit.block;
 
 import cn.nukkit.Player;
 import cn.nukkit.item.Item;
+import cn.nukkit.level.Level;
 import cn.nukkit.math.AxisAlignedBB;
+import cn.nukkit.utils.TextFormat;
 
 /**
  * author: MagicDroidX
@@ -52,7 +54,7 @@ public class Bed extends Transparent {
 
     @Override
     public boolean onActivate(Item item, Player player) {
-        /*int time = this.getLevel().getTime() % Level.TIME_FULL;
+        int time = this.getLevel().getTime() % Level.TIME_FULL;
 
         boolean isNight = (time >= Level.TIME_NIGHT && time < Level.TIME_SUNRISE);
 
@@ -86,8 +88,78 @@ public class Bed extends Transparent {
                 return true;
             }
         }
-*/
-        //todo
+
+        if (player != null && !player.sleepOn(b)) {
+            player.sendMessage(TextFormat.GRAY + "This bed is occupied");
+        }
+
+
+        return true;
+    }
+
+    @Override
+    public boolean place(Item item, Block block, Block target, int face, double fx, double fy, double fz) {
+        return this.place(item, block, target, face, fx, fy, fz, null);
+    }
+
+    @Override
+    public boolean place(Item item, Block block, Block target, int face, double fx, double fy, double fz, Player player) {
+        Block down = this.getSide(0);
+        if (!down.isTransparent()) {
+            int[] faces = {3, 4, 2, 5};
+            int d = player != null ? player.getDirection() : 0;
+            Block next = this.getSide(faces[((d + 3) % 4)]);
+            Block downNext = this.getSide(0);
+
+            if (next.canBeReplaced() && !downNext.isTransparent()) {
+                int meta = ((d + 3) % 4) & 0x03;
+                this.getLevel().setBlock(block, Block.get(this.id, meta), true, true);
+                this.getLevel().setBlock(next, Block.get(this.id, meta | 0x08), true, true);
+
+                return true;
+            }
+        }
+
         return false;
+    }
+
+    @Override
+    public boolean onBreak(Item item) {
+        Block blockNorth = this.getSide(2); //Gets the blocks around them
+        Block blockSouth = this.getSide(3);
+        Block blockEast = this.getSide(5);
+        Block blockWest = this.getSide(4);
+
+        if ((this.meta & 0x08) == 0x08) { //This is the Top part of bed
+            if (blockNorth.getId() == this.id && blockNorth.meta != 0x08) { //Checks if the block ID&&meta are right
+                this.getLevel().setBlock(blockNorth, new Air(), true, true);
+            } else if (blockSouth.getId() == this.id && blockSouth.meta != 0x08) {
+                this.getLevel().setBlock(blockSouth, new Air(), true, true);
+            } else if (blockEast.getId() == this.id && blockEast.meta != 0x08) {
+                this.getLevel().setBlock(blockEast, new Air(), true, true);
+            } else if (blockWest.getId() == this.id && blockWest.meta != 0x08) {
+                this.getLevel().setBlock(blockWest, new Air(), true, true);
+            }
+        } else { //Bottom Part of Bed
+            if (blockNorth.getId() == this.id && (blockNorth.meta & 0x08) == 0x08) {
+                this.getLevel().setBlock(blockNorth, new Air(), true, true);
+            } else if (blockSouth.getId() == this.id && (blockSouth.meta & 0x08) == 0x08) {
+                this.getLevel().setBlock(blockSouth, new Air(), true, true);
+            } else if (blockEast.getId() == this.id && (blockEast.meta & 0x08) == 0x08) {
+                this.getLevel().setBlock(blockEast, new Air(), true, true);
+            } else if (blockWest.getId() == this.id && (blockWest.meta & 0x08) == 0x08) {
+                this.getLevel().setBlock(blockWest, new Air(), true, true);
+            }
+        }
+        this.getLevel().setBlock(this, new Air(), true, true);
+
+        return true;
+    }
+
+    @Override
+    public int[][] getDrops(Item item) {
+        return new int[][]{
+                {Item.BED, 0, 1}
+        };
     }
 }
