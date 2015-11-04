9b0c7d6a8f61dd676c90b5e64662c0ffa59e9e81
MagicDroidX <1323471525@qq.com>
2015-11-04 17:29:58 +0800

    Lava and Water

--- src/main/java/cn/nukkit/block/Lava.java
+++ src/main/java/cn/nukkit/block/Lava.java
@@ -1,5 +1,14 @@
 package cn.nukkit.block;
 
+import cn.nukkit.Player;
+import cn.nukkit.Server;
+import cn.nukkit.entity.Effect;
+import cn.nukkit.entity.Entity;
+import cn.nukkit.event.entity.EntityCombustByBlockEvent;
+import cn.nukkit.event.entity.EntityDamageByBlockEvent;
+import cn.nukkit.event.entity.EntityDamageEvent;
+import cn.nukkit.item.Item;
+
 /**
  * author: MagicDroidX
  * Nukkit Project
@@ -16,9 +25,42 @@ public class Lava extends Liquid {
     }
 
     @Override
+    public int getLightLevel() {
+        return 15;
+    }
+
+    @Override
     public String getName() {
         return "Lava";
     }
 
-    //todo
+    @Override
+    public void onEntityCollide(Entity entity) {
+        entity.fallDistance *= 0.5;
+        if (!entity.hasEffect(Effect.FIRE_RESISTANCE)) {
+            EntityDamageByBlockEvent ev = new EntityDamageByBlockEvent(this, entity, EntityDamageEvent.CAUSE_LAVA, 4);
+            entity.attack(ev.getFinalDamage(), ev);
+        }
+
+        EntityCombustByBlockEvent ev = new EntityCombustByBlockEvent(this, entity, 15);
+        Server.getInstance().getPluginManager().callEvent(ev);
+        if (!ev.isCancelled()) {
+            entity.setOnFire(ev.getDuration());
+        }
+
+        entity.resetFallDistance();
+    }
+
+    @Override
+    public boolean place(Item item, Block block, Block target, int face, double fx, double fy, double fz) {
+        return this.place(item, block, target, face, fx, fy, fz, null);
+    }
+
+    @Override
+    public boolean place(Item item, Block block, Block target, int face, double fx, double fy, double fz, Player player) {
+        boolean ret = this.getLevel().setBlock(this, this, true, false);
+        this.getLevel().scheduleUpdate(this, this.tickRate());
+
+        return ret;
+    }
 }
--- src/main/java/cn/nukkit/block/Water.java
+++ src/main/java/cn/nukkit/block/Water.java
@@ -1,5 +1,9 @@
 package cn.nukkit.block;
 
+import cn.nukkit.Player;
+import cn.nukkit.entity.Entity;
+import cn.nukkit.item.Item;
+
 /**
  * author: MagicDroidX
  * Nukkit Project
@@ -20,5 +24,26 @@ public class Water extends Liquid {
         return "Water";
     }
 
-    //todo
+    @Override
+    public void onEntityCollide(Entity entity) {
+        entity.resetFallDistance();
+        if (entity.fireTicks > 0) {
+            entity.extinguish();
+        }
+
+        entity.resetFallDistance();
+    }
+
+    @Override
+    public boolean place(Item item, Block block, Block target, int face, double fx, double fy, double fz) {
+        return this.place(item, block, target, face, fx, fy, fz, null);
+    }
+
+    @Override
+    public boolean place(Item item, Block block, Block target, int face, double fx, double fy, double fz, Player player) {
+        boolean ret = this.getLevel().setBlock(this, this, true, false);
+        this.getLevel().scheduleUpdate(this, this.tickRate());
+
+        return ret;
+    }
 }
