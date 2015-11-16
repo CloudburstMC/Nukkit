90e69ef9c9e432ddeaebe6756c386e0e6d81a460
MagicDroidX <1323471525@qq.com>
2015-11-16 17:53:42 +0800

    code formatting

--- src/main/java/cn/nukkit/InterruptibleThread.java
+++ src/main/java/cn/nukkit/InterruptibleThread.java
@@ -3,10 +3,10 @@ package cn.nukkit;
 /**
  * 描述一个可以被中断的线程的接口。<br>
  * An interface to describe a thread that can be interrupted.
- *
+ * <p>
  * <p>在Nukkit服务器停止时，Nukkit会找到所有实现了{@code InterruptibleThread}的线程，并逐一中断。<br>
- *     When a Nukkit server is stopping, Nukkit finds all threads implements {@code InterruptibleThread},
- *     and interrupt them one by one.</p>
+ * When a Nukkit server is stopping, Nukkit finds all threads implements {@code InterruptibleThread},
+ * and interrupt them one by one.</p>
  *
  * @author MagicDroidX(code) @ Nukkit Project
  * @author 粉鞋大妈(javadoc) @ Nukkit Project
--- src/main/java/cn/nukkit/plugin/PluginLoader.java
+++ src/main/java/cn/nukkit/plugin/PluginLoader.java
@@ -17,15 +17,15 @@ public interface PluginLoader {
     /**
      * 通过文件名字的字符串，来加载和初始化一个插件。<br>
      * Loads and initializes a plugin by its file name.
-     *
+     * <p>
      * <p>这个方法应该设置好插件的相关属性。比如，插件所在的服务器对象，插件的加载器对象，插件的描述对象，插件的数据文件夹。<br>
-     *     Properties for loaded plugin should be set in this method. Such as, the {@code Server} object for which this
-     *     plugin is running in, the {@code PluginLoader} object for its loader, and the {@code File} object for its
-     *     data folder.</p>
-     *
+     * Properties for loaded plugin should be set in this method. Such as, the {@code Server} object for which this
+     * plugin is running in, the {@code PluginLoader} object for its loader, and the {@code File} object for its
+     * data folder.</p>
+     * <p>
      * <p>如果插件加载失败，这个方法应该返回{@code null}，或者抛出异常。<br>
-     *     If the plugin loader does not load this plugin successfully, a {@code null} should be returned,
-     *     or an exception should be thrown.</p>
+     * If the plugin loader does not load this plugin successfully, a {@code null} should be returned,
+     * or an exception should be thrown.</p>
      *
      * @param filename 这个插件的文件名字字符串。<br>A string of its file name.
      * @return 加载完毕的插件的 {@code Plugin}对象。<br>The loaded plugin as a {@code Plugin} object.
@@ -39,15 +39,15 @@ public interface PluginLoader {
     /**
      * 通过插件的 {@code File}对象，来加载和初始化一个插件。<br>
      * Loads and initializes a plugin by a {@code File} object describes the file.
-     *
+     * <p>
      * <p>这个方法应该设置好插件的相关属性。比如，插件所在的服务器对象，插件的加载器对象，插件的描述对象，插件的数据文件夹。<br>
-     *     Properties for loaded plugin should be set in this method. Such as, the {@code Server} object for which this
-     *     plugin is running in, the {@code PluginLoader} object for its loader, and the {@code File} object for its
-     *     data folder.</p>
-     *
+     * Properties for loaded plugin should be set in this method. Such as, the {@code Server} object for which this
+     * plugin is running in, the {@code PluginLoader} object for its loader, and the {@code File} object for its
+     * data folder.</p>
+     * <p>
      * <p>如果插件加载失败，这个方法应该返回{@code null}，或者抛出异常。<br>
-     *     If the plugin loader does not load this plugin successfully, a {@code null} should be returned,
-     *     or an exception should be thrown.</p>
+     * If the plugin loader does not load this plugin successfully, a {@code null} should be returned,
+     * or an exception should be thrown.</p>
      *
      * @param file 这个插件的文件的 {@code File}对象。<br>A {@code File} object for this plugin.
      * @return 加载完毕的插件的 {@code Plugin}对象。<br>The loaded plugin as a {@code Plugin} object.
@@ -61,13 +61,13 @@ public interface PluginLoader {
     /**
      * 通过插件文件名的字符串，来获得描述这个插件的 {@code PluginDescription}对象。<br>
      * Gets a {@code PluginDescription} object describes the plugin by its file name.
-     *
+     * <p>
      * <p>如果插件的描述对象获取失败，这个方法应该返回{@code null}。<br>
-     *     If the plugin loader does not get its description successfully, a {@code null} should be returned.</p>
+     * If the plugin loader does not get its description successfully, a {@code null} should be returned.</p>
      *
      * @param filename 这个插件的文件名字。<br>A string of its file name.
      * @return 描述这个插件的 {@code PluginDescription}对象。<br>
-     *          A {@code PluginDescription} object describes the plugin.
+     * A {@code PluginDescription} object describes the plugin.
      * @see #getPluginDescription(File)
      * @since Nukkit 1.0 | Nukkit API 1.0.0
      */
@@ -76,13 +76,13 @@ public interface PluginLoader {
     /**
      * 通过插件的 {@code File}对象，来获得描述这个插件的 {@code PluginDescription}对象。<br>
      * Gets a {@code PluginDescription} object describes the plugin by a {@code File} object describes the plugin file.
-     *
+     * <p>
      * <p>如果插件的描述对象获取失败，这个方法应该返回{@code null}。<br>
-     *     If the plugin loader does not get its description successfully, a {@code null} should be returned.</p>
+     * If the plugin loader does not get its description successfully, a {@code null} should be returned.</p>
      *
      * @param file 这个插件的文件的 {@code File}对象。<br>A {@code File} object for this plugin.
      * @return 描述这个插件的 {@code PluginDescription}对象。<br>
-     *          A {@code PluginDescription} object describes the plugin.
+     * A {@code PluginDescription} object describes the plugin.
      * @see #getPluginDescription(String)
      * @since Nukkit 1.0 | Nukkit API 1.0.0
      */
@@ -91,17 +91,17 @@ public interface PluginLoader {
     /**
      * 返回这个插件加载器支持的文件类型。<br>
      * Returns the file types this plugin loader supports.
-     *
+     * <p>
      * <p>在Nukkit读取所有插件时，插件管理器会查找所有已经安装的插件加载器，通过识别这个插件是否满足下面的条件，
-     *    来选择对应的插件加载器。<br>
-     *    When Nukkit is trying to load all its plugins, the plugin manager will look for all installed plugin loader,
-     *    and choose the correct one by checking if this plugin matches the filters given below.</p>
-     *
+     * 来选择对应的插件加载器。<br>
+     * When Nukkit is trying to load all its plugins, the plugin manager will look for all installed plugin loader,
+     * and choose the correct one by checking if this plugin matches the filters given below.</p>
+     * <p>
      * <p>举个例子，识别这个文件是否以jar为扩展名，它的正则表达式是：<br>
-     *    For example, to check if this file is has a "jar" extension, the regular expression should be:<br>
-     *    {@code ^.+\\.jar$}<br>
-     *    所以只读取jar扩展名的插件加载器，这个函数应该写成：<br>
-     *    So, for a jar-extension-only file plugin loader, this method should be:
+     * For example, to check if this file is has a "jar" extension, the regular expression should be:<br>
+     * {@code ^.+\\.jar$}<br>
+     * 所以只读取jar扩展名的插件加载器，这个函数应该写成：<br>
+     * So, for a jar-extension-only file plugin loader, this method should be:
      * <pre> {@code           @Override}
      *      public Pattern[] getPluginFilters() {
      *          return new Pattern[]{Pattern.compile("^.+\\.jar$")};
@@ -109,7 +109,7 @@ public interface PluginLoader {
      * </p>
      *
      * @return 表达这个插件加载器支持的文件类型的正则表达式数组。<br>
-     *          An array of regular expressions, that describes what kind of file this plugin loader supports.
+     * An array of regular expressions, that describes what kind of file this plugin loader supports.
      * @since Nukkit 1.0 | Nukkit API 1.0.0
      */
     Pattern[] getPluginFilters();
