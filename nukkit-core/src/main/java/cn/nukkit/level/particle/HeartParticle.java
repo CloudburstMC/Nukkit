c196a8125bce25ef6b9b9d7b335566ad9a24c291
Snake1999 <237356812@qq.com>
2015-11-21 17:04:55 +0800

    Particles F~P. todo: something for floating text

---
+++ nukkit-core/src/main/java/cn/nukkit/level/particle/HeartParticle.java
@@ -0,0 +1,17 @@
+package cn.nukkit.level.particle;
+
+import cn.nukkit.math.Vector3;
+
+/**
+ * Created on 2015/11/21 by xtypr.
+ * Package cn.nukkit.level.particle in project Nukkit .
+ */
+public class HeartParticle extends GenericParticle {
+    public HeartParticle(Vector3 pos){
+        this(pos, 0);
+    }
+
+    public HeartParticle(Vector3 pos, int scale) {
+        super(pos, 14, scale);
+    }
+}
