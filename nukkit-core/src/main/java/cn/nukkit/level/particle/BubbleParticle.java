fdcb462fbdcbaf288a9d9c93c139f1a5b19ee67b
Snake1999 <237356812@qq.com>
2015-11-21 16:51:03 +0800

    Particles A~E

---
+++ nukkit-core/src/main/java/cn/nukkit/level/particle/BubbleParticle.java
@@ -0,0 +1,13 @@
+package cn.nukkit.level.particle;
+
+import cn.nukkit.math.Vector3;
+
+/**
+ * Created on 2015/11/21 by xtypr.
+ * Package cn.nukkit.level.particle in project Nukkit .
+ */
+public class BubbleParticle extends GenericParticle {
+    public BubbleParticle(Vector3 pos) {
+        super(pos, 1);
+    }
+}
---
+++ nukkit-core/src/main/java/cn/nukkit/level/particle/CriticalParticle.java
@@ -0,0 +1,13 @@
+package cn.nukkit.level.particle;
+
+import cn.nukkit.math.Vector3;
+
+/**
+ * Created on 2015/11/21 by xtypr.
+ * Package cn.nukkit.level.particle in project Nukkit .
+ */
+public class CriticalParticle extends GenericParticle {
+    public CriticalParticle(Vector3 pos, int scale) {
+        super(pos, 2, scale);
+    }
+}
