78ad895b3924e47442a6805bbcc8da9688fb5b79
MagicDroidX <1323471525@qq.com>
2015-11-21 22:14:32 +0800

    more sound

---
+++ src/main/java/cn/nukkit/level/sound/AnvilBreakSound.java
@@ -0,0 +1,18 @@
+package cn.nukkit.level.sound;
+
+import cn.nukkit.math.Vector3;
+import cn.nukkit.network.protocol.LevelEventPacket;
+
+/**
+ * Created on 2015/11/21 by xtypr.
+ * Package cn.nukkit.level.sound in project Nukkit .
+ */
+public class AnvilBreakSound extends GenericSound {
+    public AnvilBreakSound(Vector3 pos) {
+        this(pos, 0);
+    }
+
+    public AnvilBreakSound(Vector3 pos, float pitch) {
+        super(pos, LevelEventPacket.EVENT_SOUND_ANVIL_BREAK, pitch);
+    }
+}
---
+++ src/main/java/cn/nukkit/level/sound/EndermanTeleportSound.java
@@ -0,0 +1,18 @@
+package cn.nukkit.level.sound;
+
+import cn.nukkit.math.Vector3;
+import cn.nukkit.network.protocol.LevelEventPacket;
+
+/**
+ * Created on 2015/11/21 by xtypr.
+ * Package cn.nukkit.level.sound in project Nukkit .
+ */
+public class EndermanTeleportSound extends GenericSound {
+    public EndermanTeleportSound(Vector3 pos) {
+        this(pos, 0);
+    }
+
+    public EndermanTeleportSound(Vector3 pos, float pitch) {
+        super(pos, LevelEventPacket.EVENT_SOUND_ENDERMAN_TELEPORT, pitch);
+    }
+}
