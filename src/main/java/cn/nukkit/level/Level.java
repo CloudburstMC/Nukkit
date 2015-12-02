2da3e1d8674d5d690e7b8ab2ecb14766f51d3df8
Angelic47 <admin@angelic47.com>
2015-12-02 17:43:19 +0800

    formatting codes

--- src/main/java/cn/nukkit/level/Level.java
+++ src/main/java/cn/nukkit/level/Level.java
@@ -419,9 +419,9 @@ public class Level implements ChunkManager, Metadatable {
     public Map<Integer, Player> getChunkPlayers(int chunkX, int chunkZ) {
         String index = Level.chunkHash(chunkX, chunkZ);
         if (this.playerLoaders.containsKey(index)) {
-            HashMap<Integer, Player> ChunkPlayers = new HashMap<Integer, Player>();
-            ChunkPlayers.putAll(this.playerLoaders.get(index));
-            return ChunkPlayers;
+            HashMap<Integer, Player> chunkPlayers = new HashMap<Integer, Player>();
+            chunkPlayers.putAll(this.playerLoaders.get(index));
+            return chunkPlayers;
         } else {
             return new HashMap<>();
         }
