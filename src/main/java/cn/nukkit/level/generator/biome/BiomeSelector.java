0bf4dc970eaa112b250066effe34a38693c5851b
MagicDroidX <1323471525@qq.com>
2015-12-02 21:57:13 +0800

    Normal generator (NEED FIXING)

--- src/main/java/cn/nukkit/level/generator/biome/BiomeSelector.java
+++ src/main/java/cn/nukkit/level/generator/biome/BiomeSelector.java
@@ -21,19 +21,51 @@ public class BiomeSelector {
 
     public BiomeSelector(Random random, Biome fallback) {
         this.fallback = fallback;
-        this.temperature = new Simplex(random, 2, 1 / 16, 1 / 512);
-        this.rainfall = new Simplex(random, 2, 1 / 16, 1 / 512);
+        this.temperature = new Simplex(random, 2d, 1 / 16d, 1 / 512d);
+        this.rainfall = new Simplex(random, 2d, 1 / 16d, 1 / 512d);
     }
 
-    public int lookup(double... args) {
-        return 0;
+    public int lookup(double temperature, double rainfall) {
+        if (rainfall < 0.25) {
+            if (rainfall < 0.7) {
+                return Biome.OCEAN;
+            } else if (rainfall < 0.85) {
+                return Biome.RIVER;
+            } else {
+                return Biome.SWAMP;
+            }
+        } else if (rainfall < 0.60) {
+            if (temperature < 0.25) {
+                return Biome.ICE_PLAINS;
+            } else if (temperature < 0.75) {
+                return Biome.PLAINS;
+            } else {
+                return Biome.DESERT;
+            }
+        } else if (rainfall < 0.80) {
+            if (temperature < 0.25) {
+                return Biome.TAIGA;
+            } else if (temperature < 0.75) {
+                return Biome.FOREST;
+            } else {
+                return Biome.BIRCH_FOREST;
+            }
+        } else {
+            if (rainfall < 0.25) {
+                return Biome.MOUNTAINS;
+            } else if (rainfall < 0.70) {
+                return Biome.SMALL_MOUNTAINS;
+            } else {
+                return Biome.RIVER;
+            }
+        }
     }
 
     public void recalculate() {
         this.map = new int[64 * 64];
         for (int i = 0; i < 64; ++i) {
             for (int j = 0; j < 64; ++j) {
-                this.map[i + (j << 6)] = this.lookup(i / 63, j / 63);
+                this.map[i + (j << 6)] = this.lookup(i / 63d, j / 63d);
             }
         }
     }
@@ -51,8 +83,9 @@ public class BiomeSelector {
     }
 
     public Biome pickBiome(double x, double z) {
-        int temperature = (int) (this.getTemperature(x, z) * 63);
-        int rainfall = (int) (this.getRainfall(x, z) * 63);
+        int temperature = (int) (this.getTemperature(x, z) * 63d);
+        int rainfall = (int) (this.getRainfall(x, z) * 63d);
+
         int biomeId = this.map[temperature + (rainfall << 6)];
         return this.biomes.containsKey(biomeId) ? this.biomes.get(biomeId) : this.fallback;
     }
