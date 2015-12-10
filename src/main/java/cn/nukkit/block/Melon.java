e57ad198ab746ce9bfb7d29f106f4d34787f82d4
Pub4Game <Pub4Game@users.noreply.github.com>
2015-12-11 02:01:52 +0300

    implementation Melon Block

---
+++ src/main/java/cn/nukkit/block/Melon.java
@@ -0,0 +1,40 @@
+package cn.nukkit.block;
+
+import cn.nukkit.item.Item;
+import cn.nukkit.item.Tool;
+
+import java.util.Random;
+
+public class Melon extends Transparent {
+
+    public Melon() {
+        this(0);
+    }
+
+    public Melon(int meta) {
+        super(meta);
+    }
+
+    @Override
+    public int getId() {
+        return MELON_BLOCK;
+    }
+
+    public String getName() {
+        return "Melon Block";
+    }
+
+    public double getHardness() {
+        return 1;
+    }
+
+    @Override
+    public int[][] getDrops(Item item) {
+        return new int[][]{new int[]{Item.MELON_SLICE, 0, new Random().nextInt(3) + 4}};
+    }
+
+    @Override
+    public int getToolType() {
+        return Tool.TYPE_AXE;
+    }
+}
