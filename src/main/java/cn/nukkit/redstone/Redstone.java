8d4e940bf25bbfd8541c1b1088fc534bf46ff144
Angelic47 <admin@angelic47.com>
2015-12-14 14:11:38 +0800

    test redstone

---
+++ src/main/java/cn/nukkit/redstone/Redstone.java
@@ -0,0 +1,13 @@
+package cn.nukkit.redstone;
+
+/**
+ * author: Angelic47
+ * Nukkit Project
+ */
+public class Redstone {
+
+    public static final int ENERGY_NONE = 0;
+    public static final int ENERGY_WEAK = 1;
+    public static final int ENERGY_STRONG = 17;
+
+}
