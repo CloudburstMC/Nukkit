1564a9754758c028c3af9f90f578e4af60c051dc
Angelic47 <admin@angelic47.com>
2015-12-14 10:57:50 +0800

    packedice, chest(buggy), and small fix

---
+++ src/main/java/cn/nukkit/block/SnowBlock.java
@@ -0,0 +1,73 @@
+package cn.nukkit.block;
+
+import cn.nukkit.Player;
+import cn.nukkit.item.Item;
+import cn.nukkit.item.Tool;
+import cn.nukkit.level.Level;
+
+public class SnowBlock extends Solid {
+
+    public SnowBlock() {
+        this(0);
+    }
+
+    public SnowBlock(int meta) {
+        super(meta);
+    }
+
+    @Override
+    public String getName() {
+        return "Snow Block";
+    }
+
+    @Override
+    public int getId() {
+        return SNOW_BLOCK;
+    }
+
+    @Override
+    public double getHardness() {
+        return 0.2;
+    }
+
+    @Override
+    public int getToolType() {
+        return Tool.TYPE_SHOVEL;
+    }
+
+    @Override
+    public boolean canBeReplaced() {
+        return true;
+    }
+
+    //TODO:雪片叠垒乐
+
+    @Override
+    public boolean place(Item item, Block block, Block target, int face, double fx, double fy, double fz, Player player) {
+return this.place(item, block, target, face, fx, fy, fz, player);
+    }
+/*
+    @Override
+    public int onUpdate(int type) {
+
+        if (type == Level.BLOCK_UPDATE_NORMAL) {
+            if (this.getSide(0).getId() == AIR) {
+                this.getLevel().useBreakOn(this);
+
+                return Level.BLOCK_UPDATE_NORMAL;
+            }
+        }
+
+        return 0;
+    }
+*/
+    @Override
+    public int[][] getDrops(Item item) {
+        if (item.isShovel()) {
+            return new int[][]{
+                    {Item.SNOWBALL, 0, 8}
+            };
+        }
+        return new int[][]{};
+    }
+}
