38903c9b96499d6ba35b09063d9a189b54b66282
Pub4Game <Pub4Game@users.noreply.github.com>
2016-01-03 22:21:16 +0300

    Create RedMushroom.java

---
+++ src/main/java/cn/nukkit/block/RedMushroom.java
@@ -0,0 +1,58 @@
+package cn.nukkit.block;
+
+import cn.nukkit.Player;
+import cn.nukkit.item.Item;
+import cn.nukkit.level.Level;
+import cn.nukkit.math.AxisAlignedBB;
+
+/**
+ * Created by Pub4Game on 03.01.2015.
+ */
+public class RedMushroom extends Flowable {
+
+    public RedMushroom(int meta) {
+        super(meta);
+    }
+
+    public RedMushroom() {
+        this(0);
+    }
+
+    @Override
+    public String getName() {
+        return "Red Mushroom";
+    }
+
+    @Override
+    public int getId() {
+        return Block.RED_MUSHROOM;
+    }
+
+    @Override
+    public int onUpdate(int type) {
+        if (type == Level.BLOCK_UPDATE_NORMAL) {
+            if (getSide(0).isTransparent()) {
+                getLevel().useBreakOn(this);
+
+                return Level.BLOCK_UPDATE_NORMAL;
+            }
+        }
+        return 0;
+    }
+
+    @Override
+    public boolean place(Item item, Block block, Block target, int face, double fx, double fy, double fz, Player player) {
+        Block down = getSide(0);
+        if (!down.isTransparent()) {
+            getLevel().setBlock(block, this, true, true);
+
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public AxisAlignedBB getBoundingBox() {
+        return null;
+    }
+}
