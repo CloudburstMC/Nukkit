283c0e0c0fc57ba7a620a3426b679bcf8cc92419
Pub4Game <Pub4Game@users.noreply.github.com>
2016-01-10 15:37:18 +0300

    Update Ice.java

--- src/main/java/cn/nukkit/block/Ice.java
+++ src/main/java/cn/nukkit/block/Ice.java
@@ -2,6 +2,7 @@ package cn.nukkit.block;
 
 import cn.nukkit.item.Item;
 import cn.nukkit.item.Tool;
+import cn.nukkit.level.Level;
 
 /**
  * author: MagicDroidX
@@ -28,19 +29,35 @@ public class Ice extends Transparent {
     }
 
     @Override
+    public double getResistance() {
+        return 2.5;
+    }
+
+    @Override
     public double getHardness() {
         return 0.5;
     }
 
     @Override
     public int getToolType() {
-        return Tool.TYPE_NONE;
+        return Tool.TYPE_PICKAXE;
     }
 
     @Override
     public boolean onBreak(Item item) {
         this.getLevel().setBlock(this, new Water(), true);
         return true;
+    }
+
+    @Override
+    public int onUpdate(int type) {
+        if(type == Level.BLOCK_UPDATE_RANDOM){
+            if(this.getLevel().getBlockLightAt((int) this.x, (int) this.y, (int) this.z) >= 12){
+                this.getLevel().setBlock(this, new Water(), true);
+                return Level.BLOCK_UPDATE_NORMAL;
+            }
+        }
+        return 0;
     }
 
     @Override
