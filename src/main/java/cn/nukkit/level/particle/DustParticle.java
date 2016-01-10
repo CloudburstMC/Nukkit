59365ec5e99247af39553ba41c35c1b8601acc08
Snake1999 <237356812@qq.com>
2016-01-10 21:55:56 +0800

    Color API for colored particles

--- src/main/java/cn/nukkit/level/particle/DustParticle.java
+++ src/main/java/cn/nukkit/level/particle/DustParticle.java
@@ -1,12 +1,17 @@
 package cn.nukkit.level.particle;
 
 import cn.nukkit.math.Vector3;
+import cn.nukkit.utils.Color;
 
 /**
  * Created on 2015/11/21 by xtypr.
  * Package cn.nukkit.level.particle in project Nukkit .
  */
 public class DustParticle extends GenericParticle {
+
+    public DustParticle(Vector3 pos, Color color) {
+        this(pos, color.getRed(), color.getGreen(), color.getBlue(), color.getAlpha());
+    }
 
     public DustParticle(Vector3 pos, int r, int g, int b) {
         this(pos, r, g, b, 255);
