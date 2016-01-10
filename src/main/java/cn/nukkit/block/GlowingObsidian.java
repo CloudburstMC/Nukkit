456b2417164718895fc701cb587cafac1d1d4e2c
Pub4Game <Pub4Game@users.noreply.github.com>
2016-01-10 18:08:37 +0300

    Update GlowingObsidian.java

--- src/main/java/cn/nukkit/block/GlowingObsidian.java
+++ src/main/java/cn/nukkit/block/GlowingObsidian.java
@@ -1,5 +1,8 @@
 package cn.nukkit.block;
 
+import cn.nukkit.item.Item;
+import cn.nukkit.item.Tool;
+
 /**
  * Created on 2015/11/22 by xtypr.
  * Package cn.nukkit.block in project Nukkit .
@@ -22,9 +25,23 @@ public class GlowingObsidian extends Solid {
     public String getName() {
         return "Glowing Obsidian";
     }
+    
+    @Override
+    public double getResistance() {
+        return 6000;
+    }
 
     @Override
     public int getLightLevel() {
         return 12;
+    }
+    
+    @Override
+    public int[][] getDrops(Item item) {
+        if (item.isPickaxe() && item.getTier() > Tool.DIAMOND_PICKAXE) {
+            return new int[][]{{Block.GLOWING_OBSIDIAN, 0, 1}};
+        } else {
+            return new int[0][];
+        }
     }
 }
