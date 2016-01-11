70b75f2ffb587b6b02ba72edf2ec17e3f5daca38
grzegorz2047 <grzegorz2047@gmail.com>
2016-01-11 12:46:38 +0100

    Fixed snowball taking damage > 0

--- src/main/java/cn/nukkit/entity/Projectile.java
+++ src/main/java/cn/nukkit/entity/Projectile.java
@@ -133,6 +133,9 @@ public abstract class Projectile extends Entity {
                     if (this instanceof Arrow && ((Arrow) this).isCritical) {
                         damage += new Random().nextInt((int) (damage / 2) + 1);
                     }
+                    if (this instanceof Snowball) {
+                        damage = 0;
+                    }
 
                     EntityDamageEvent ev;
                     if (this.shootingEntity == null) {
