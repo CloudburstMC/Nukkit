0fb6d7575479a7f7da4f9a2bce59d059a8747228
极夜暗芒使 <1323471525@qq.com>
2016-01-22 20:03:08 +0800

    More friendly API

--- src/main/java/cn/nukkit/item/randomitem/Fishing.java
+++ src/main/java/cn/nukkit/item/randomitem/Fishing.java
@@ -1,8 +1,8 @@
 package cn.nukkit.item.randomitem;
 
+import cn.nukkit.item.Dye;
 import cn.nukkit.item.Item;
 import cn.nukkit.item.enchantment.Enchantment;
-import cn.nukkit.utils.Dyes;
 import cn.nukkit.utils.Potions;
 
 import static cn.nukkit.item.randomitem.RandomItem.*;
@@ -36,7 +36,7 @@ public final class Fishing {
     public static final Selector JUNK_STRING_ITEM = putSelector(new ConstantItemSelector(Item.STRING, JUNKS), 0.06F);
     public static final Selector JUNK_WATTER_BOTTLE = putSelector(new ConstantItemSelector(Item.POTION, Potions.NO_EFFECTS, JUNKS), 0.12F);
     public static final Selector JUNK_BONE = putSelector(new ConstantItemSelector(Item.BONE, JUNKS), 0.12F);
-    public static final Selector JUNK_INK_SAC = putSelector(new ConstantItemSelector(Item.DYE, Dyes.BLACK, 10, JUNKS), 0.012F);
+    public static final Selector JUNK_INK_SAC = putSelector(new ConstantItemSelector(Item.DYE, Dye.BLACK, 10, JUNKS), 0.012F);
     public static final Selector JUNK_TRIPWIRE_HOOK = putSelector(new ConstantItemSelector(Item.TRIPWIRE_HOOK, JUNKS), 0.12F);
 
     public static Item getFishingResult(Item rod) {
