815181ef6fc3140a82bc832239b01dea6594307b
fromgate <fromgate@gmail.com>
2016-01-23 17:12:41 +0300

    Fix Location and position methods will return Location and Positions :) Added getBlock() method  to Position

--- src/main/java/cn/nukkit/level/Position.java
+++ src/main/java/cn/nukkit/level/Position.java
@@ -1,5 +1,6 @@
 package cn.nukkit.level;
 
+import cn.nukkit.block.Block;
 import cn.nukkit.math.Vector3;
 import cn.nukkit.utils.LevelException;
 
@@ -70,7 +71,6 @@ public class Position extends Vector3 {
         if (!this.isValid()) {
             throw new LevelException("Undefined Level reference");
         }
-
         return Position.fromObject(super.getSide(side, step), this.level);
     }
 
@@ -85,6 +85,91 @@ public class Position extends Vector3 {
         this.y = y;
         this.z = z;
         return this;
+    }
+
+    public Block getBlock(){
+        if (this.isValid()) return this.level.getBlock(this);
+        else throw new LevelException("Undefined Level reference");
+    }
+
+    public Location getLocation(){
+        if (this.isValid()) return new Location (this.x,this.y,this.z,0,0,this.level);
+        else throw new LevelException("Undefined Level reference");
+    }
+
+    @Override
+    public Position add(double x) {
+        return this.add(x, 0, 0);
+    }
+
+    @Override
+    public Position add(double x, double y) {
+        return this.add(x, y, 0);
+    }
+
+    @Override
+    public Position add(double x, double y, double z) {
+        return new Position(this.x + x, this.y + y, this.z + z,this.level);
+    }
+
+    @Override
+    public Position add(Vector3 x) {
+        return new Position(this.x + x.getX(), this.y + x.getY(), this.z + x.getZ(),this.level);
+    }
+
+    @Override
+    public Position subtract() {
+        return this.subtract(0, 0, 0);
+    }
+
+    @Override
+    public Position subtract(double x) {
+        return this.subtract(x, 0, 0);
+    }
+
+    @Override
+    public Position subtract(double x, double y) {
+        return this.subtract(x, y, 0);
+    }
+
+    @Override
+    public Position subtract(double x, double y, double z) {
+        return this.add(-x, -y, -z);
+    }
+
+    @Override
+    public Position subtract(Vector3 x) {
+        return this.add(-x.getX(), -x.getY(), -x.getZ());
+    }
+
+    @Override
+    public Position multiply(double number) {
+        return new Position(this.x * number, this.y * number, this.z * number,this.level);
+    }
+
+    @Override
+    public Position divide(double number) {
+        return new Position(this.x / number, this.y / number, this.z / number,this.level);
+    }
+
+    @Override
+    public Position ceil() {
+        return new Position ((int) Math.ceil(this.x), (int) Math.ceil(this.y), (int) Math.ceil(this.z),this.level);
+    }
+
+    @Override
+    public Position floor() {
+        return new Position(this.getFloorX(), this.getFloorY(), this.getFloorZ(),this.level);
+    }
+
+    @Override
+    public Position round() {
+        return new Position(Math.round(this.x), Math.round(this.y), Math.round(this.z),this.level);
+    }
+
+    @Override
+    public Position abs() {
+        return new Position((int) Math.abs(this.x), (int) Math.abs(this.y), (int) Math.abs(this.z),this.level);
     }
 
 }
