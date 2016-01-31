e3ccfbc22e8849b8f8e69788b774160f67eb1ee5
Pub4Game <Pub4Game@users.noreply.github.com>
2016-01-31 23:11:44 +0300

    Fixed bugs related to SnowBlock

--- src/main/java/cn/nukkit/block/SnowBlock.java
+++ src/main/java/cn/nukkit/block/SnowBlock.java
@@ -1,6 +1,5 @@
 package cn.nukkit.block;
 
-import cn.nukkit.Player;
 import cn.nukkit.item.Item;
 import cn.nukkit.item.Tool;
 import cn.nukkit.utils.BlockColor;
@@ -31,42 +30,20 @@ public class SnowBlock extends Solid {
     }
 
     @Override
+    public double getResistance() {
+        return 1;
+    }
+
+    @Override
     public int getToolType() {
         return Tool.TYPE_SHOVEL;
     }
 
     @Override
-    public boolean canBeReplaced() {
-        return true;
-    }
-
-    //TODO:雪片叠垒乐
-
-    @Override
-    public boolean place(Item item, Block block, Block target, int face, double fx, double fy, double fz, Player player) {
-        return super.place(item, block, target, face, fx, fy, fz, player);
-    }
-
-    /*
-        @Override
-        public int onUpdate(int type) {
-
-            if (type == Level.BLOCK_UPDATE_NORMAL) {
-                if (this.getSide(0).getId() == AIR) {
-                    this.getLevel().useBreakOn(this);
-
-                    return Level.BLOCK_UPDATE_NORMAL;
-                }
-            }
-
-            return 0;
-        }
-    */
-    @Override
     public int[][] getDrops(Item item) {
         if (item.isShovel()) {
             return new int[][]{
-                    {Item.SNOWBALL, 0, 8}
+                    {Item.SNOWBALL, 0, 4}
             };
         }
         return new int[][]{};
