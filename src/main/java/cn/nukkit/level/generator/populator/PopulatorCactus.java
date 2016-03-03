c919587c7cc654c74b2157ac5aede8f47485217c
Niall7459 <niall_lindsay@icloud.com>
2016-03-03 16:22:21 +0000

    Added base populators: cacti, dead bush, lily pad and mineshaft.

---
+++ src/main/java/cn/nukkit/level/generator/populator/PopulatorCactus.java
@@ -0,0 +1,14 @@
+package cn.nukkit.level.generator.populator;
+
+import cn.nukkit.level.ChunkManager;
+import cn.nukkit.math.NukkitRandom;
+
+public class PopulatorCactus extends Populator {
+
+	@Override
+	public void populate(ChunkManager level, int chunkX, int chunkZ, NukkitRandom random) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
