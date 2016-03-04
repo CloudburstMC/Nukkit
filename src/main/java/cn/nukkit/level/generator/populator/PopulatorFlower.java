56f744f0265dda8ea12e1b94cb0b36f721e13645
Niall7459 <niall_lindsay@icloud.com>
2016-03-04 15:57:47 +0000

    Added more flower populator types.

--- src/main/java/cn/nukkit/level/generator/populator/PopulatorFlower.java
+++ src/main/java/cn/nukkit/level/generator/populator/PopulatorFlower.java
@@ -1,18 +1,33 @@
 package cn.nukkit.level.generator.populator;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map.Entry;
+import java.util.Set;
+
 import cn.nukkit.block.Block;
+import cn.nukkit.block.BlockFlower;
 import cn.nukkit.level.ChunkManager;
 import cn.nukkit.math.NukkitMath;
 import cn.nukkit.math.NukkitRandom;
 
 /**
  * author: Angelic47
+ * contributer: Niall Lindsay <Niall7459>
+ * 
  * Nukkit Project
  */
+
 public class PopulatorFlower extends Populator {
+	
     private ChunkManager level;
     private int randomAmount;
     private int baseAmount;
+    
+    private List<int[]> flowerTypes = new ArrayList<int[]>();
 
     public void setRandomAmount(int randomAmount) {
         this.randomAmount = randomAmount;
@@ -21,24 +36,39 @@ public class PopulatorFlower extends Populator {
     public void setBaseAmount(int baseAmount) {
         this.baseAmount = baseAmount;
     }
+    
+    public void addType(int a, int b) {
+    	int[] c = new int[1]; c[0] = a; c[1] = b;
+    	this.flowerTypes.add(c);
+    }
+  
+    public List<int[]> getTypes() {
+    	return this.flowerTypes;
+    }
 
     @Override
     public void populate(ChunkManager level, int chunkX, int chunkZ, NukkitRandom random) {
         this.level = level;
         int amount = random.nextBoundedInt(this.randomAmount + 1) + this.baseAmount;
+        
+        if(flowerTypes.size() == 0){
+			this.addType(Block.RED_FLOWER, BlockFlower.TYPE_POPPY);
+			this.addType(Block.DANDELION, 0);
+		}
+        
+        int endNum = this.flowerTypes.size();
+        
         for (int i = 0; i < amount; ++i) {
             int x = NukkitMath.randomRange(random, chunkX * 16, chunkX * 16 + 15);
             int z = NukkitMath.randomRange(random, chunkZ * 16, chunkZ * 16 + 15);
             int y = this.getHighestWorkableBlock(x, z);
-
+            
+           
+        
             if (y != -1 && this.canFlowerStay(x, y, z)) {
-                if (random.nextBoolean()) {
-                    this.level.setBlockIdAt(x, y, z, Block.DANDELION);
-                    this.level.setBlockDataAt(x, y, z, 0);
-                } else {
-                    this.level.setBlockIdAt(x, y, z, Block.ROSE);
-                    this.level.setBlockDataAt(x, y, z, 0);
-                }
+            	 int[] type = getTypes().get(random.nextRange(0, endNum));
+            	 this.level.setBlockIdAt(x, y, z, type[0]);
+                 this.level.setBlockDataAt(x, y, z, type[1]);
             }
         }
     }
