4179dd12a2e7617270e38c177e138434246dcf3d
mepeisen <mep_eisen@web.de>
2016-03-11 09:37:57 +0100

    Making nukkit more robust for failing plugins

--- src/main/java/cn/nukkit/scheduler/ServerScheduler.java
+++ src/main/java/cn/nukkit/scheduler/ServerScheduler.java
@@ -114,7 +114,12 @@ public class ServerScheduler {
 
     public void cancelTask(int taskId) {
         if (taskMap.containsKey(taskId)) {
-            taskMap.remove(taskId).cancel();
+        	try {
+        		taskMap.remove(taskId).cancel();
+        	}
+        	catch (RuntimeException ex) {
+        		Server.getInstance().getLogger().critical("Exception while invoking onCancel " + ex.getMessage());
+        	}
         }
     }
 
@@ -125,14 +130,24 @@ public class ServerScheduler {
         for (Map.Entry<Integer, TaskHandler> entry : taskMap.entrySet()) {
             TaskHandler taskHandler = entry.getValue();
             if (plugin.equals(taskHandler.getPlugin())) {
-                taskHandler.cancel(); /* It will remove from task map automatic in next main heartbeat. */
+            	try {
+            		taskHandler.cancel(); /* It will remove from task map automatic in next main heartbeat. */
+            	}
+            	catch (RuntimeException ex) {
+            		Server.getInstance().getLogger().critical("Exception while invoking onCancel " + ex.getMessage());
+            	}
             }
         }
     }
 
     public void cancelAllTasks() {
         for (Map.Entry<Integer, TaskHandler> entry : this.taskMap.entrySet()) {
-            entry.getValue().cancel();
+        	try {
+        		entry.getValue().cancel();
+        	}
+        	catch (RuntimeException ex) {
+        		Server.getInstance().getLogger().critical("Exception while invoking onCancel " + ex.getMessage());
+        	}
         }
         this.taskMap.clear();
         this.queue.clear();
@@ -192,7 +207,12 @@ public class ServerScheduler {
                 taskHandler.setNextRunTick(currentTick + taskHandler.getPeriod());
                 pending.offer(taskHandler);
             } else {
-                taskMap.remove(taskHandler.getTaskId()).cancel();
+            	try {
+            		taskMap.remove(taskHandler.getTaskId()).cancel();
+            	}
+            	catch (RuntimeException ex) {
+            		Server.getInstance().getLogger().critical("Exception while invoking onCancel " + ex.getMessage());
+            	}
             }
         }
         AsyncTask.collectTask();
