7fbb35c8443d6c64861024ccbf6cbec7acfd4490
Pub4Game <Pub4Game@users.noreply.github.com>
2016-03-18 19:36:07 +0300

    Update PlayerFood.java

--- src/main/java/cn/nukkit/PlayerFood.java
+++ src/main/java/cn/nukkit/PlayerFood.java
@@ -140,7 +140,7 @@ public class PlayerFood {
             if (this.foodTickTimer >= 80) {
                 if (this.getPlayer().getHealth() < this.getPlayer().getMaxHealth()) {
                     EntityRegainHealthEvent ev = new EntityRegainHealthEvent(this.getPlayer(), 1, EntityRegainHealthEvent.CAUSE_EATING);
-                    this.getPlayer().heal(ev);
+                    this.getPlayer().heal(ev.getAmount(), ev);
                     //this.updateFoodExpLevel(3);
                 }
                 this.foodTickTimer = 0;
