04a2c2fdf40a026cbf25cfbf8fcb3c87d6088a78
Snake1999 <237356812@qq.com>
2016-04-01 22:36:18 +0800

    Small fix: Fixed "Lit redstone lamp doesn't have lightness"

--- src/main/java/cn/nukkit/block/BlockLitRedstoneLamp.java
+++ src/main/java/cn/nukkit/block/BlockLitRedstoneLamp.java
@@ -27,6 +27,11 @@ public class BlockLitRedstoneLamp extends BlockRedstoneLamp {
     }
 
     @Override
+    public int getLightLevel() {
+        return 15;
+    }
+
+    @Override
     public int onUpdate(int type) {
         if ((type == Level.BLOCK_UPDATE_NORMAL || type == Level.BLOCK_UPDATE_SCHEDULED) && this.getNeighborPowerLevel() <= 0) {
             this.getLevel().setBlock(this, new BlockRedstoneLamp());
