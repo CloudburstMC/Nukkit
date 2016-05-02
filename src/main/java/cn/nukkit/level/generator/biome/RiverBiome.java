6628f9090a98b440a86f581607e5a513fadf2742
Niall Lindsay <niall_lindsay@icloud.com>
2016-05-02 14:51:18 +0100

    Add sugarcane populator and tall sugarcanes populator (#748)
    * Add sugarcane populator base.
    
    * Add base tall sugarcane populator.
    
    * Add sugarcane + tall sugarcane populator functionality.
    
    * Finalize
    
    * Register populator to biomes.
    
    * Add credits (:
    
    * Update PopulatorTallSugarcane.java
    
    * Update PopulatorTallSugarcane.java

--- src/main/java/cn/nukkit/level/generator/biome/RiverBiome.java
+++ src/main/java/cn/nukkit/level/generator/biome/RiverBiome.java
@@ -1,7 +1,9 @@
 package cn.nukkit.level.generator.biome;
 
 import cn.nukkit.level.generator.populator.PopulatorGrass;
+import cn.nukkit.level.generator.populator.PopulatorSugarcane;
 import cn.nukkit.level.generator.populator.PopulatorTallGrass;
+import cn.nukkit.level.generator.populator.PopulatorTallSugarcane;
 
 /**
  * author: MagicDroidX
@@ -11,7 +13,12 @@ public class RiverBiome extends WateryBiome {
 
     public RiverBiome() {
         super();
-
+        
+        PopulatorSugarcane sugarcane = new PopulatorSugarcane();
+        sugarcane.setBaseAmount(6);
+        PopulatorTallSugarcane tallSugarcane = new PopulatorTallSugarcane();
+        tallSugarcane.setBaseAmount(60);
+        
         PopulatorGrass grass = new PopulatorGrass();
         grass.setBaseAmount(30);
         this.addPopulator(grass);
@@ -20,7 +27,9 @@ public class RiverBiome extends WateryBiome {
         tallGrass.setBaseAmount(5);
 
         this.addPopulator(tallGrass);
-
+        this.addPopulator(sugarcane);
+        this.addPopulator(tallSugarcane);
+        
         this.setElevation(58, 62);
 
         this.temperature = 0.5;
