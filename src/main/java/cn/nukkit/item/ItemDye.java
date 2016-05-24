fc8cdad463587d8c86b88c130c29d74003586200
Pub4Game <Pub4Game@users.noreply.github.com>
2016-05-24 21:06:20 +0300

    Update ItemDye.java

--- src/main/java/cn/nukkit/item/ItemDye.java
+++ src/main/java/cn/nukkit/item/ItemDye.java
@@ -20,22 +20,22 @@ public class ItemDye extends Item {
         super(DYE, meta, count, "Dye");
     }
 
-    public static final int BLACK = 0;
-    public static final int RED = 1;
-    public static final int GREEN = 2;
-    public static final int BROWN = 3;
-    public static final int BLUE = 4;
-    public static final int PURPLE = 5;
-    public static final int CYAN = 6;
-    public static final int SILVER = 7;
-    public static final int GRAY = 8;
-    public static final int PINK = 9;
-    public static final int LIME = 10;
-    public static final int YELLOW = 11;
-    public static final int LIGHT_BLUE = 12;
-    public static final int MAGENTA = 13;
-    public static final int ORANGE = 14;
-    public static final int WHITE = 15;
+    public static final int WHITE = 0;
+    public static final int ORANGE = 1;
+    public static final int MAGENTA = 2;
+    public static final int LIGHT_BLUE = 3;
+    public static final int YELLOW = 4;
+    public static final int LIME = 5;
+    public static final int PINK = 6;
+    public static final int GRAY = 7;
+    public static final int LIGHT_GRAY = 8;
+    public static final int CYAN = 9;
+    public static final int PURPLE = 10;
+    public static final int BLUE = 11;
+    public static final int BROWN = 12;
+    public static final int GREEN = 13;
+    public static final int RED = 14;
+    public static final int BLACK = 15;
 
     public static BlockColor getColor(int meta) {
         switch (meta & 0x0f) {
@@ -55,8 +55,8 @@ public class ItemDye extends Item {
                 return BlockColor.PINK_BLOCK_COLOR;
             case GRAY:
                 return BlockColor.GRAY_BLOCK_COLOR;
-            case SILVER:
-                return BlockColor.SILVER_BLOCK_COLOR;
+            case LIGHT_GRAY:
+                return BlockColor.LIGHT_GRAY_BLOCK_COLOR;
             case CYAN:
                 return BlockColor.CYAN_BLOCK_COLOR;
             case PURPLE:
@@ -71,7 +71,6 @@ public class ItemDye extends Item {
                 return BlockColor.RED_BLOCK_COLOR;
             case BLACK:
                 return BlockColor.BLACK_BLOCK_COLOR;
-
             default:
                 return BlockColor.WHITE_BLOCK_COLOR;
         }
@@ -79,22 +78,22 @@ public class ItemDye extends Item {
 
     public static String getColorName(int meta) {
         String[] names = new String[]{
-                "Black",
-                "Red",
-                "Green",
-                "Brown",
-                "Blue",
-                "Purple",
-                "Cyan",
-                "Silver",
-                "Gray",
-                "Pink",
-                "Lime",
-                "Yellow",
-                "Light Blue",
-                "Magenta",
+                "White",
                 "Orange",
-                "White"
+                "Magenta",
+                "Light Blue",
+                "Yellow",
+                "Lime",
+                "Pink",
+                "Gray",
+                "Light Gray",
+                "Cyan",
+                "Purple",
+                "Blue",
+                "Brown",
+                "Green",
+                "Red",
+                "Black",
         };
         return names[meta & 0x0f];
     }
