5db7b0f72b6500e7ea94196c23796a1908459051
jack <imjack@users.noreply.github.com>
2016-05-26 01:59:39 +0100

    Fix's TNT destroying liquids

--- src/main/java/cn/nukkit/block/BlockLiquid.java
+++ src/main/java/cn/nukkit/block/BlockLiquid.java
@@ -385,6 +385,11 @@ public abstract class BlockLiquid extends BlockTransparent {
     public double getHardness() {
         return 100;
     }
+    
+    @Override
+    public double getResistance() {
+    	return 500;
+    };
 
     private boolean[] getOptimalFlowDirections() {
         if (this.temporalVector == null) {
