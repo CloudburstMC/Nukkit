b6fb1ef868cfe4989ddb1f9fcbb25bc6cb747583
Pub4Game <Pub4Game@users.noreply.github.com>
2016-06-02 10:08:35 +0300

    Fixed a bug with removal of the cake from your inventory after you place it.
    To fix #799

--- src/main/java/cn/nukkit/item/ItemCake.java
+++ src/main/java/cn/nukkit/item/ItemCake.java
@@ -1,9 +1,6 @@
 package cn.nukkit.item;
 
-import cn.nukkit.Player;
-import cn.nukkit.block.Block;
 import cn.nukkit.block.BlockCake;
-import cn.nukkit.level.Level;
 
 /**
  * author: MagicDroidX
@@ -21,23 +18,11 @@ public class ItemCake extends Item {
 
     public ItemCake(Integer meta, int count) {
         super(CAKE, 0, count, "Cake");
+        this.block = new BlockCake();
     }
 
     @Override
     public int getMaxStackSize() {
         return 1;
     }
-
-    @Override
-    public boolean canBeActivated() {
-        return true;
-    }
-
-    @Override
-    public boolean onActivate(Level level, Player player, Block block, Block target, int face, double fx, double fy, double fz) {
-
-        level.setBlock(block, new BlockCake(), true);
-        return true;
-    }
-
 }
