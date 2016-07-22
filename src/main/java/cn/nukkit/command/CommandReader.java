41d0ecdc1d02635da121f821e706fe333f98128a
boybook <fangyizhou1997@qq.com>
2016-07-22 15:42:28 +0800

    Fixed #941 (#942)
    * We can interact blocks(like open the chest) in game mode 2!
    
    * Small change GenericParticle
    
    * Fix getClientId()
    
    close #938
    
    * Fixed incorrect behavior of the lever
    
    * Try to fix movement lags.
    
    * Code formatting.
    
    * Code cleanup and improvements
    
    * Fix example config.
    
    * Fix #941
    
    * Fix #941

--- src/main/java/cn/nukkit/command/CommandReader.java
+++ src/main/java/cn/nukkit/command/CommandReader.java
@@ -89,7 +89,7 @@ public class CommandReader extends Thread implements InterruptibleThread {
         this.running = false;
     }
 
-    public void stashLine() {
+    public synchronized void stashLine() {
         this.stashed = reader.getCursorBuffer().copy();
         try {
             reader.getOutput().write("\u001b[1G\u001b[K");
@@ -99,7 +99,7 @@ public class CommandReader extends Thread implements InterruptibleThread {
         }
     }
 
-    public void unstashLine() {
+    public synchronized void unstashLine() {
         try {
             reader.resetPromptLine("> ", this.stashed.toString(), this.stashed.cursor);
         } catch (IOException e) {
