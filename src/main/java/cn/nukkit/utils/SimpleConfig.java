b7c90c306202ca0a6edff1a806aadc922822ab65
Creeperface01 <honzaxp01@gmail.com>
2016-08-06 01:41:21 +0200

    Update SimpleConfig.java (#967)

--- src/main/java/cn/nukkit/utils/SimpleConfig.java
+++ src/main/java/cn/nukkit/utils/SimpleConfig.java
@@ -36,6 +36,10 @@ public abstract class SimpleConfig {
     }
 
     public boolean save() {
+        return save(false);
+    }
+
+    public boolean save(boolean async) {
         if (configFile.exists()) try {
             configFile.createNewFile();
         } catch (Exception e) {
@@ -51,7 +55,7 @@ public abstract class SimpleConfig {
                 return false;
             }
         }
-        cfg.save();
+        cfg.save(async);
         return true;
     }
 
