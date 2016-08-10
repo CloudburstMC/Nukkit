8ad47552d61db26e4cb3988776ad92faae3be7cf
ItsLucas <itslucas@itslucas.me>
2016-08-10 15:53:47 +0800

    add explosion (#656)
    add explosion

--- src/main/java/cn/nukkit/entity/item/EntityMinecartTNT.java
+++ src/main/java/cn/nukkit/entity/item/EntityMinecartTNT.java
@@ -1,5 +1,8 @@
 package cn.nukkit.entity.item;
 
+import cn.nukkit.block.Block;
+import cn.nukkit.event.entity.EntityExplosionPrimeEvent;
+import cn.nukkit.level.Explosion;
 import cn.nukkit.level.format.FullChunk;
 import cn.nukkit.nbt.tag.CompoundTag;
 
@@ -15,7 +18,26 @@ public class EntityMinecartTNT extends EntityMinecartEmpty {
         super(chunk, nbt);
     }
 
-    // TODO: 2016/1/30 on update, if activator rail downsiade, make a explosion
+    public boolean onUpdate(int currentTick) {
+        Block downSide = this.getLocation().floor().getLevelBlock();
+        if (downSide.getId() == Block.ACTIVATOR_RAIL && downSide.isPowered()) {
+            explode();
+            kill();
+        }
+        return true;
+    }
 
+    public void explode() {
+        EntityExplosionPrimeEvent event = new EntityExplosionPrimeEvent(this, 4);
+        server.getPluginManager().callEvent(event);
+        if (event.isCancelled()) {
+            return;
+        }
+        Explosion explosion = new Explosion(this, event.getForce(), this);
+        if (event.isBlockBreaking()) {
+            explosion.explodeA();
+        }
+        explosion.explodeB();
+    }
 
 }
