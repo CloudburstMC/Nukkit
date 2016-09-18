a83e7dd998f41dd7348b0ab08d72c273549ae009
Creeperface01 <honzaxp01@gmail.com>
2016-09-18 23:31:55 +0200

    Add double plant #1022

--- src/main/java/cn/nukkit/block/BlockDoublePlant.java
+++ src/main/java/cn/nukkit/block/BlockDoublePlant.java
@@ -1,8 +1,12 @@
 package cn.nukkit.block;
 
+import cn.nukkit.Player;
 import cn.nukkit.item.Item;
 import cn.nukkit.level.Level;
+import cn.nukkit.math.Vector3;
 import cn.nukkit.utils.BlockColor;
+
+import java.util.Vector;
 
 /**
  * Created on 2015/11/23 by xtypr.
@@ -62,8 +66,46 @@ public class BlockDoublePlant extends BlockFlowable {
     }
 
     @Override
+    public boolean place(Item item, Block block, Block target, int face, double fx, double fy, double fz, Player player) {
+        Block down = getSide(Vector3.SIDE_DOWN);
+        Block up = getSide(Vector3.SIDE_UP);
+
+        if (up.getId() == 0 && (down.getId() == GRASS || down.getId() == DIRT)) {
+            this.getLevel().setBlock(block, this, true);
+            this.getLevel().setBlock(up, new BlockDoublePlant(meta ^ 0x08), true);
+            return true;
+        }
+
+        return false;
+    }
+
+    @Override
+    public boolean onBreak(Item item) {
+        Block down = getSide(Vector3.SIDE_DOWN);
+        Block up = getSide(Vector3.SIDE_UP);
+
+        if ((this.meta & 0x08) == 0x08) { // top part
+            if (up.getId() == this.getId() && up.meta != 0x08) {
+                this.getLevel().setBlock(up, new BlockAir(), true, true);
+            } else if (down.getId() == this.getId() && down.meta != 0x08) {
+                this.getLevel().setBlock(down, new BlockAir(), true, true);
+            }
+        } else { // Bottom Part
+            if (up.getId() == this.getId() && (up.meta & 0x08) == 0x08) {
+                this.getLevel().setBlock(up, new BlockAir(), true, true);
+            } else if (down.getId() == this.getId() && (down.meta & 0x08) == 0x08) {
+                this.getLevel().setBlock(down, new BlockAir(), true, true);
+            }
+        }
+
+        return true;
+    }
+
+    @Override
     public int[][] getDrops(Item item) {
-        //todo
+        if ((this.meta & 0x08) != 0x08) {
+            return new int[][]{{DOUBLE_PLANT, this.meta, 1}};
+        }
 
         return new int[0][0];
     }
