1f008cded002082eab24b696700d198abbfca1dd
MrPowerGamerBR <leonardomalaman@gmail.com>
2016-10-04 18:49:20 -0300

    0.15.10 (#1082)

--- src/main/java/cn/nukkit/network/protocol/ProtocolInfo.java
+++ src/main/java/cn/nukkit/network/protocol/ProtocolInfo.java
@@ -9,7 +9,7 @@ public interface ProtocolInfo {
     /**
      * Actual Minecraft: PE protocol version
      */
-    byte CURRENT_PROTOCOL = 83;
+    byte CURRENT_PROTOCOL = 84;
 
     byte LOGIN_PACKET = (byte) 0x01;
     byte PLAY_STATUS_PACKET = (byte) 0x02;
