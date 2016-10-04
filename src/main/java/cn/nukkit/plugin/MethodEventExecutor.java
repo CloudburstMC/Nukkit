bffc83c5de9945b3985ae11648f3d77f23f30e17
MrPowerGamerBR <leonardomalaman@gmail.com>
2016-10-04 14:21:14 -0300

    Change TABs to spaces to match the rest of the class.

--- src/main/java/cn/nukkit/plugin/MethodEventExecutor.java
+++ src/main/java/cn/nukkit/plugin/MethodEventExecutor.java
@@ -20,16 +20,16 @@ public class MethodEventExecutor implements EventExecutor {
     }
 
     @SuppressWarnings("unchecked")
-	@Override
+    @Override
     public void execute(Listener listener, Event event) throws EventException {
         try {
-        	Class<Event>[] params = (Class<Event>[]) method.getParameterTypes();
-        	for (Class<Event> param : params) {
-        		if (param.isAssignableFrom(event.getClass())) {
-        			method.invoke(listener, event);
-        			break;
-        		}
-        	}
+            Class<Event>[] params = (Class<Event>[]) method.getParameterTypes();
+            for (Class<Event> param : params) {
+                if (param.isAssignableFrom(event.getClass())) {
+                    method.invoke(listener, event);
+                    break;
+                }
+            }
         } catch (InvocationTargetException ex) {
             throw new EventException(ex.getCause());
         } catch (Throwable t) {
