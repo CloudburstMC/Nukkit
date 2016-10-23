7279a544e04e5de72ea2744111449947a9450325
Creeperface01 <honzaxp01@gmail.com>
2016-10-23 18:02:35 +0200

    Fix IcePlains biome (#1187)
    * Fix IcePlains biome
    
    * Ooops

--- src/main/java/cn/nukkit/level/generator/biome/IcePlainsBiome.java
+++ src/main/java/cn/nukkit/level/generator/biome/IcePlainsBiome.java
@@ -1,28 +1,31 @@
 package cn.nukkit.level.generator.biome;
 
+import cn.nukkit.block.BlockSapling;
 import cn.nukkit.level.generator.populator.PopulatorTallGrass;
+import cn.nukkit.level.generator.populator.PopulatorTree;
 
 /**
  * author: MagicDroidX
  * Nukkit Project
  */
-public class IcePlainsBiome extends GrassyBiome {
+public class IcePlainsBiome extends SnowyBiome {
 
     public IcePlainsBiome() {
         super();
-
         PopulatorTallGrass tallGrass = new PopulatorTallGrass();
         tallGrass.setBaseAmount(5);
 
+        PopulatorTree trees = new PopulatorTree(BlockSapling.SPRUCE);
+        trees.setBaseAmount(1);
+        trees.setRandomAmount(1);
+
         this.addPopulator(tallGrass);
-
+        this.addPopulator(trees);
         this.setElevation(63, 74);
-
-        this.temperature = 0.05;
-        this.rainfall = 0.8;
+        this.temperature = 0D;
+        this.rainfall = 0.5D;
     }
 
-    @Override
     public String getName() {
         return "Ice Plains";
     }
