7e1bc8328266a2d58a048eef3648c41e699dc83f
Lin Mulan <admin@snake1999.com>
2016-11-05 21:44:20 +0800

    Sound fix for 0.16! Now noteblock songs are available (#1261)

--- src/main/java/cn/nukkit/network/protocol/LevelSoundEventPacket.java
+++ src/main/java/cn/nukkit/network/protocol/LevelSoundEventPacket.java
@@ -7,24 +7,33 @@ public class LevelSoundEventPacket extends DataPacket {
 
     public static final byte NETWORK_ID = ProtocolInfo.LEVEL_SOUND_EVENT_PACKET;
 
-    public byte sound;
+    // silence if case2==1
+    public static final int SOUND_BLOCK_PLACE = 1;
+    // block break sound when pitch==1, footstep when pitch==2. case1-> block id
+    public static final int SOUND_BLOCK_BREAK = 2;
+    // silence if case2==1
+    public static final int SOUND_BLOCK_FOOTSTEP = 4;
+    // When type -> SOUND_NOTE,  case1 -> instrument type, case2 -> pitch
+    public static final int SOUND_NOTE = 32;
+
+    public int type;
     public float x;
     public float y;
     public float z;
-    public int volume;
-    public int pitch;
+    public int case1;
+    public int case2;
     public boolean unknownBool;
     public boolean unknownBool2;
 
     @Override
     public void decode() {
-        this.sound = (byte) this.getByte();
+        this.type = this.getVarInt();
         Vector3f v = this.getVector3f();
         this.x = v.x;
         this.y = v.y;
         this.z = v.z;
-        this.volume = this.getVarInt();
-        this.pitch = this.getVarInt();
+        this.case1 = this.getVarInt();
+        this.case2 = this.getVarInt();
         this.unknownBool = this.getBoolean();
         this.unknownBool2 = this.getBoolean();
     }
@@ -32,10 +41,10 @@ public class LevelSoundEventPacket extends DataPacket {
     @Override
     public void encode() {
         this.reset();
-        this.putByte(this.sound);
+        this.putVarInt(this.type);
         this.putVector3f(this.x, this.y, this.z);
-        this.putVarInt(this.volume);
-        this.putVarInt(this.pitch);
+        this.putVarInt(this.case1);
+        this.putVarInt(this.case2);
         this.putBoolean(this.unknownBool);
         this.putBoolean(this.unknownBool2);
     }
