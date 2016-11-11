65b4cfc1dd160ec2427151199b70c925e1a3ab57
Creeperface01 <honzaxp01@gmail.com>
2016-11-11 14:31:01 +0100

    Achievements (#1269)
    * Fix allow-flight
    
    * Achievements base
    
    * Achievements!
    
    * hmmm

--- src/main/java/cn/nukkit/Player.java
+++ src/main/java/cn/nukkit/Player.java
@@ -35,10 +35,7 @@ import cn.nukkit.event.player.PlayerTeleportEvent.TeleportCause;
 import cn.nukkit.event.server.DataPacketReceiveEvent;
 import cn.nukkit.event.server.DataPacketSendEvent;
 import cn.nukkit.inventory.*;
-import cn.nukkit.item.Item;
-import cn.nukkit.item.ItemArrow;
-import cn.nukkit.item.ItemBlock;
-import cn.nukkit.item.ItemGlassBottle;
+import cn.nukkit.item.*;
 import cn.nukkit.item.enchantment.Enchantment;
 import cn.nukkit.item.food.Food;
 import cn.nukkit.lang.TextContainer;
@@ -56,10 +53,7 @@ import cn.nukkit.level.sound.LaunchSound;
 import cn.nukkit.math.*;
 import cn.nukkit.metadata.MetadataValue;
 import cn.nukkit.nbt.NBTIO;
-import cn.nukkit.nbt.tag.CompoundTag;
-import cn.nukkit.nbt.tag.DoubleTag;
-import cn.nukkit.nbt.tag.FloatTag;
-import cn.nukkit.nbt.tag.ListTag;
+import cn.nukkit.nbt.tag.*;
 import cn.nukkit.network.SourceInterface;
 import cn.nukkit.network.protocol.*;
 import cn.nukkit.permission.PermissibleBase;
@@ -129,7 +123,7 @@ public class Player extends EntityHuman implements CommandSender, InventoryHolde
 
     public boolean blocked = false;
 
-    //todo: achievements
+    public final HashSet<String> achievements = new HashSet<>();
 
     protected SimpleTransactionGroup currentTransaction = null;
 
@@ -525,6 +519,14 @@ public class Player extends EntityHuman implements CommandSender, InventoryHolde
 
     public boolean isPlayer() {
         return true;
+    }
+
+    public void removeAchievement(String achievementId) {
+        achievements.remove(achievementId);
+    }
+
+    public boolean hasAchievement(String achievementId) {
+        return achievements.contains(achievementId);
     }
 
     public boolean isConnected() {
@@ -1010,6 +1012,30 @@ public class Player extends EntityHuman implements CommandSender, InventoryHolde
         }
     }
 
+    public boolean awardAchievement(String achievementId) {
+        Achievement achievement = Achievement.achievements.get(achievementId);
+
+        if (achievement == null || hasAchievement(achievementId)) {
+            return false;
+        }
+
+        for (String id : achievement.requires) {
+            if (!this.hasAchievement(id)) {
+                return false;
+            }
+        }
+        PlayerAchievementAwardedEvent event = new PlayerAchievementAwardedEvent(this, achievementId);
+        this.server.getPluginManager().callEvent(event);
+
+        if (event.isCancelled()) {
+            return false;
+        }
+
+        this.achievements.add(achievementId);
+        achievement.broadcast(this);
+        return true;
+    }
+
     public int getGamemode() {
         return gamemode;
     }
@@ -1225,7 +1251,15 @@ public class Player extends EntityHuman implements CommandSender, InventoryHolde
                             continue;
                         }
 
-                        //todo: achievement
+                        switch (item.getId()) {
+                            case Item.WOOD:
+                            case Item.WOOD2:
+                                this.awardAchievement("mineWood");
+                                break;
+                            case Item.DIAMOND:
+                                this.awardAchievement("diamond");
+                                break;
+                        }
                         /*switch (item.getId()) {
                             case Item.WOOD:
                                 this.awardAchievement("mineWood");
@@ -1692,7 +1726,16 @@ public class Player extends EntityHuman implements CommandSender, InventoryHolde
             this.setLevel(level);
         }
 
-        //todo achievement
+        for (Tag achievement : nbt.getCompound("Achievements").getAllTags()) {
+            if (!(achievement instanceof ByteTag)) {
+                continue;
+            }
+
+            if (((ByteTag) achievement).getData() > 0) {
+                this.achievements.add(achievement.getName());
+            }
+        }
+
         nbt.putLong("lastPlayed", System.currentTimeMillis() / 1000);
 
         if (this.server.getAutoSave()) {
@@ -3157,7 +3200,41 @@ public class Player extends EntityHuman implements CommandSender, InventoryHolde
                             }
                         }
                     }
-                    //todo: achievement
+
+                    switch(recipe.getResult().getId()) {
+                        case Item.WORKBENCH:
+                            this.awardAchievement("buildWorkBench");
+                            break;
+                        case Item.WOODEN_PICKAXE:
+                            this.awardAchievement("buildPickaxe");
+                            break;
+                        case Item.FURNACE:
+                            this.awardAchievement("buildFurnace");
+                            break;
+                        case Item.WOODEN_HOE:
+                            this.awardAchievement("buildHoe");
+                            break;
+                        case Item.BREAD:
+                            this.awardAchievement("makeBread");
+                            break;
+                        case Item.CAKE:
+                            //TODO: detect complex recipes like cake that leave remains
+                            this.awardAchievement("bakeCake");
+                            this.inventory.addItem(new ItemBucket(0, 3));
+                            break;
+                        case Item.STONE_PICKAXE:
+                        case Item.GOLD_PICKAXE:
+                        case Item.IRON_PICKAXE:
+                        case Item.DIAMOND_PICKAXE:
+                            this.awardAchievement("buildBetterPickaxe");
+                            break;
+                        case Item.WOODEN_SWORD:
+                            this.awardAchievement("buildSword");
+                            break;
+                        case Item.DIAMOND:
+                            this.awardAchievement("diamond");
+                            break;
+                    }
 
                     break;
                 case ProtocolInfo.CONTAINER_SET_SLOT_PACKET:
@@ -3225,8 +3302,27 @@ public class Player extends EntityHuman implements CommandSender, InventoryHolde
                     this.currentTransaction.addTransaction(transaction);
 
                     if (this.currentTransaction.canExecute() || this.isCreative()) {
-                        //todo achievement
-                        this.currentTransaction.execute(this.isCreative());
+                        HashSet<String> achievements = new HashSet<>();
+
+                        for (Transaction tr : this.currentTransaction.getTransactions()) {
+                            Inventory inv = tr.getInventory();
+
+                            if (inv instanceof FurnaceInventory) {
+                                if (tr.getSlot() == 2) {
+                                    switch (((FurnaceInventory) inv).getResult().getId()) {
+                                        case Item.IRON_INGOT:
+                                            achievements.add("acquireIron");
+                                            break;
+                                    }
+                                }
+                            }
+                        }
+
+                        if (this.currentTransaction.execute(this.isCreative())) {
+                            for (String achievement : achievements) {
+                                this.awardAchievement(achievement);
+                            }
+                        }
 
                         this.currentTransaction = null;
                     } else {
@@ -3578,7 +3674,12 @@ public class Player extends EntityHuman implements CommandSender, InventoryHolde
                 this.namedTag.putInt("SpawnZ", (int) this.spawnPosition.z);
             }
 
-            //todo save achievement
+            CompoundTag achievements = new CompoundTag();
+            for (String achievement : this.achievements) {
+                achievements.putByte(achievement, 1);
+            }
+
+            this.namedTag.putCompound("Achievements", achievements);
 
             this.namedTag.putInt("playerGameType", this.gamemode);
             this.namedTag.putLong("lastPlayed", System.currentTimeMillis() / 1000);
