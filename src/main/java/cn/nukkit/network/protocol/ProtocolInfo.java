d32caffe903debc8186753465cdcc04a16d1a903
VentroxStudio <ventroxstudio@gmail.com>
2017-04-21 18:09:42 +0200

    1.0.7 update

--- src/main/java/cn/nukkit/network/protocol/ProtocolInfo.java
+++ src/main/java/cn/nukkit/network/protocol/ProtocolInfo.java
@@ -9,9 +9,9 @@ public interface ProtocolInfo {
     /**
      * Actual Minecraft: PE protocol version
      */
-    byte CURRENT_PROTOCOL = 106;
-    String MINECRAFT_VERSION = "v1.0.6";
-    String MINECRAFT_VERSION_NETWORK = "1.0.6";
+    byte CURRENT_PROTOCOL = 107;
+    String MINECRAFT_VERSION = "v1.0.7";
+    String MINECRAFT_VERSION_NETWORK = "1.0.7";
 
     byte LOGIN_PACKET = 0x01;
     byte PLAY_STATUS_PACKET = 0x02;
