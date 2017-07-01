d5e488424cc7c1af05fb003f391859b231dc0fb9
Scallop <862578730@qq.com>
2017-06-30 19:43:06 -0500

    Update to 1.1.2 (#1765)

--- src/main/java/cn/nukkit/network/protocol/ProtocolInfo.java
+++ src/main/java/cn/nukkit/network/protocol/ProtocolInfo.java
@@ -10,8 +10,8 @@ public interface ProtocolInfo {
      * Actual Minecraft: PE protocol version
      */
     byte CURRENT_PROTOCOL = 113;
-    String MINECRAFT_VERSION = "v1.1.0";
-    String MINECRAFT_VERSION_NETWORK = "1.1.0";
+    String MINECRAFT_VERSION = "v1.1.2";
+    String MINECRAFT_VERSION_NETWORK = "1.1.2";
 
     byte LOGIN_PACKET = 0x01;
     byte PLAY_STATUS_PACKET = 0x02;
