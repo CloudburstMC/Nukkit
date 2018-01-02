f216383c658ec46e0d0de6e543d61e77a50ab5fa
SupremeMortal <SupremeMortal@users.noreply.github.com>
2018-01-02 14:06:04 +0000

    Fixed lang unknown command issue in Server.

--- src/main/java/cn/nukkit/Server.java
+++ src/main/java/cn/nukkit/Server.java
@@ -652,7 +652,7 @@ public class Server {
             return true;
         }
 
-        sender.sendMessage(new TranslationContainer(TextFormat.RED + "%commands.generic.unknown", commandLine));
+        sender.sendMessage(new TranslationContainer(TextFormat.RED + "%commands.generic.notFound", commandLine));
 
         return false;
     }
