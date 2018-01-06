fcba84ae24460b0c9491c3bb58fe5d3c18dc9c69
CreeperFace <honzaxp01@gmail.com>
2018-01-06 08:51:46 +0100

    Register UpdateTtributesPacket

--- server/src/main/java/cn/nukkit/server/network/Network.java
+++ server/src/main/java/cn/nukkit/server/network/Network.java
@@ -292,6 +292,7 @@ public class Network {
         this.registerPacket(ProtocolInfo.START_GAME_PACKET, StartGamePacket.class);
         this.registerPacket(ProtocolInfo.TAKE_ITEM_ENTITY_PACKET, TakeItemEntityPacket.class);
         this.registerPacket(ProtocolInfo.TEXT_PACKET, TextPacket.class);
+        this.registerPacket(ProtocolInfo.UPDATE_ATTRIBUTES_PACKET, UpdateAttributesPacket.class);
         this.registerPacket(ProtocolInfo.UPDATE_BLOCK_PACKET, UpdateBlockPacket.class);
         this.registerPacket(ProtocolInfo.UPDATE_TRADE_PACKET, UpdateTradePacket.class);
     }
