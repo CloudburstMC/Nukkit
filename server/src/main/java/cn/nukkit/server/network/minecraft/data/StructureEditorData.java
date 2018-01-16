84d463ea382acfb63b923dd72c5745d88c1449b3
SupremeMortal <SupremeMortal@users.noreply.github.com>
2018-01-16 18:04:03 +0000

    Finally figured out all the fields in the StructureBlockUpdatePacket.

---
+++ server/src/main/java/cn/nukkit/server/network/minecraft/data/StructureEditorData.java
@@ -0,0 +1,17 @@
+package cn.nukkit.server.network.minecraft.data;
+
+import com.flowpowered.math.vector.Vector3i;
+import lombok.Value;
+
+@Value
+public class StructureEditorData {
+    private String unknown0;
+    private String metadata;
+    private Vector3i structureOffset;
+    private Vector3i structureSize;
+    private boolean includeEntities;
+    private boolean ignoringBlocks;
+    private boolean includePlayers;
+    private boolean showingAir;
+    private StructureSettings structureSettings;
+}
--- server/src/main/java/cn/nukkit/server/network/minecraft/packet/StructureBlockUpdatePacket.java
+++ server/src/main/java/cn/nukkit/server/network/minecraft/packet/StructureBlockUpdatePacket.java
@@ -2,38 +2,28 @@ package cn.nukkit.server.network.minecraft.packet;
 
 import cn.nukkit.server.network.NetworkPacketHandler;
 import cn.nukkit.server.network.minecraft.MinecraftPacket;
+import cn.nukkit.server.network.minecraft.data.StructureEditorData;
 import com.flowpowered.math.vector.Vector3i;
 import io.netty.buffer.ByteBuf;
 import lombok.Data;
 
 import static cn.nukkit.server.nbt.util.VarInt.writeUnsignedInt;
-import static cn.nukkit.server.network.minecraft.MinecraftUtil.writeBlockPosition;
+import static cn.nukkit.server.network.minecraft.MinecraftUtil.writeStructureEditorData;
+import static cn.nukkit.server.network.minecraft.MinecraftUtil.writeVector3i;
 
 @Data
 public class StructureBlockUpdatePacket implements MinecraftPacket {
     private Vector3i blockPosition;
     private int structureType;
-    //private StructureEditorData structureEditorData;
-    /*
-    StructureEditorData:
-    xStructureOffset
-    yStructureOffset
-    zStructureOffset
-    xStructureSize
-    yStructureSize
-    zStructureSize
-    ...
-    StructureSettings
-    TODO: Delve deeper into this mess.
-     */
+    private StructureEditorData structureEditorData;
     private boolean boundingBoxVisible;
     private boolean powered;
 
     @Override
     public void encode(ByteBuf buffer) {
-        writeBlockPosition(buffer, blockPosition);
+        writeVector3i(buffer, blockPosition);
         writeUnsignedInt(buffer, structureType);
-        //writeStructureEditorData(buffer, structureEditorData);
+        writeStructureEditorData(buffer, structureEditorData);
         buffer.writeBoolean(boundingBoxVisible);
         buffer.writeBoolean(powered);
     }
@@ -45,7 +35,6 @@ public class StructureBlockUpdatePacket implements MinecraftPacket {
 
     @Override
     public void handle(NetworkPacketHandler handler) {
-        // Only client bound.
     }
 
     public enum Type {
