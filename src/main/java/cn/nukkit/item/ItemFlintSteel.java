b3b624511b0515f6318e2e1dd4d47212f7d5ed8b
DaPorkchop_ <DaMatrix@users.noreply.github.com>
2018-02-25 15:40:48 +0100

    better portal detection algorithm
    - can detect portals without corners
    - doesn't only work on the Z axis

--- src/main/java/cn/nukkit/item/ItemFlintSteel.java
+++ src/main/java/cn/nukkit/item/ItemFlintSteel.java
@@ -13,7 +13,12 @@ import java.util.concurrent.ThreadLocalRandom;
  * Nukkit Project
  */
 public class ItemFlintSteel extends ItemTool {
-
+    /**
+     * The maximum possible size of the outside of a nether portal
+     * 23x23 in vanilla
+     */
+    private static final int MAX_PORTAL_SIZE = 23;
+    
     public ItemFlintSteel() {
         this(0, 1);
     }
@@ -34,54 +39,216 @@ public class ItemFlintSteel extends ItemTool {
     @Override
     public boolean onActivate(Level level, Player player, Block block, Block target, BlockFace face, double fx, double fy, double fz) {
         if (block.getId() == AIR && (target instanceof BlockSolid || target instanceof BlockSolidMeta)) {
+            PORTAL:
             if (target.getId() == OBSIDIAN) {
-                int targetX = target.getFloorX();
-                int targetY = target.getFloorY();
-                int targetZ = target.getFloorZ();
-                int x_max = targetX;
-                int x_min = targetX;
-                int x;
-                for (x = targetX + 1; level.getBlock(new Vector3(x, targetY, targetZ)).getId() == OBSIDIAN; x++) {
-                    x_max++;
-                }
-                for (x = targetX - 1; level.getBlock(new Vector3(x, targetY, targetZ)).getId() == OBSIDIAN; x--) {
-                    x_min--;
-                }
-                int count_x = x_max - x_min + 1;
-                int z_max = targetZ;
-                int z_min = targetZ;
-                int z;
-                for (z = targetZ + 1; level.getBlock(new Vector3(targetX, targetY, z)).getId() == OBSIDIAN; z++) {
-                    z_max++;
-                }
-                for (z = targetZ - 1; level.getBlock(new Vector3(targetX, targetY, z)).getId() == OBSIDIAN; z--) {
-                    z_min--;
-                }
-                int count_z = z_max - z_min + 1;
-                int z_max_y = targetY;
-                int z_min_y = targetY;
-                int y;
-                for (y = targetY; level.getBlock(new Vector3(targetX, y, z_max)).getId() == OBSIDIAN; y++) {
-                    z_max_y++;
-                }
-                for (y = targetY; level.getBlock(new Vector3(targetX, y, z_min)).getId() == OBSIDIAN; y++) {
-                    z_min_y++;
-                }
-                int y_max = Math.min(z_max_y, z_min_y) - 1;
-                int count_y = y_max - targetY + 2;
-                if ((count_x >= 4 && count_x <= 23 || count_z >= 4 && count_z <= 23) && count_y >= 5 && count_y <= 23) {
-                    int count_up = 0;
-                    for (int up_z = z_min; level.getBlock(new Vector3(targetX, y_max, up_z)).getId() == OBSIDIAN && up_z <= z_max; up_z++) {
-                        count_up++;
+                final int targX = target.getFloorX();
+                final int targY = target.getFloorY();
+                final int targZ = target.getFloorZ();
+                //check if there's air above (at least 3 blocks)
+                for (int i = 1; i < 4; i++) {
+                    if (level.getBlockIdAt(targX, targY + i, targZ) != AIR) {
+                        break PORTAL;
                     }
-                    if (count_up == count_z) {
-                        for (int block_z = z_min + 1; block_z < z_max; block_z++) {
-                            for (int block_y = targetY + 1; block_y < y_max; block_y++) {
-                                level.setBlock(new Vector3(targetX, block_y, block_z), new BlockNetherPortal());
+                }
+                int sizePosX = 0;
+                int sizeNegX = 0;
+                int sizePosZ = 0;
+                int sizeNegZ = 0;
+                for (int i = 1; i < MAX_PORTAL_SIZE; i++) {
+                    if (level.getBlockIdAt(targX + i, targY, targZ) == OBSIDIAN) {
+                        sizePosX++;
+                    } else {
+                        break;
+                    }
+                }
+                for (int i = 1; i < MAX_PORTAL_SIZE; i++) {
+                    if (level.getBlockIdAt(targX - i, targY, targZ) == OBSIDIAN) {
+                        sizeNegX++;
+                    } else {
+                        break;
+                    }
+                }
+                for (int i = 1; i < MAX_PORTAL_SIZE; i++) {
+                    if (level.getBlockIdAt(targX, targY, targZ + i) == OBSIDIAN) {
+                        sizePosZ++;
+                    } else {
+                        break;
+                    }
+                }
+                for (int i = 1; i < MAX_PORTAL_SIZE; i++) {
+                    if (level.getBlockIdAt(targX, targY, targZ - i) == OBSIDIAN) {
+                        sizeNegZ++;
+                    } else {
+                        break;
+                    }
+                }
+                //plus one for target block
+                int sizeX = sizePosX + sizeNegX + 1;
+                int sizeZ = sizePosZ + sizeNegZ + 1;
+                if (sizeX >= 2 && sizeX <= MAX_PORTAL_SIZE) {
+                    //start scan from 1 block above base
+                    //find pillar or end of portal to start scan
+                    int scanX = targX;
+                    int scanY = targY + 1;
+                    int scanZ = targZ;
+                    for (int i = 0; i < sizePosX + 1; i++) {
+                        //this must be air
+                        if (level.getBlockIdAt(scanX + i, scanY, scanZ) != AIR) {
+                            break PORTAL;
+                        }
+                        if (level.getBlockIdAt(scanX + i + 1, scanY, scanZ) == OBSIDIAN) {
+                            scanX += i;
+                            break;
+                        }
+                    }
+                    //make sure that the above loop finished
+                    if (level.getBlockIdAt(scanX + 1, scanY, scanZ) != OBSIDIAN) {
+                        break PORTAL;
+                    }
+
+                    int innerWidth = 0;
+                    LOOP: for (int i = 0; i < MAX_PORTAL_SIZE - 2; i++) {
+                        int id = level.getBlockIdAt(scanX - i, scanY, scanZ);
+                        switch (id) {
+                            case AIR:
+                                innerWidth++;
+                                break;
+                            case OBSIDIAN:
+                                break LOOP;
+                            default:
+                                break PORTAL;
+                        }
+                    }
+                    int innerHeight = 0;
+                    LOOP: for (int i = 0; i < MAX_PORTAL_SIZE - 2; i++) {
+                        int id = level.getBlockIdAt(scanX, scanY + i, scanZ);
+                        switch (id) {
+                            case AIR:
+                                innerHeight++;
+                                break;
+                            case OBSIDIAN:
+                                break LOOP;
+                            default:
+                                break PORTAL;
+                        }
+                    }
+                    if (!(innerWidth <= MAX_PORTAL_SIZE - 2
+                            && innerWidth >= 2
+                            && innerHeight <= MAX_PORTAL_SIZE - 2
+                            && innerHeight >= 3))   {
+                        break PORTAL;
+                    }
+
+                    for (int height = 0; height < innerHeight + 1; height++)    {
+                        if (height == innerHeight) {
+                            for (int width = 0; width < innerWidth; width++) {
+                                if (level.getBlockIdAt(scanX - width, scanY + height, scanZ) != OBSIDIAN) {
+                                    break PORTAL;
+                                }
+                            }
+                        } else {
+                            if (level.getBlockIdAt(scanX + 1, scanY + height, scanZ) != OBSIDIAN
+                                    || level.getBlockIdAt(scanX - innerWidth, scanY + height, scanZ) != OBSIDIAN) {
+                                break PORTAL;
+                            }
+
+                            for (int width = 0; width < innerWidth; width++) {
+                                if (level.getBlockIdAt(scanX - width, scanY + height, scanZ) != AIR) {
+                                    break PORTAL;
+                                }
                             }
                         }
-                        return true;
                     }
+
+                    for (int height = 0; height < innerHeight; height++)    {
+                        for (int width = 0; width < innerWidth; width++)    {
+                            level.setBlock(new Vector3(scanX - width, scanY + height, scanZ), new BlockNetherPortal());
+                        }
+                    }
+
+                    return true;
+                } else if (sizeZ >= 2 && sizeZ <= MAX_PORTAL_SIZE) {
+                    //start scan from 1 block above base
+                    //find pillar or end of portal to start scan
+                    int scanX = targX;
+                    int scanY = targY + 1;
+                    int scanZ = targZ;
+                    for (int i = 0; i < sizePosZ + 1; i++) {
+                        //this must be air
+                        if (level.getBlockIdAt(scanX, scanY, scanZ + i) != AIR) {
+                            break PORTAL;
+                        }
+                        if (level.getBlockIdAt(scanX, scanY, scanZ + i + 1) == OBSIDIAN) {
+                            scanZ += i;
+                            break;
+                        }
+                    }
+                    //make sure that the above loop finished
+                    if (level.getBlockIdAt(scanX, scanY, scanZ + 1) != OBSIDIAN) {
+                        break PORTAL;
+                    }
+
+                    int innerWidth = 0;
+                    LOOP: for (int i = 0; i < MAX_PORTAL_SIZE - 2; i++) {
+                        int id = level.getBlockIdAt(scanX, scanY, scanZ - i);
+                        switch (id) {
+                            case AIR:
+                                innerWidth++;
+                                break;
+                            case OBSIDIAN:
+                                break LOOP;
+                            default:
+                                break PORTAL;
+                        }
+                    }
+                    int innerHeight = 0;
+                    LOOP: for (int i = 0; i < MAX_PORTAL_SIZE - 2; i++) {
+                        int id = level.getBlockIdAt(scanX, scanY + i, scanZ);
+                        switch (id) {
+                            case AIR:
+                                innerHeight++;
+                                break;
+                            case OBSIDIAN:
+                                break LOOP;
+                            default:
+                                break PORTAL;
+                        }
+                    }
+                    if (!(innerWidth <= MAX_PORTAL_SIZE - 2
+                            && innerWidth >= 2
+                            && innerHeight <= MAX_PORTAL_SIZE - 2
+                            && innerHeight >= 3))   {
+                        break PORTAL;
+                    }
+
+                    for (int height = 0; height < innerHeight + 1; height++)    {
+                        if (height == innerHeight) {
+                            for (int width = 0; width < innerWidth; width++) {
+                                if (level.getBlockIdAt(scanX, scanY + height, scanZ - width) != OBSIDIAN) {
+                                    break PORTAL;
+                                }
+                            }
+                        } else {
+                            if (level.getBlockIdAt(scanX, scanY + height, scanZ + 1) != OBSIDIAN
+                                    || level.getBlockIdAt(scanX, scanY + height, scanZ - innerWidth) != OBSIDIAN) {
+                                break PORTAL;
+                            }
+
+                            for (int width = 0; width < innerWidth; width++) {
+                                if (level.getBlockIdAt(scanX, scanY + height, scanZ - width) != AIR) {
+                                    break PORTAL;
+                                }
+                            }
+                        }
+                    }
+
+                    for (int height = 0; height < innerHeight; height++)    {
+                        for (int width = 0; width < innerWidth; width++)    {
+                            level.setBlock(new Vector3(scanX, scanY + height, scanZ - width), new BlockNetherPortal());
+                        }
+                    }
+
+                    return true;
                 }
             }
             BlockFire fire = new BlockFire();
