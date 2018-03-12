77592416fd1fa228f28395efbef272c732b281aa
DaMatrix <joey.rabil@gmail.com>
2018-03-12 20:23:22 +0100

    fix some issues reported by NycuRO

--- src/main/java/cn/nukkit/level/biome/impl/swamp/SwampBiome.java
+++ src/main/java/cn/nukkit/level/biome/impl/swamp/SwampBiome.java
@@ -3,8 +3,10 @@ package cn.nukkit.level.biome.impl.swamp;
 import cn.nukkit.block.Block;
 import cn.nukkit.block.BlockFlower;
 import cn.nukkit.level.biome.type.GrassyBiome;
+import cn.nukkit.level.generator.populator.impl.MushroomPopulator;
 import cn.nukkit.level.generator.populator.impl.PopulatorFlower;
 import cn.nukkit.level.generator.populator.impl.PopulatorLilyPad;
+import cn.nukkit.level.generator.populator.impl.PopulatorSmallMushroom;
 import cn.nukkit.level.generator.populator.impl.tree.SwampTreePopulator;
 
 /**
@@ -29,6 +31,16 @@ public class SwampBiome extends GrassyBiome {
         flower.setBaseAmount(2);
         flower.addType(Block.RED_FLOWER, BlockFlower.TYPE_BLUE_ORCHID);
         this.addPopulator(flower);
+
+        MushroomPopulator mushroom = new MushroomPopulator(1);
+        mushroom.setBaseAmount(-5);
+        mushroom.setRandomAmount(7);
+        this.addPopulator(mushroom);
+
+        PopulatorSmallMushroom smallMushroom = new PopulatorSmallMushroom();
+        smallMushroom.setBaseAmount(0);
+        smallMushroom.setRandomAmount(2);
+        this.addPopulator(smallMushroom);
 
         this.setBaseHeight(-0.2f);
         this.setHeightVariation(0.1f);
