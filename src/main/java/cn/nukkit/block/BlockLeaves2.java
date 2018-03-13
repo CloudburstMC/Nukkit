80ef21d94763006a3a4a3cb3d67f10767f0f27aa
root <root@makk-mail.ml>
2018-03-13 20:15:16 +0100

    Removed debug

--- src/main/java/cn/nukkit/block/BlockLeaves2.java
+++ src/main/java/cn/nukkit/block/BlockLeaves2.java
@@ -136,10 +136,7 @@ private Boolean findLog(Block pos, LongSet visited, Integer distance, Integer ch
                     getLevel().useBreakOn(this);
                     return Level.BLOCK_UPDATE_NORMAL;
                 }
-            }else{ 
-            int i = getDamage() & 0b00001100;
-            System.out.println(i+"");}
-        }
+            }
         return 0;
     }
 }
