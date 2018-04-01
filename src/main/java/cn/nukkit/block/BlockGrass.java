824eed6d867b1ce7242929a7e6c49fee0530b08b
Petteri <petteri.malkavaara1@gmail.com>
2018-04-01 16:38:15 +0300

    Fix grass growing underwater

--- src/main/java/cn/nukkit/block/BlockGrass.java
+++ src/main/java/cn/nukkit/block/BlockGrass.java
@@ -77,7 +77,7 @@ public class BlockGrass extends BlockDirt {
             z = random.nextRange((int) z - 1, (int) z + 1);
             Block block = this.getLevel().getBlock(new Vector3(x, y, z));
             if (block.getId() == Block.DIRT) {
-                if (block.up() instanceof BlockTransparent) {
+                if (block.up() instanceof BlockAir) {
                     BlockSpreadEvent ev = new BlockSpreadEvent(block, this, new BlockGrass());
                     Server.getInstance().getPluginManager().callEvent(ev);
                     if (!ev.isCancelled()) {
