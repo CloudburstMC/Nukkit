a491fb0aaf4e864b3cc104538ca551c9fe29a4ee
Reece Mackie <20544390+Rover656@users.noreply.github.com>
2018-08-07 18:06:11 +0100

    Fix shulker boxes not saving items (#302)
    * Fix shulker boxes not saving items
    
    * Fix issues with placing undyed shulker boxes
    
    * Formatting changes

---
+++ src/main/java/cn/nukkit/block/BlockUndyedShulkerBox.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cn.nukkit.block;
+
+
+/**
+ *
+ * @author Reece Mackie
+ */
+public class BlockUndyedShulkerBox extends BlockShulkerBox {
+    
+    @Override
+    public int getId() {
+        return UNDYED_SHULKER_BOX;
+    }
+}
