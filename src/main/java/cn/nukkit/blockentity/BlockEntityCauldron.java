12597d56f9adf02dfb882dcb5ec04100b5b483a8
Rover656 <Rover656@users.noreply.github.com>
2018-08-12 19:25:59 +0100

    Put empty line back

--- src/main/java/cn/nukkit/blockentity/BlockEntityCauldron.java
+++ src/main/java/cn/nukkit/blockentity/BlockEntityCauldron.java
@@ -3,6 +3,7 @@ package cn.nukkit.blockentity;
 import cn.nukkit.block.Block;
 import cn.nukkit.level.format.FullChunk;
 import cn.nukkit.nbt.tag.CompoundTag;
+
 import cn.nukkit.utils.BlockColor;
 
 /**
