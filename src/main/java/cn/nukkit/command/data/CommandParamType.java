040b0da004789bb604c2316fc6ffead2ae0a395e
SupremeMortal <SupremeMortal@users.noreply.github.com>
2018-08-17 18:43:15 +0100

    Fix command params

--- src/main/java/cn/nukkit/command/data/CommandParamType.java
+++ src/main/java/cn/nukkit/command/data/CommandParamType.java
@@ -1,24 +1,24 @@
 package cn.nukkit.command.data;
 
+import static cn.nukkit.network.protocol.AvailableCommandsPacket.*;
+
 /**
  * @author CreeperFace
  */
 public enum CommandParamType {
-    INT(0x01),
-    FLOAT(0x02),
-    VALUE(0x03),
-    TARGET(0x04),
-
-    STRING(0x0d),
-    POSITION(0x0e),
-
-    RAWTEXT(0x11),
-
-    TEXT(0x13),
-
-    JSON(0x16),
-
-    COMMAND(0x1d);
+    INT(ARG_TYPE_INT),
+    FLOAT(ARG_TYPE_FLOAT),
+    VALUE(ARG_TYPE_VALUE),
+    WILDCARD_INT(ARG_TYPE_WILDCARD_INT),
+    TARGET(ARG_TYPE_TARGET),
+    WILDCARD_TARGET(ARG_TYPE_WILDCARD_TARGET),
+    STRING(ARG_TYPE_STRING),
+    POSITION(ARG_TYPE_POSITION),
+    MESSAGE(ARG_TYPE_MESSAGE),
+    RAWTEXT(ARG_TYPE_RAWTEXT),
+    JSON(ARG_TYPE_JSON),
+    TEXT(ARG_TYPE_MESSAGE), // backwards compatibility
+    COMMAND(ARG_TYPE_COMMAND);
 
     private final int id;
 
