1a646c9c02c1900f49351f8c0a2d1554ef41229c
Reece Mackie <20544390+Rover656@users.noreply.github.com>
2018-08-19 18:52:23 +0100

    Fix gate drops (#331)
    * Fix gate drops
    
    * Change to override toItem
    
    * Fix unnecessary change

--- src/main/java/cn/nukkit/block/BlockFenceGateJungle.java
+++ src/main/java/cn/nukkit/block/BlockFenceGateJungle.java
@@ -1,5 +1,7 @@
 package cn.nukkit.block;
 
+import cn.nukkit.item.Item;
+
 /**
  * Created on 2015/11/23 by xtypr.
  * Package cn.nukkit.block in project Nukkit .
@@ -21,5 +23,10 @@ public class BlockFenceGateJungle extends BlockFenceGate {
     @Override
     public String getName() {
         return "Jungle Fence Gate";
+    }
+
+    @Override
+    public Item toItem() {
+        return Item.get(Item.FENCE_GATE_JUNGLE, 0, 1);
     }
 }
--- src/main/java/cn/nukkit/block/BlockFenceNetherBrick.java
+++ src/main/java/cn/nukkit/block/BlockFenceNetherBrick.java
@@ -68,5 +68,4 @@ public class BlockFenceNetherBrick extends BlockFence {
     public boolean canHarvestWithHand() {
         return false;
     }
-
 }
