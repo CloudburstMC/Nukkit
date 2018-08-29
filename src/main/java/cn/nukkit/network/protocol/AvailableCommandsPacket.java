8f9b2bfe4410348d0f206230842430f8bc07197a
SupremeMortal <SupremeMortal@users.noreply.github.com>
2018-08-29 14:38:43 +0100

    Fix AvailableCommand arg types

--- src/main/java/cn/nukkit/network/protocol/AvailableCommandsPacket.java
+++ src/main/java/cn/nukkit/network/protocol/AvailableCommandsPacket.java
@@ -21,16 +21,14 @@ public class AvailableCommandsPacket extends DataPacket {
     public static final int ARG_TYPE_FLOAT = 0x02;
     public static final int ARG_TYPE_VALUE = 0x03;
     public static final int ARG_TYPE_WILDCARD_INT = 0x04;
-    public static final int ARG_TYPE_TARGET = 0x05;
-    public static final int ARG_TYPE_WILDCARD_TARGET = 0x06;
-
-    public static final int ARG_TYPE_STRING = 0x0f;
-    public static final int ARG_TYPE_POSITION = 0x10;
-
-    public static final int ARG_TYPE_MESSAGE = 0x13;
-    public static final int ARG_TYPE_RAWTEXT = 0x15;
-    public static final int ARG_TYPE_JSON = 0x18;
-    public static final int ARG_TYPE_COMMAND = 0x1f;
+    public static final int ARG_TYPE_OPERATOR = 0x05;
+    public static final int ARG_TYPE_TARGET = 0x06;
+    public static final int ARG_TYPE_STRING = 0x14;
+    public static final int ARG_TYPE_POSITION = 0x1a;
+    public static final int ARG_TYPE_MESSAGE = 0x1c;
+    public static final int ARG_TYPE_TEXT = 0x1f;
+    public static final int ARG_TYPE_JSON = 0x20;
+    public static final int ARG_TYPE_COMMAND = 0x23;
 
     public static final int ARG_FLAG_ENUM = 0x200000;
     public static final int ARG_FLAG_POSTFIX = 0x1000000;
@@ -121,7 +119,7 @@ public class AvailableCommandsPacket extends DataPacket {
                     if ((type & ARG_FLAG_ENUM) != 0) {
                         int index = type & 0xffff;
                         parameter.enumData = enums.get(index);
-                    } else if ((type & ARG_FLAG_VALID) == 0) {
+                    } else if ((type & ARG_FLAG_POSTFIX) != 0) {
                         parameter.postFix = postFixes.get(type & 0xffff);
                     }
 
@@ -244,9 +242,9 @@ public class AvailableCommandsPacket extends DataPacket {
                         }
 
 
-                        type = (ARG_FLAG_VALID | parameter.type.getId()) << 24 | i;
+                        type = ARG_FLAG_POSTFIX | i | ARG_FLAG_VALID ;
                     } else {
-                        type = parameter.type.getId() | ARG_FLAG_VALID;
+                        type = parameter.type.getId() | ARG_FLAG_VALID ;
                     }
 
                     putLInt(type);
