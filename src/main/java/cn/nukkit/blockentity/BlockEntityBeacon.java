17ebaa53902faef4587f04d6c7c5ee818b46b28a
Reece Mackie <20544390+Rover656@users.noreply.github.com>
2018-09-03 13:53:53 +0100

    fix another silly error

--- src/main/java/cn/nukkit/blockentity/BlockEntityBeacon.java
+++ src/main/java/cn/nukkit/blockentity/BlockEntityBeacon.java
@@ -158,7 +158,7 @@ public class BlockEntityBeacon extends BlockEntitySpawnable implements Inventory
         //Check every block from our y coord to the top of the world
         for (int y = tileY + 1; y <= 255; y++) {
             int testBlockId = level.getBlockIdAt(tileX, y, tileZ);
-            if (!testBlockId != Block.AIR) {
+            if (testBlockId != Block.AIR) {
                 //There is no sky access
                 return false;
             }
