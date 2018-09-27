371589bff50e6daccff284b5c8fb62f3ef0fff13
Petteri <petteri.malkavaara1@gmail.com>
2018-09-27 16:54:58 +0300

    Update EntityMob.java

--- src/main/java/cn/nukkit/entity/mob/EntityMob.java
+++ src/main/java/cn/nukkit/entity/mob/EntityMob.java
@@ -1,6 +1,7 @@
 package cn.nukkit.entity.mob;
 
 import cn.nukkit.entity.EntityCreature;
+import cn.nukkit.item.Item;
 import cn.nukkit.level.format.FullChunk;
 import cn.nukkit.nbt.tag.CompoundTag;
 
@@ -12,5 +13,18 @@ public abstract class EntityMob extends EntityCreature {
 
     public EntityMob(FullChunk chunk, CompoundTag nbt) {
         super(chunk, nbt);
+    }
+
+    @Override
+    public boolean onInteract(Player player, Item item) {
+        if (item.getId() == Item.NAME_TAG) {
+            if (item.hasCustomName()) {
+                this.setNameTag(item.getCustomName());
+                this.setNameTagVisible(true);
+                player.getInventory().removeItem(item);
+                return true;
+            }
+        }
+        return false;
     }
 }
