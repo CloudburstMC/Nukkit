755d9274ac1ff02ba909cb90a60e32aa742f855c
Creeperface01 <honzaxp01@gmail.com>
2018-10-27 10:21:00 +0200

    Fixed redstone torch (#446)

--- src/main/java/cn/nukkit/Player.java
+++ src/main/java/cn/nukkit/Player.java
@@ -3046,7 +3046,18 @@ public class Player extends EntityHuman implements CommandSender, InventoryHolde
                                         }
                                     }
 
-                                    EntityDamageByEntityEvent entityDamageByEntityEvent = new EntityDamageByEntityEvent(this, target, DamageCause.ENTITY_ATTACK, damage);
+                                    float knockback = 0.8f;
+
+                                    Enchantment kb = this.inventory.getItemInHand().getEnchantment(Enchantment.ID_KNOCKBACK);
+                                    if (kb != null) {
+                                        knockback += kb.getLevel();
+                                    }
+
+                                    if (this.isSprinting()) {
+                                        knockback++;
+                                    }
+
+                                    EntityDamageByEntityEvent entityDamageByEntityEvent = new EntityDamageByEntityEvent(this, target, DamageCause.ENTITY_ATTACK, damage, knockback * 0.5f);
                                     if (this.isSpectator()) entityDamageByEntityEvent.setCancelled();
                                     if ((target instanceof Player) && !this.level.getGameRules().getBoolean(GameRule.PVP)) {
                                         entityDamageByEntityEvent.setCancelled();
