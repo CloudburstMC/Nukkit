339eff0d5ad57dbee681fb9bb248ff253fb53c02
Petteri <petteri.malkavaara1@gmail.com>
2018-12-08 17:57:42 +0200

    Fix trapdoor redstone (#486)
    * Fix trapdoor redstone
    
    * Update BlockTrapdoor.java

--- src/main/java/cn/nukkit/block/BlockTrapdoor.java
+++ src/main/java/cn/nukkit/block/BlockTrapdoor.java
@@ -161,9 +161,12 @@ public class BlockTrapdoor extends BlockTransparentMeta {
 
     @Override
     public int onUpdate(int type) {
-        if (type == Level.BLOCK_UPDATE_REDSTONE || type == Level.BLOCK_UPDATE_NORMAL) {
+        if (type == Level.BLOCK_UPDATE_REDSTONE) {
             if ((!isOpen() && this.level.isBlockPowered(this.getLocation())) || (isOpen() && !this.level.isBlockPowered(this.getLocation()))) {
                 this.level.getServer().getPluginManager().callEvent(new BlockRedstoneEvent(this, isOpen() ? 15 : 0, isOpen() ? 0 : 15));
+                this.setDamage(this.getDamage() ^ 0x08);
+                this.level.setBlock(this, this, true);
+                this.level.addSound(this, isOpen() ? Sound.RANDOM_DOOR_OPEN : Sound.RANDOM_DOOR_CLOSE);
                 return type;
             }
         }
