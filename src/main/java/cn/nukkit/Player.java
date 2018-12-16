63bfb3adba46ce403c66f10b6797829b18187691
Petteri <petteri.malkavaara1@gmail.com>
2018-12-16 13:12:01 +0200

    Enable eating sounds and particles (#354)

--- src/main/java/cn/nukkit/Player.java
+++ src/main/java/cn/nukkit/Player.java
@@ -2648,8 +2648,8 @@ public class Player extends EntityHuman implements CommandSender, InventoryHolde
                                 break;
                             }
 
-                            /*this.dataPacket(packet); //bug?
-                            Server.broadcastPacket(this.getViewers().values(), packet);*/
+                            this.dataPacket(packet);
+                            Server.broadcastPacket(this.getViewers().values(), packet);
                             break;
                     }
                     break;
