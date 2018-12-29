2bff06cb7c53d82afc5b0c2038d81c8375e9caaa
SupremeMortal <SupremeMortal@users.noreply.github.com>
2018-12-29 16:11:11 +0000

    Fix Container Saving (#515)
    * Set default value to true for autoSave
    
    * Mark block entity as dirty if chest has been changed.

--- src/main/java/cn/nukkit/Server.java
+++ src/main/java/cn/nukkit/Server.java
@@ -139,7 +139,7 @@ public class Server {
 
     private int maxPlayers;
 
-    private boolean autoSave;
+    private boolean autoSave = true;
 
     private RCON rcon;
 
--- src/main/java/cn/nukkit/level/Level.java
+++ src/main/java/cn/nukkit/level/Level.java
@@ -198,7 +198,7 @@ public class Level implements ChunkManager, Metadatable {
     private int chunkGenerationQueueSize = 8;
     private int chunkPopulationQueueSize = 2;
 
-    private boolean autoSave = true;
+    private boolean autoSave;
 
     private BlockMetadataStore blockMetadata;
 
