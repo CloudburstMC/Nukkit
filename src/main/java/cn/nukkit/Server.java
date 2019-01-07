7654610b81f37eb378e43eb286ce6a7b172d0ddd
Wode <17339354+wode490390@users.noreply.github.com>
2019-01-07 09:41:33 +0800

    Fix Noteblock

--- src/main/java/cn/nukkit/Server.java
+++ src/main/java/cn/nukkit/Server.java
@@ -2104,6 +2104,7 @@ public class Server {
         BlockEntity.registerBlockEntity(BlockEntity.JUKEBOX, BlockEntityJukebox.class);
         BlockEntity.registerBlockEntity(BlockEntity.SHULKER_BOX, BlockEntityShulkerBox.class);
         BlockEntity.registerBlockEntity(BlockEntity.BANNER, BlockEntityBanner.class);
+        BlockEntity.registerBlockEntity(BlockEntity.MUSIC, BlockEntityNoteBlock.class);
     }
 
     public boolean isNetherAllowed() {
