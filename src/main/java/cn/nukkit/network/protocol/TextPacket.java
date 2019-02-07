434b7820f508ce8b545f3db87c802d39653d47da
SupremeMortal <SupremeMortal@users.noreply.github.com>
2019-02-07 23:13:37 +0000

    Fix 1.9 crashes

--- src/main/java/cn/nukkit/network/protocol/TextPacket.java
+++ src/main/java/cn/nukkit/network/protocol/TextPacket.java
@@ -16,10 +16,12 @@ public class TextPacket extends DataPacket {
     public static final byte TYPE_CHAT = 1;
     public static final byte TYPE_TRANSLATION = 2;
     public static final byte TYPE_POPUP = 3;
-    public static final byte TYPE_TIP = 4;
-    public static final byte TYPE_SYSTEM = 5;
-    public static final byte TYPE_WHISPER = 6;
-    public static final byte TYPE_ANNOUNCEMENT = 7;
+    public static final byte TYPE_JUKEBOX_POPUP = 4;
+    public static final byte TYPE_TIP = 5;
+    public static final byte TYPE_SYSTEM = 6;
+    public static final byte TYPE_WHISPER = 7;
+    public static final byte TYPE_ANNOUNCEMENT = 8;
+    public static final byte TYPE_JSON = 9;
 
     public byte type;
     public String source = "";
@@ -41,10 +43,12 @@ public class TextPacket extends DataPacket {
             case TYPE_RAW:
             case TYPE_TIP:
             case TYPE_SYSTEM:
+            case TYPE_JSON:
                 this.message = this.getString();
                 break;
 
             case TYPE_TRANSLATION:
+            case TYPE_JUKEBOX_POPUP:
                 this.message = this.getString();
                 int count = (int) this.getUnsignedVarInt();
                 this.parameters = new String[count];
@@ -69,10 +73,12 @@ public class TextPacket extends DataPacket {
             case TYPE_RAW:
             case TYPE_TIP:
             case TYPE_SYSTEM:
+            case TYPE_JSON:
                 this.putString(this.message);
                 break;
 
             case TYPE_TRANSLATION:
+            case TYPE_JUKEBOX_POPUP:
                 this.putString(this.message);
                 this.putUnsignedVarInt(this.parameters.length);
                 for (String parameter : this.parameters) {
