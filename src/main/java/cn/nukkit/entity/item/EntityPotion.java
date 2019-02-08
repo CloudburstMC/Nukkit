b09cae5a1883e7a6a2654f7ffcf84212911e46b8
Aaman <38054324+Vrysed@users.noreply.github.com>
2019-02-08 00:00:57 -0800

    Improvements for Splash Potions
    Improved Gravity and Radius

--- src/main/java/cn/nukkit/entity/item/EntityPotion.java
+++ src/main/java/cn/nukkit/entity/item/EntityPotion.java
@@ -76,7 +76,7 @@ public class EntityPotion extends EntityProjectile {
 
     @Override
     protected float getGravity() {
-        return 0.1f;
+        return 0.05f;
     }
 
     @Override
@@ -133,7 +133,7 @@ public class EntityPotion extends EntityProjectile {
         this.getLevel().addParticle(particle);
         this.getLevel().addLevelSoundEvent(this, LevelSoundEventPacket.SOUND_GLASS);
 
-        Entity[] entities = this.getLevel().getNearbyEntities(this.getBoundingBox().grow(8.25, 4.24, 8.25));
+        Entity[] entities = this.getLevel().getNearbyEntities(this.getBoundingBox().grow(4.25, 2.24, 4.25));
         for (Entity anEntity : entities) {
             double distance = anEntity.distanceSquared(this);
 
