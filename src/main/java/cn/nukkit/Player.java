11f9d4f85d3d4020ba3581c4b73157150eb4eab7
Wode <17339354+wode490390@users.noreply.github.com>
2019-02-16 02:24:34 +0800

    Implement DragonEgg teleporting (#584)
    * Implement DragonEgg teleporting
    
    * touch
    
    * LevelEventPacket
    
    * vanilla

--- src/main/java/cn/nukkit/Player.java
+++ src/main/java/cn/nukkit/Player.java
@@ -2321,6 +2321,7 @@ public class Player extends EntityHuman implements CommandSender, InventoryHolde
                     Vector3 pos = new Vector3(playerActionPacket.x, playerActionPacket.y, playerActionPacket.z);
                     BlockFace face = BlockFace.fromIndex(playerActionPacket.face);
 
+                    actionswitch:
                     switch (playerActionPacket.action) {
                         case PlayerActionPacket.ACTION_START_BREAK:
                             long currentBreak = System.currentTimeMillis();
@@ -2336,9 +2337,13 @@ public class Player extends EntityHuman implements CommandSender, InventoryHolde
                                 this.inventory.sendHeldItem(this);
                                 break;
                             }
-                            if (target.getId() == Block.NOTEBLOCK) {
-                                ((BlockNoteblock) target).emitSound();
-                                break;
+                            switch (target.getId()) {
+                                case Block.NOTEBLOCK:
+                                    ((BlockNoteblock) target).emitSound();
+                                    break actionswitch;
+                                case Block.DRAGON_EGG:
+                                    ((BlockDragonEgg) target).teleport();
+                                    break actionswitch;
                             }
                             Block block = target.getSide(face);
                             if (block.getId() == Block.FIRE) {
--- src/main/java/cn/nukkit/block/BlockDragonEgg.java
+++ src/main/java/cn/nukkit/block/BlockDragonEgg.java
@@ -1,6 +1,9 @@
 package cn.nukkit.block;
 
+import cn.nukkit.level.Level;
+import cn.nukkit.network.protocol.LevelEventPacket;
 import cn.nukkit.utils.BlockColor;
+import java.util.concurrent.ThreadLocalRandom;
 
 public class BlockDragonEgg extends BlockFallable {
 
@@ -40,5 +43,34 @@ public class BlockDragonEgg extends BlockFallable {
     @Override
     public boolean isTransparent() {
         return true;
+    }
+
+    @Override
+    public int onUpdate(int type) {
+        if (type == Level.BLOCK_UPDATE_TOUCH) {
+            this.teleport();
+        }
+        return super.onUpdate(type);
+    }
+
+    public void teleport() {
+        for (int i = 0; i < 1000; ++i) {
+            Block t = this.getLevel().getBlock(this.add(ThreadLocalRandom.current().nextInt(-16, 16), ThreadLocalRandom.current().nextInt(-16, 16), ThreadLocalRandom.current().nextInt(-16, 16)));
+            if (t.getId() == AIR) {
+                int diffX = this.getFloorX() - t.getFloorX();
+                int diffY = this.getFloorY() - t.getFloorY();
+                int diffZ = this.getFloorZ() - t.getFloorZ();
+                LevelEventPacket pk = new LevelEventPacket();
+                pk.evid = LevelEventPacket.EVENT_PARTICLE_DRAGON_EGG_TELEPORT;
+                pk.data = (((((Math.abs(diffX) << 16) | (Math.abs(diffY) << 8)) | Math.abs(diffZ)) | ((diffX < 0 ? 1 : 0) << 24)) | ((diffY < 0 ? 1 : 0) << 25)) | ((diffZ < 0 ? 1 : 0) << 26);
+                pk.x = this.getFloorX();
+                pk.y = this.getFloorY();
+                pk.z = this.getFloorZ();
+                this.getLevel().addChunkPacket(this.getFloorX() >> 4, this.getFloorZ() >> 4, pk);
+                this.getLevel().setBlock(this, get(AIR), true);
+                this.getLevel().setBlock(t, this, true);
+                return;
+            }
+        }
     }
 }
