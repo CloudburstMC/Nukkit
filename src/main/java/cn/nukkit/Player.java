07f8c0cad9ed654502a16b8fed98e024df35120a
Aaman <38054324+Vrysed@users.noreply.github.com>
2019-03-29 10:44:37 -0700

    Fix Food Change Event Fix #474
    Taking damage does not affect hunger.
    Clicking fast on player fixed

--- src/main/java/cn/nukkit/Player.java
+++ src/main/java/cn/nukkit/Player.java
@@ -3985,13 +3985,12 @@ public class Player extends EntityHuman implements CommandSender, InventoryHolde
         if (source instanceof EntityDamageByEntityEvent) {
             Entity damager = ((EntityDamageByEntityEvent) source).getDamager();
             if (damager instanceof Player) {
-                ((Player) damager).getFoodData().updateFoodExpLevel(0.3);
+                ((Player) damager).getFoodData().updateFoodExpLevel(0.025);
             }
         }
 
         if (super.attack(source)) { //!source.isCancelled()
             if (this.getLastDamageCause() == source && this.spawned) {
-                this.getFoodData().updateFoodExpLevel(0.3);
                 EntityEventPacket pk = new EntityEventPacket();
                 pk.eid = this.id;
                 pk.event = EntityEventPacket.HURT_ANIMATION;
