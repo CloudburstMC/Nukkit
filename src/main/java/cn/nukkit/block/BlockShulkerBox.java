e1048bccb89a699ea64fa90d8a78814fabb1ea2d
wode490390 <wode490390@qq.com>
2019-04-04 11:37:03 +0800

    Fix the direction of ShulkerBox

--- src/main/java/cn/nukkit/block/BlockShulkerBox.java
+++ src/main/java/cn/nukkit/block/BlockShulkerBox.java
@@ -56,13 +56,13 @@ public class BlockShulkerBox extends BlockTransparentMeta {
     public int getToolType() {
         return ItemTool.TYPE_PICKAXE;
     }
-    
+
     @Override
     public Item toItem() {
         ItemBlock item = new ItemBlock(this, this.getDamage(), 1);
-                        
+
         BlockEntityShulkerBox t = (BlockEntityShulkerBox)this.getLevel().getBlockEntity(this);
-        
+
         if (t != null) {
             ShulkerBoxInventory i = t.getRealInventory();
 
@@ -85,12 +85,12 @@ public class BlockShulkerBox extends BlockTransparentMeta {
 
                 item.setCompoundTag(nbt);
             }
-            
+
             if (t.hasName()) {
                 item.setCustomName(t.getName());
             }
         }
-        
+
         return item;
     }
 
@@ -108,12 +108,7 @@ public class BlockShulkerBox extends BlockTransparentMeta {
     @Override
     public boolean place(Item item, Block block, Block target, BlockFace face, double fx, double fy, double fz, Player player) {
         this.getLevel().setBlock(block, this, true, true);
-        CompoundTag nbt = new CompoundTag("")
-                .putList(new ListTag<>("Items"))
-                .putString("id", BlockEntity.SHULKER_BOX)
-                .putInt("x", (int) this.x)
-                .putInt("y", (int) this.y)
-                .putInt("z", (int) this.z)
+        CompoundTag nbt = BlockEntity.getDefaultCompound(this, BlockEntity.SHULKER_BOX)
                 .putByte("facing", face.getIndex());
 
         if (item.hasCustomName()) {
@@ -121,7 +116,7 @@ public class BlockShulkerBox extends BlockTransparentMeta {
         }
 
         CompoundTag t = item.getNamedTag();
-            
+
         if (t != null) {
             if (t.contains("Items")) {
                 nbt.putList(t.getList("Items"));
@@ -139,17 +134,6 @@ public class BlockShulkerBox extends BlockTransparentMeta {
     }
 
     @Override
-    public boolean onBreak(Item item) {
-        BlockEntity t = this.getLevel().getBlockEntity(this);
-        if (t instanceof BlockEntityShulkerBox) {
-            ((BlockEntityShulkerBox) t).unpair();
-        }
-        this.getLevel().setBlock(this, new BlockAir(), true, true);
-
-        return true;
-    }
-
-    @Override
     public boolean onActivate(Item item, Player player) {
         if (player != null) {
             BlockEntity t = this.getLevel().getBlockEntity(this);
@@ -157,17 +141,12 @@ public class BlockShulkerBox extends BlockTransparentMeta {
             if (t instanceof BlockEntityShulkerBox) {
                 box = (BlockEntityShulkerBox) t;
             } else {
-                CompoundTag nbt = new CompoundTag("")
-                        .putList(new ListTag<>("Items"))
-                        .putString("id", BlockEntity.SHULKER_BOX)
-                        .putInt("x", (int) this.x)
-                        .putInt("y", (int) this.y)
-                        .putInt("z", (int) this.z)
-                        .putByte("facing", 0);
+                CompoundTag nbt = BlockEntity.getDefaultCompound(this, BlockEntity.SHULKER_BOX);
                 box = new BlockEntityShulkerBox(this.getLevel().getChunk((int) (this.x) >> 4, (int) (this.z) >> 4), nbt);
             }
 
-            if (this.getSide(BlockFace.fromIndex(box.namedTag.getByte("facing"))).getId() != AIR) {
+            Block block = this.getSide(BlockFace.fromIndex(box.namedTag.getByte("facing")));
+            if (!(block instanceof BlockAir) && !(block instanceof BlockLiquid) && !(block instanceof BlockFlowable)) {
                 return true;
             }
 
--- src/main/java/cn/nukkit/block/BlockUndyedShulkerBox.java
+++ src/main/java/cn/nukkit/block/BlockUndyedShulkerBox.java
@@ -5,15 +5,41 @@
  */
 package cn.nukkit.block;
 
+import cn.nukkit.utils.BlockColor;
+import cn.nukkit.utils.DyeColor;
 
 /**
  *
  * @author Reece Mackie
  */
 public class BlockUndyedShulkerBox extends BlockShulkerBox {
-    
+
+    public BlockUndyedShulkerBox() {
+        super(0);
+    }
+
     @Override
     public int getId() {
         return UNDYED_SHULKER_BOX;
+    }
+
+    @Override
+    public String getName() {
+        return "Shulker Box";
+    }
+
+    @Override
+    public BlockColor getColor() {
+        return BlockColor.PURPLE_BLOCK_COLOR;
+    }
+
+    @Override
+    public DyeColor getDyeColor() {
+        return null;
+    }
+
+    @Override
+    public void setDamage(int meta) {
+
     }
 }
