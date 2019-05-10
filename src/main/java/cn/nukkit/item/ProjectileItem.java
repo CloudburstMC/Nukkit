5d86becef1e7e5da488c007587db82975bdb5b16
Petteri <petteri.malkavaara1@gmail.com>
2019-05-10 21:42:47 +0300

    Update ProjectileItem.java

--- src/main/java/cn/nukkit/item/ProjectileItem.java
+++ src/main/java/cn/nukkit/item/ProjectileItem.java
@@ -42,6 +42,13 @@ public abstract class ProjectileItem extends Item {
 
         Entity projectile = Entity.createEntity(this.getProjectileEntityType(), player.getLevel().getChunk(player.getFloorX() >> 4, player.getFloorZ() >> 4), nbt, player);
         if (projectile != null) {
+            if (projectile instanceof EntityEnderPearl) {
+                if (player.getServer().getTick() - player.getLastEnderPearlThrowingTick() < 20) {
+                    projectile.kill();
+                    return false;
+                }
+            }
+
             projectile.setMotion(projectile.getMotion().multiply(this.getThrowForce()));
             this.count--;
 
@@ -52,6 +59,9 @@ public abstract class ProjectileItem extends Item {
                 if (ev.isCancelled()) {
                     projectile.kill();
                 } else {
+                    if (projectile instanceof EntityEnderPearl) {
+                        player.onThrowEnderPearl();
+                    }
                     projectile.spawnToAll();
                     player.getLevel().addLevelSoundEvent(player, LevelSoundEventPacket.SOUND_BOW);
                 }
