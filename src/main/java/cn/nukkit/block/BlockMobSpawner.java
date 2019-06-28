19aadbf3812c06dd396317854031ebc59b79df04
Petteri <petteri.malkavaara1@gmail.com>
2019-06-28 23:41:22 +0300

    Mob spawner should have no drops

--- src/main/java/cn/nukkit/block/BlockMobSpawner.java
+++ src/main/java/cn/nukkit/block/BlockMobSpawner.java
@@ -38,13 +38,7 @@ public class BlockMobSpawner extends BlockSolid {
 
     @Override
     public Item[] getDrops(Item item) {
-        if (item.isPickaxe() && item.getTier() >= ItemTool.TIER_WOODEN) {
-            return new Item[]{
-                    toItem()
-            };
-        } else {
-            return new Item[0];
-        }
+        return new Item[0];
     }
 
     @Override
