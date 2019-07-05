5eb217ed8a9720efe6439f94c3a17246058c1e3a
Petteri <petteri.malkavaara1@gmail.com>
2019-07-06 00:29:59 +0300

    Arrows shouldn't always burn even player does

--- src/main/java/cn/nukkit/item/ItemBow.java
+++ src/main/java/cn/nukkit/item/ItemBow.java
@@ -52,7 +52,7 @@ public class ItemBow extends ItemTool {
         }
 
         double damage = 2;
-        boolean flame = false;
+        boolean flame;
 
         if (this.hasEnchantments()) {
             Enchantment bowDamage = this.getEnchantment(Enchantment.ID_BOW_POWER);
@@ -77,7 +77,7 @@ public class ItemBow extends ItemTool {
                 .putList(new ListTag<FloatTag>("Rotation")
                         .add(new FloatTag("", (player.yaw > 180 ? 360 : 0) - (float) player.yaw))
                         .add(new FloatTag("", (float) -player.pitch)))
-                .putShort("Fire", player.isOnFire() || flame ? 45 * 60 : 0)
+                .putShort("Fire", flame ? 45 * 60 : 0)
                 .putDouble("damage", damage);
 
         int diff = (Server.getInstance().getTick() - player.getStartActionTick());
