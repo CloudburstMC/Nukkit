5cdf1467ca0ac3d8d75e7f8a176ab13527f62d89
Woder <17339354+wode490390@users.noreply.github.com>
2019-07-23 20:35:19 +0800

    Coarse dirt can be tilled to become dirt (#814)

--- src/main/java/cn/nukkit/block/BlockPodzol.java
+++ src/main/java/cn/nukkit/block/BlockPodzol.java
@@ -1,5 +1,8 @@
 package cn.nukkit.block;
 
+import cn.nukkit.Player;
+import cn.nukkit.item.Item;
+
 /**
  * Created on 2015/11/22 by xtypr.
  * Package cn.nukkit.block in project Nukkit .
@@ -7,6 +10,12 @@ package cn.nukkit.block;
 public class BlockPodzol extends BlockDirt {
 
     public BlockPodzol() {
+        this(0);
+    }
+
+    public BlockPodzol(int meta) {
+        // Podzol can't have meta.
+        super(0);
     }
 
     @Override
@@ -22,5 +31,25 @@ public class BlockPodzol extends BlockDirt {
     @Override
     public boolean canSilkTouch() {
         return true;
+    }
+
+    @Override
+    public boolean canBeActivated() {
+        return false;
+    }
+
+    @Override
+    public boolean onActivate(Item item, Player player) {
+        return false;
+    }
+
+    @Override
+    public int getFullId() {
+        return this.getId() << 4;
+    }
+
+    @Override
+    public void setDamage(int meta) {
+
     }
 }
