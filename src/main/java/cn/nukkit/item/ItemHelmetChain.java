998c93f2f568289762c58a9f151b7c1f3cf06eff
Kevims <kcodeyt@gmail.com>
2019-08-22 14:55:42 +0200

    Forgot to rename Chain Helmet

--- src/main/java/cn/nukkit/item/ItemHelmetChain.java
+++ src/main/java/cn/nukkit/item/ItemHelmetChain.java
@@ -15,7 +15,7 @@ public class ItemHelmetChain extends ItemArmor {
     }
 
     public ItemHelmetChain(Integer meta, int count) {
-        super(CHAIN_HELMET, meta, count, "Chainmail Helmet");
+        super(CHAIN_HELMET, meta, count, "Chain Helmet");
     }
 
     @Override
