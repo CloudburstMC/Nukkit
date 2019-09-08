afd31fa21262e76edcbf20fac6902c85934da32b
马踏踏 <1453041655@qq.com>
2019-09-08 13:38:33 +0800

    Update EntityStray.java

--- src/main/java/cn/nukkit/entity/mob/EntityStray.java
+++ src/main/java/cn/nukkit/entity/mob/EntityStray.java
@@ -3,11 +3,11 @@ package cn.nukkit.entity.mob;
 import cn.nukkit.item.Item;
 import cn.nukkit.level.format.FullChunk;
 import cn.nukkit.nbt.tag.CompoundTag;
-
+import cn.nukkit.entity.EntitySmite;
 /**
  * @author PikyCZ
  */
-public class EntityStray extends EntityMob {
+public class EntityStray extends EntityMob implements EntitySmite{
 
     public static final int NETWORK_ID = 46;
 
