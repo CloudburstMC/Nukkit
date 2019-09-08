f4a23c308a853f8f331fc54b6836365dfd6655a5
马踏踏 <1453041655@qq.com>
2019-09-08 13:35:27 +0800

    Update EntitySilverfish.java

--- src/main/java/cn/nukkit/entity/mob/EntitySilverfish.java
+++ src/main/java/cn/nukkit/entity/mob/EntitySilverfish.java
@@ -2,11 +2,11 @@ package cn.nukkit.entity.mob;
 
 import cn.nukkit.level.format.FullChunk;
 import cn.nukkit.nbt.tag.CompoundTag;
-
+import cn.nukkit.entity.EntityArthropod;
 /**
  * @author PikyCZ
  */
-public class EntitySilverfish extends EntityMob {
+public class EntitySilverfish extends EntityMob implements EntityArthropod{
 
     public static final int NETWORK_ID = 39;
 
