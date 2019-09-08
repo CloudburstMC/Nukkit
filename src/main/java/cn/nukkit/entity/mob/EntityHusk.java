7fa931c01e9701ab99e2e0e135cf0c7f71723409
马踏踏 <1453041655@qq.com>
2019-09-08 13:41:04 +0800

    Update EntityHusk.java

--- src/main/java/cn/nukkit/entity/mob/EntityHusk.java
+++ src/main/java/cn/nukkit/entity/mob/EntityHusk.java
@@ -2,11 +2,11 @@ package cn.nukkit.entity.mob;
 
 import cn.nukkit.level.format.FullChunk;
 import cn.nukkit.nbt.tag.CompoundTag;
-
+import cn.nukkit.entity.EntitySmite;
 /**
  * @author PikyCZ
  */
-public class EntityHusk extends EntityMob {
+public class EntityHusk extends EntityMob implements EntitySmite{
 
     public static final int NETWORK_ID = 47;
 
