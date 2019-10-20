1a28cb39409078c359eada6cb5d63e979f68b3e5
Woder <17339354+wode490390@users.noreply.github.com>
2019-10-20 22:58:13 +0800

    Fix TerrainParticle (#915)

--- src/main/java/cn/nukkit/level/particle/TerrainParticle.java
+++ src/main/java/cn/nukkit/level/particle/TerrainParticle.java
@@ -10,6 +10,6 @@ import cn.nukkit.math.Vector3;
  */
 public class TerrainParticle extends GenericParticle {
     public TerrainParticle(Vector3 pos, Block block) {
-        super(pos, Particle.TYPE_TERRAIN, GlobalBlockPalette.getOrCreateRuntimeId(block.getDamage(), block.getId()));
+        super(pos, Particle.TYPE_TERRAIN, GlobalBlockPalette.getOrCreateRuntimeId(block.getId(), block.getDamage()));
     }
 }
