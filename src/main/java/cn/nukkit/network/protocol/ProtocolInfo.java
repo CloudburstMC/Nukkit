dab1c8ab123cc4d898d5c50a4173d0e513475ce5
LinuxLogo <24740835+jrodr10@users.noreply.github.com>
2019-12-10 16:29:42 -0500

    1.14

--- src/main/java/cn/nukkit/network/protocol/ProtocolInfo.java
+++ src/main/java/cn/nukkit/network/protocol/ProtocolInfo.java
@@ -14,12 +14,12 @@ public interface ProtocolInfo {
      * Actual Minecraft: PE protocol version
      */
     @SuppressWarnings("UnnecessaryBoxing")
-    int CURRENT_PROTOCOL = Integer.valueOf("388"); // DO NOT REMOVE BOXING
+    int CURRENT_PROTOCOL = Integer.valueOf("389"); // DO NOT REMOVE BOXING
 
-    List<Integer> SUPPORTED_PROTOCOLS = Ints.asList(CURRENT_PROTOCOL);
+    List<Integer> SUPPORTED_PROTOCOLS = Ints.asList(CURRENT_PROTOCOL, 388);
 
-    String MINECRAFT_VERSION = "v1.13.0";
-    String MINECRAFT_VERSION_NETWORK = "1.13.0";
+    String MINECRAFT_VERSION = "v1.14.0";
+    String MINECRAFT_VERSION_NETWORK = "1.14.0";
 
     byte LOGIN_PACKET = 0x01;
     byte PLAY_STATUS_PACKET = 0x02;
