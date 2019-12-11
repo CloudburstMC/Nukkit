8dee517e54a632e73ae040e553fe6afaa8f49b20
Sleepybear <thomas.ucsc@gmail.com>
2019-12-11 10:21:26 -0800

    Honeycomb and honey block hardness

--- src/main/java/cn/nukkit/block/BlockHoney.java
+++ src/main/java/cn/nukkit/block/BlockHoney.java
@@ -1,14 +1,20 @@
 package cn.nukkit.block;
 
-public class BlockHoney extends Block {
+public class BlockHoney extends BlockTransparent {
 
     @Override
     public String getName() {
-        return "Honey";
+        return "Honey Block";
     }
 
     @Override
     public int getId() {
         return HONEY_BLOCK;
     }
+
+    @Override
+    public double getHardness() { return 0; }
+
+    @Override
+    public double getResistance() { return 0;}
 }
--- src/main/java/cn/nukkit/block/BlockHoneyComb.java
+++ src/main/java/cn/nukkit/block/BlockHoneyComb.java
@@ -1,14 +1,17 @@
 package cn.nukkit.block;
 
-public class BlockHoneyComb extends Block {
+public class BlockHoneyComb extends BlockSolid {
 
     @Override
     public String getName() {
-        return "Honeycomb";
+        return "Honeycomb Block";
     }
 
     @Override
     public int getId() {
         return HONEYCOMB_BLOCK;
     }
+
+    @Override
+    public double getHardness() { return 0.6; }
 }
