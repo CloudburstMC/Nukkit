2212962bcb404a70b0af1a74150cf07f8c7a1ef9
Mr.Pandaz <44748508+DaRealPandaz@users.noreply.github.com>
2019-12-16 16:59:44 -0500

    Create GamemodeTypes.java

---
+++ src/main/java/cn/nukkit/utils/GamemodeTypes.java
@@ -0,0 +1,10 @@
+package cn.nukkit.utils;
+
+public class GamemodeTypes {
+
+    public static int TYPE_CREATIVE = 1;
+    public static int TYPE_SURVIVAL = 0;
+    public static int TYPE_ADVENTURE = 2;
+    public static int TYPE_SPECTATOR = 3;
+
+}
