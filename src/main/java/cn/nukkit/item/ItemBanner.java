1ac6d50d36f07b6f1a02df299d9591d78c379db9
Kevims <kcodeyt@gmail.com>
2019-12-18 18:37:13 +0100

    Add API methods for banners (#1019)
    * Add basic api methods for banners
    
    * Remove colour vars and use DyeColor instead
    
    * Remove int color method its not used
    
    * Change int color to DyeColor
    
    * Created a BannerPattern class and return it instead a tag
    
    * Set the value from BY_NAME direct
    
    * Remove setPatterns and getPatterns and add getPatternsSize

--- src/main/java/cn/nukkit/item/ItemBanner.java
+++ src/main/java/cn/nukkit/item/ItemBanner.java
@@ -3,6 +3,9 @@ package cn.nukkit.item;
 import cn.nukkit.block.Block;
 import cn.nukkit.nbt.tag.CompoundTag;
 import cn.nukkit.nbt.tag.IntTag;
+import cn.nukkit.nbt.tag.ListTag;
+import cn.nukkit.utils.BannerPattern;
+import cn.nukkit.utils.DyeColor;
 
 /**
  * Created by PetteriM1
@@ -20,6 +23,7 @@ public class ItemBanner extends Item {
     public ItemBanner(Integer meta, int count) {
         super(BANNER, meta, count, "Banner");
         this.block = Block.get(Block.STANDING_BANNER);
+        this.correctNBT();
     }
 
     @Override
@@ -31,8 +35,49 @@ public class ItemBanner extends Item {
         return this.getNamedTag().getInt("Base");
     }
 
-    public void setBaseColor(int color) {
-        this.getNamedTag().putInt("Base", color & 0x0f);
+    public void setBaseColor(DyeColor color) {
+        CompoundTag tag = this.hasCompoundTag() ? this.getNamedTag() : new CompoundTag();
+        tag.putInt("Base", color.getDyeData() & 0x0f);
+        this.setDamage(color.getDyeData() & 0x0f);
+        this.setNamedTag(tag);
+    }
+
+    public int getType() {
+        return this.getNamedTag().getInt("Type");
+    }
+
+    public void setType(int type) {
+        CompoundTag tag = this.hasCompoundTag() ? this.getNamedTag() : new CompoundTag();
+        tag.putInt("Type", type);
+        this.setNamedTag(tag);
+    }
+
+    public void addPattern(BannerPattern pattern) {
+        CompoundTag tag = this.hasCompoundTag() ? this.getNamedTag() : new CompoundTag();
+        ListTag<CompoundTag> patterns = tag.getList("Patterns", CompoundTag.class);
+        patterns.add(new CompoundTag("").
+                putInt("Color", pattern.getColor().getDyeData() & 0x0f).
+                putString("Pattern", pattern.getType().getName()));
+        tag.putList(patterns);
+        this.setNamedTag(tag);
+    }
+
+    public BannerPattern getPattern(int index) {
+        CompoundTag tag = this.hasCompoundTag() ? this.getNamedTag() : new CompoundTag();
+        return BannerPattern.fromCompoundTag(tag.getList("Patterns").size() > index && index >= 0 ? tag.getList("Patterns", CompoundTag.class).get(index) : new CompoundTag());
+    }
+
+    public void removePattern(int index) {
+        CompoundTag tag = this.hasCompoundTag() ? this.getNamedTag() : new CompoundTag();
+        ListTag<CompoundTag> patterns = tag.getList("Patterns", CompoundTag.class);
+        if(patterns.size() > index && index >= 0) {
+            patterns.remove(index);
+        }
+        this.setNamedTag(tag);
+    }
+
+    public int getPatternsSize() {
+        return (this.hasCompoundTag() ? this.getNamedTag() : new CompoundTag()).getList("Patterns").size();
     }
 
     public void correctNBT() {
