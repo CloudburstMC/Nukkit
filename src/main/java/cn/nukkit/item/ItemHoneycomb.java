2c2066c7ff94adf2f652110cceb33eced2574de6
joserobjr <jose.rob.jr@gmail.com>
2019-12-28 17:03:42 -0300

    Implements honey bottle and honeycombs

---
+++ src/main/java/cn/nukkit/item/ItemHoneycomb.java
@@ -0,0 +1,17 @@
+package cn.nukkit.item;
+
+public class ItemHoneycomb extends Item {
+    
+    public ItemHoneycomb() {
+        this(0, 1);
+    }
+    
+    public ItemHoneycomb(Integer meta) {
+        this(meta, 1);
+    }
+    
+    public ItemHoneycomb(Integer meta, int count) {
+        super(HONEYCOMB, meta, count, "Honeycomb");
+    }
+    
+}
