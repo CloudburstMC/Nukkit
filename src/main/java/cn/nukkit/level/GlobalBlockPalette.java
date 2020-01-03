ff06a0d2b755b4abc119e463b82eba696aa85673
SupremeMortal <6178101+SupremeMortal@users.noreply.github.com>
2020-01-03 21:14:21 +0000

    Reserialize block palette without meta tags

--- src/main/java/cn/nukkit/level/GlobalBlockPalette.java
+++ src/main/java/cn/nukkit/level/GlobalBlockPalette.java
@@ -11,6 +11,7 @@ import it.unimi.dsi.fastutil.ints.Int2IntOpenHashMap;
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.nio.ByteOrder;
 import java.util.NoSuchElementException;
 import java.util.concurrent.atomic.AtomicInteger;
 
@@ -30,12 +31,10 @@ public class GlobalBlockPalette {
         }
         ListTag<CompoundTag> tag;
         try {
-            //noinspection UnstableApiUsage
-            BLOCK_PALETTE = ByteStreams.toByteArray(stream);
             //noinspection unchecked
-            tag = (ListTag<CompoundTag>) NBTIO.readNetwork(new ByteArrayInputStream(BLOCK_PALETTE));
+            tag = (ListTag<CompoundTag>) NBTIO.readNetwork(stream);
         } catch (IOException e) {
-            throw new AssertionError(e);
+            throw new AssertionError("Unable to load block palette", e);
         }
 
         for (CompoundTag state : tag.getAll()) {
@@ -52,6 +51,12 @@ public class GlobalBlockPalette {
                 legacyToRuntimeId.put(legacyId, runtimeId);
             }
             state.remove("meta"); // No point in sending this since the client doesn't use it.
+        }
+
+        try {
+            BLOCK_PALETTE = NBTIO.write(tag, ByteOrder.LITTLE_ENDIAN, true);
+        } catch (IOException e) {
+            throw new AssertionError("Unable to write block palette", e);
         }
     }
 
