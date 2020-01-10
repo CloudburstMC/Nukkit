e43acd2ffc4474ad260dcf7ffb9e945677fb9334
SupremeMortal <6178101+SupremeMortal@users.noreply.github.com>
2020-01-10 21:14:48 +0000

    Fix palette decoding issues.

--- src/main/java/cn/nukkit/level/GlobalBlockPalette.java
+++ src/main/java/cn/nukkit/level/GlobalBlockPalette.java
@@ -4,17 +4,17 @@ import cn.nukkit.Server;
 import cn.nukkit.nbt.NBTIO;
 import cn.nukkit.nbt.tag.CompoundTag;
 import cn.nukkit.nbt.tag.ListTag;
-import com.google.common.io.ByteStreams;
 import it.unimi.dsi.fastutil.ints.Int2IntMap;
 import it.unimi.dsi.fastutil.ints.Int2IntOpenHashMap;
 
-import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.ByteOrder;
+import java.util.Arrays;
 import java.util.NoSuchElementException;
 import java.util.concurrent.atomic.AtomicInteger;
 
+@Log4j2
 public class GlobalBlockPalette {
     private static final Int2IntMap legacyToRuntimeId = new Int2IntOpenHashMap();
     private static final Int2IntMap runtimeIdToLegacy = new Int2IntOpenHashMap();
@@ -32,7 +32,7 @@ public class GlobalBlockPalette {
         ListTag<CompoundTag> tag;
         try {
             //noinspection unchecked
-            tag = (ListTag<CompoundTag>) NBTIO.readNetwork(stream);
+            tag = (ListTag<CompoundTag>) NBTIO.readTag(stream, ByteOrder.LITTLE_ENDIAN, false);
         } catch (IOException e) {
             throw new AssertionError("Unable to load block palette", e);
         }
