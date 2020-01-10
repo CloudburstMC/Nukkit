4e9be379e0018a43058513445e8438c975daabfb
joserobjr <jose.rob.jr@gmail.com>
2020-01-10 14:41:53 -0300

    Fix an auto-merge error

--- src/main/java/cn/nukkit/utils/BlockColor.java
+++ src/main/java/cn/nukkit/utils/BlockColor.java
@@ -51,7 +51,6 @@ public class BlockColor  {
     public static final BlockColor OBSIDIAN_BLOCK_COLOR = new BlockColor(0x15, 0x14, 0x1f);
     public static final BlockColor SPRUCE_BLOCK_COLOR = new BlockColor(0x81, 0x56, 0x31);
     public static final BlockColor NETHERRACK_BLOCK_COLOR = new BlockColor(0x70, 0x02, 0x00);
-    public static final BlockColor SPRUCE_BLOCK_COLOR = new BlockColor(0x81, 0x56, 0x31);
     public static final BlockColor REDSTONE_BLOCK_COLOR = TNT_BLOCK_COLOR;
 
     public static final BlockColor WHITE_TERRACOTA_BLOCK_COLOR = new BlockColor(0xd1, 0xb1, 0xa1);
