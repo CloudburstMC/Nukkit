9b1ee2d22b6e489ec6f8ebe4551c8cd5e46cc443
joserobjr <jose.rob.jr@gmail.com>
2020-01-20 22:39:01 -0300

    Change the codename to PowerNukkit

--- src/main/java/cn/nukkit/Nukkit.java
+++ src/main/java/cn/nukkit/Nukkit.java
@@ -43,7 +43,7 @@ public class Nukkit {
     public final static Properties GIT_INFO = getGitInfo();
     public final static String VERSION = getVersion();
     public final static String API_VERSION = "1.0.10";
-    public final static String CODENAME = "";
+    public final static String CODENAME = "PowerNukkit";
     @Deprecated
     public final static String MINECRAFT_VERSION = ProtocolInfo.MINECRAFT_VERSION;
     @Deprecated
