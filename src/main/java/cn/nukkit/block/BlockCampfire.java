a668bd05a0e7db00a7a41b2c03b80f905ea55277
Sleepybear <thomas.ucsc@gmail.com>
2020-01-23 16:42:12 -0800

    create block class

---
+++ src/main/java/cn/nukkit/block/BlockCampfire.java
@@ -0,0 +1,36 @@
+package cn.nukkit.block;
+
+import cn.nukkit.item.ItemTool;
+import cn.nukkit.math.BlockFace;
+import cn.nukkit.utils.Faceable;
+import cn.nukkit.utils.Identifier;
+
+public class BlockCampfire extends BlockSolid implements Faceable {
+
+    private static final int CAMPFIRE_LIT_MASK = 0x08;
+    private static final int CAMPFIRE_FACING_MASK = 0x07;
+
+    public BlockCampfire(Identifier id) {
+        super(id);
+    }
+
+    public BlockCampfire(Identifier id, int meta) {
+        super(id);
+        this.setDamage(meta);
+    }
+
+    @Override
+    public double getHardness() {
+        return 2.0;
+    }
+
+    @Override
+    public int getToolType() {
+        return ItemTool.TYPE_AXE;
+    }
+
+    @Override
+    public BlockFace getBlockFace() {
+        return BlockFace.fromHorizontalIndex(this.getDamage() & CAMPFIRE_FACING_MASK);
+    }
+}
