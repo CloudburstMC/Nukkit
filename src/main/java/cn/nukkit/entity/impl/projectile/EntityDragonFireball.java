ab99d347b0f6daf62b23daae18fb38061d48b9af
SupremeMortal <6178101+SupremeMortal@users.noreply.github.com>
2020-01-25 18:59:44 +0000

    Fix projectile entities.

--- src/main/java/cn/nukkit/entity/impl/projectile/EntityDragonFireball.java
+++ src/main/java/cn/nukkit/entity/impl/projectile/EntityDragonFireball.java
@@ -1,12 +1,11 @@
 package cn.nukkit.entity.impl.projectile;
 
 import cn.nukkit.entity.EntityType;
-import cn.nukkit.entity.impl.BaseEntity;
 import cn.nukkit.entity.projectile.DragonFireball;
 import cn.nukkit.level.chunk.Chunk;
 import cn.nukkit.nbt.tag.CompoundTag;
 
-public class EntityDragonFireball extends BaseEntity implements DragonFireball {
+public class EntityDragonFireball extends EntityProjectile implements DragonFireball {
     public EntityDragonFireball(EntityType<?> type, Chunk chunk, CompoundTag nbt) {
         super(type, chunk, nbt);
     }
--- src/main/java/cn/nukkit/entity/impl/projectile/EntityShulkerBullet.java
+++ src/main/java/cn/nukkit/entity/impl/projectile/EntityShulkerBullet.java
@@ -1,12 +1,11 @@
 package cn.nukkit.entity.impl.projectile;
 
 import cn.nukkit.entity.EntityType;
-import cn.nukkit.entity.impl.BaseEntity;
 import cn.nukkit.entity.projectile.ShulkerBullet;
 import cn.nukkit.level.chunk.Chunk;
 import cn.nukkit.nbt.tag.CompoundTag;
 
-public class EntityShulkerBullet extends BaseEntity implements ShulkerBullet {
+public class EntityShulkerBullet extends EntityProjectile implements ShulkerBullet {
     public EntityShulkerBullet(EntityType<?> type, Chunk chunk, CompoundTag nbt) {
         super(type, chunk, nbt);
     }
