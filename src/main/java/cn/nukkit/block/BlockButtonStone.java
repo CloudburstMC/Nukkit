c6943fe6f0d5308eaa892948df8a43e931ff94ce
joserobjr <jose.rob.jr@gmail.com>
2020-02-06 07:40:48 -0300

    Fix stone_button break time and dropping without pickaxe (#1140)

--- src/main/java/cn/nukkit/block/BlockButtonStone.java
+++ src/main/java/cn/nukkit/block/BlockButtonStone.java
@@ -1,5 +1,6 @@
 package cn.nukkit.block;
 
+import cn.nukkit.item.Item;
 import cn.nukkit.item.ItemTool;
 import cn.nukkit.utils.Identifier;
 
@@ -15,5 +16,19 @@ public class BlockButtonStone extends BlockButton {
     @Override
     public int getToolType() {
         return ItemTool.TYPE_PICKAXE;
+    }
+
+    @Override
+    public boolean canHarvestWithHand() {
+        return false;
+    }
+
+    @Override
+    public Item[] getDrops(Item item) {
+        if (item.isPickaxe()) {
+            return super.getDrops(item);
+        } else {
+            return new Item[0];
+        }
     }
 }
