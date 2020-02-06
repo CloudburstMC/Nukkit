687deccee76a535ea106ee3952098162d8a4d41a
joserobjr <jose.rob.jr@gmail.com>
2020-02-06 03:51:58 -0300

    Removes an unnecessary empty line change

--- src/main/java/cn/nukkit/registry/BlockRegistry.java
+++ src/main/java/cn/nukkit/registry/BlockRegistry.java
@@ -517,7 +517,7 @@ public class BlockRegistry implements Registry {
         this.factoryMap.put(CAMPFIRE, BlockCampfire::new);
 
         this.factoryMap.put(BARREL, BlockBarrel::new); // 457
-
+      
         this.factoryMap.put(BLUE_ICE, BlockBlueIce::new); //266
 
         this.factoryMap.put(GRANITE_STAIRS, BlockStairsGranite::new); //424
