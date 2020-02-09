d2e5f859db24baf2e4c4e777ff35b367977511b2
joserobjr <jose.rob.jr@gmail.com>
2020-02-09 17:17:34 -0300

    Fixes lava and water collision when water is at layer 1

--- src/main/java/cn/nukkit/block/BlockLava.java
+++ src/main/java/cn/nukkit/block/BlockLava.java
@@ -160,7 +160,8 @@ public class BlockLava extends BlockLiquid {
         Block colliding = null;
         for(int side = 1; side < 6; ++side){ //don't check downwards side
             Block blockSide = this.getSide(BlockFace.fromIndex(side));
-            if(blockSide instanceof BlockWater){
+            if(blockSide instanceof BlockWater
+                    || (blockSide = blockSide.getBlockAtLayer(1)) instanceof BlockWater){
                 colliding = blockSide;
                 break;
             }
