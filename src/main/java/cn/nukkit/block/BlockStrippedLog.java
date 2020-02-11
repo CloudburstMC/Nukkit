abd10c1c7ce8a4cbdb5e0c6bef0813f7248956e0
joserobjr <jose.rob.jr@gmail.com>
2020-02-11 11:18:28 -0300

    Fixes wrong Stripped Log Block damage (#1176)
    Co-authored-by: Gabriel Henrique <31969146+Gabriel8579@users.noreply.github.com>

--- src/main/java/cn/nukkit/block/BlockStrippedLog.java
+++ src/main/java/cn/nukkit/block/BlockStrippedLog.java
@@ -1,5 +1,9 @@
 package cn.nukkit.block;
 
+import cn.nukkit.item.Item;
+import cn.nukkit.math.BlockFace;
+import cn.nukkit.math.Vector3f;
+import cn.nukkit.player.Player;
 import cn.nukkit.utils.Identifier;
 
 public class BlockStrippedLog extends BlockLog {
@@ -11,5 +15,11 @@ public class BlockStrippedLog extends BlockLog {
     @Override
     public boolean canBeActivated() {
         return false;
+    }
+
+    @Override
+    public boolean place(Item item, Block block, Block target, BlockFace face, Vector3f clickPos, Player player) {
+        this.setDamage(FACES[face.getIndex()] >> 2);
+        return this.getLevel().setBlock(this.asVector3i(), this, true, true);
     }
 }
