c64b4fe52aa0491e1ac83656be94eab0cd1e4d46
Gabriel8579 <gygamer4@gmail.com>
2020-02-12 19:35:02 -0300

    Fixes can't be waterlogging flowing

--- src/main/java/cn/nukkit/block/BlockLight.java
+++ src/main/java/cn/nukkit/block/BlockLight.java
@@ -31,6 +31,11 @@ public class BlockLight extends BlockTransparent {
     }
 
     @Override
+    public boolean canWaterlogFlowing() {
+        return true;
+    }
+
+    @Override
     public boolean canBeReplaced() {
         return true;
     }
