b3a60621ae395db304319d8fe8c834909202f8e0
roridev <t3ctotalmenterandom1@outlook.com>
2020-02-12 22:11:13 -0300

    Reformatted code

--- src/main/java/cn/nukkit/block/BlockKelp.java
+++ src/main/java/cn/nukkit/block/BlockKelp.java
@@ -18,58 +18,51 @@ public class BlockKelp extends Block {
     public BlockKelp(Identifier id) {
         super(id);
     }
-    @Override
-    public boolean canWaterlogSource() { return true; }
 
     @Override
-    public boolean place(Item item, Block block, Block target, BlockFace face, Vector3f clickPos, Player player)
-    {
+    public boolean canWaterlogSource() {
+        return true;
+    }
+
+    @Override
+    public boolean place(Item item, Block block, Block target, BlockFace face, Vector3f clickPos, Player player) {
         Block down = down();
         Block layerOneBlock = getBlockAtLayer(1);
         int waterDamage;
-        if((down.getId() == BlockIds.KELP || down.isSolid()) && down.getId() != BlockIds.MAGMA && down.getId() != BlockIds.ICE &&
+        if ((down.getId() == BlockIds.KELP || down.isSolid()) && down.getId() != BlockIds.MAGMA && down.getId() != BlockIds.ICE &&
                 down.getId() != BlockIds.SOUL_SAND && (layerOneBlock instanceof BlockWater && ((waterDamage = (block.getDamage())) == 0
                 || waterDamage == 8))) {
-            if(waterDamage == 8)
-            {
+            if (waterDamage == 8) {
                 this.level.setBlock(this, new BlockWater(BlockIds.WATER), true, false);
             }
-            if(down.getId() == BlockIds.KELP && down.getDamage() != 24)
-            {
+            if (down.getId() == BlockIds.KELP && down.getDamage() != 24) {
                 down.setDamage(24);
-                this.level.setBlock(this,this,true,true);
+                this.level.setBlock(this, this, true, true);
             }
             setDamage(ThreadLocalRandom.current().nextInt(25));
-            this.level.setBlock(this,this,true,true);
+            this.level.setBlock(this, this, true, true);
             return true;
-        }
-        else
+        } else
             return false;
     }
 
     @Override
-    public int onUpdate(int type)
-    {
-        if(type == Level.BLOCK_UPDATE_NORMAL)
-        {
+    public int onUpdate(int type) {
+        if (type == Level.BLOCK_UPDATE_NORMAL) {
             Block down = down();
             Block layerOneBlock = getBlockAtLayer(1);
             int waterDamage;
-            if( !(layerOneBlock.getId().equals(BlockIds.FROSTED_ICE)) &&
+            if (!(layerOneBlock.getId().equals(BlockIds.FROSTED_ICE)) &&
                     (!(layerOneBlock instanceof BlockWater) || ((waterDamage = layerOneBlock.getDamage()) != 0 && waterDamage != 8))
-            )
-            {
+            ) {
                 this.getLevel().useBreakOn(this);
                 return type;
             }
-            if(down instanceof BlockWater)
-            {
+            if (down instanceof BlockWater) {
                 this.getLevel().useBreakOn(this);
                 return type;
             }
-        }
-        else if(type == Level.BLOCK_UPDATE_RANDOM)
-        {
+        } else if (type == Level.BLOCK_UPDATE_RANDOM) {
             if (ThreadLocalRandom.current().nextInt(100) <= 14) {
                 grow();
             }
@@ -90,7 +83,7 @@ public class BlockKelp extends Block {
                 if (!ev.isCancelled()) {
                     this.setDamage(25);
                     this.getLevel().setBlock(this, this, true, true);
-                    this.getLevel().setBlock(up,  new BlockWater(BlockIds.WATER), true, false);
+                    this.getLevel().setBlock(up, new BlockWater(BlockIds.WATER), true, false);
                     this.getLevel().setBlock(up, ev.getNewState(), true, true);
                     return true;
                 }
