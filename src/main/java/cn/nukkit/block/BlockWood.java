40ab83b98695e115b73ee3d55c06f5b99b684521
joserobjr <jose.rob.jr@gmail.com>
2020-02-13 21:42:18 -0300

    Sanitize the file even more and makes some fixes, expand for details
    * Added getter and setter for all block properties
    * Simplified the code by using the property accessors
    * Makes sures that tools of creative players won't get damaged
    * Removed an unnecessary logic when stripping the block
    * Fixes the block color checking for incorrect damage values

--- src/main/java/cn/nukkit/block/BlockWood.java
+++ src/main/java/cn/nukkit/block/BlockWood.java
@@ -3,22 +3,24 @@ package cn.nukkit.block;
 import cn.nukkit.item.Item;
 import cn.nukkit.item.ItemTool;
 import cn.nukkit.math.BlockFace;
+import cn.nukkit.math.BlockFace.Axis;
 import cn.nukkit.math.Vector3f;
 import cn.nukkit.player.Player;
 import cn.nukkit.utils.BlockColor;
 import cn.nukkit.utils.Identifier;
 
+//Block state information: https://hastebin.com/emuvawasoj.js
 public class BlockWood extends BlockSolid {
     private static final int STRIPPED_BIT = 0b1000;
-
-    protected static final short[] FACES = new short[]{
-            0,    //y
-            0,    //y
-            0x20, //z
-            0x20, //z
-            0x10, //x
-            0x10  //x
-    };
+    public static final int OAK = 0;
+    public static final int SPRUCE = 1;
+    public static final int BIRCH = 2;
+    public static final int JUNGLE = 3;
+    public static final int ACACIA = 4;
+    public static final int DARK_OAK = 5;
+    private static final int AXIS_Y = 0;
+    private static final int AXIS_X = 1 << 4;
+    private static final int AXIS_Z = 2 << 4;
 
     public BlockWood(Identifier id) {
         super(id);
@@ -51,60 +53,95 @@ public class BlockWood extends BlockSolid {
 
     @Override
     public Item toItem() {
-        return Item.get(id, this.getDamage() & 0xF);
+        return Item.get(id, getDamage() & 0xF);
     }
 
 
     @Override
     public boolean canBeActivated() {
-        return (STRIPPED_BIT & getDamage()) == 0;
+        return !isStripped();
     }
 
     @Override
     public boolean onActivate(Item item, Player player) {
-        if (!item.isAxe() || !item.useOn(this)) {
+        if (!item.isAxe() || !player.isCreative() && !item.useOn(this)) {
             return false;
         }
 
-        /*
-         * Information extracted from commit 5160037c32a5c8e42620b12fb82c11250d7ba37b
-         * Bit explanation (rigth to left)
-         * bit 0 - 2: Determines the wood_type
-         * bit 3: Determines if the wood is stripped Except when is a dark oak wood: 0b1000 Dark Oak Wood > 0b1101 Dark Oak Wood
-         * bit 4 - 5: Determines the pillar_axis
-         *
-         * Block state information:
-         * https://hastebin.com/emuvawasoj.js
-         */
-        if ((this.getDamage() & STRIPPED_BIT) == STRIPPED_BIT) {
-            this.setDamage((this.getDamage() ^ STRIPPED_BIT) | 0b1101);
-        }
-        Block replace = Block.get(getId(), this.getDamage() | STRIPPED_BIT); // adds the offset for stripped woods
-        level.setBlock(x, y, z, layer, replace, true, true);
+        setDamage(getDamage() | STRIPPED_BIT);  // adds the offset for stripped woods
+        getLevel().setBlock(this, this, true);
         return true;
+    }
+
+    public void setStripped(boolean stripped) {
+        setDamage((getDamage() & ~STRIPPED_BIT) | (stripped ? STRIPPED_BIT : 0));
+    }
+
+    public boolean isStripped() {
+        return (getDamage() & STRIPPED_BIT) != 0;
+    }
+
+    public Axis getAxis() {
+        switch (getDamage() & 0x30) {
+            default:
+            case AXIS_Y:
+                return Axis.Y;
+            case AXIS_X:
+                return Axis.X;
+            case AXIS_Z:
+                return Axis.Z;
+        }
+    }
+
+    public void setAxis(Axis axis) {
+        int axisProp;
+        switch (axis) {
+            default:
+            case Y:
+                axisProp = AXIS_Y;
+                break;
+            case X:
+                axisProp = AXIS_X;
+                break;
+            case Z:
+                axisProp = AXIS_Z;
+                break;
+        }
+        setDamage((getDamage() & ~0x30) | axisProp);
     }
 
     @Override
     public boolean place(Item item, Block block, Block target, BlockFace face, Vector3f clickPos, Player player) {
-        this.setDamage(this.getDamage() | FACES[face.getIndex()]);
+        setAxis(face.getAxis());
         return super.place(item, block, target, face, clickPos, player);
+    }
+
+    public void setWoodType(int woodType) {
+        if (woodType < OAK || woodType > DARK_OAK) {
+            woodType = OAK;
+        }
+        setDamage((getDamage() & -0b1000) | woodType);
+    }
+
+    public int getWoodType() {
+        return getDamage() & 0b111;
     }
 
     @Override
     public BlockColor getColor() {
-        switch (this.getDamage() & 0x07) {
+        switch (getWoodType()) {
             default:
-            case 1: //OAK
+            case OAK:
                 return BlockColor.WOOD_BLOCK_COLOR;
-            case 2: //SPRUCE
+            case SPRUCE:
                 return BlockColor.SPRUCE_BLOCK_COLOR;
-            case 3: //BIRCH
+            case BIRCH:
                 return BlockColor.SAND_BLOCK_COLOR;
-            case 4: //JUNGLE
+            case JUNGLE:
                 return BlockColor.DIRT_BLOCK_COLOR;
-            case 5: //ACACIA
+            case ACACIA:
                 return BlockColor.ORANGE_BLOCK_COLOR;
-            case 6: //DARK OAK
+            case DARK_OAK: //DARK OAK
                 return BlockColor.BROWN_BLOCK_COLOR;
         }
     }
