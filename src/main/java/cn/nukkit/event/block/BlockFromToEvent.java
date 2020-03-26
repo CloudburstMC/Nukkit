935ed29c6784179fec8706ec731aa1a20c3a293a
Sleepybear <thomas.ucsc@gmail.com>
2020-03-25 17:27:22 -0700

    Update BlockFromToEvent

--- src/main/java/cn/nukkit/event/block/BlockFromToEvent.java
+++ src/main/java/cn/nukkit/event/block/BlockFromToEvent.java
@@ -12,14 +12,22 @@ public class BlockFromToEvent extends BlockEvent implements Cancellable {
         return handlers;
     }
 
-    private final Block to;
+    private Block to;
 
     public BlockFromToEvent(Block block, Block to) {
         super(block);
         this.to = to;
     }
 
+    public Block getFrom() {
+        return getBlock();
+    }
+
     public Block getTo() {
         return to;
+    }
+
+    public void setTo(Block newTo) {
+        to = newTo;
     }
 }
