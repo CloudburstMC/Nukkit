d5161257bae2e63d42973a90703178480a17833a
CiviledCode <civiledgamer@gmail.com>
2020-04-16 14:52:33 -0700

    Removed saveAdditionalData()

--- src/main/java/cn/nukkit/blockentity/impl/DaylightDetectorBlockEntity.java
+++ src/main/java/cn/nukkit/blockentity/impl/DaylightDetectorBlockEntity.java
@@ -1,18 +1,14 @@
 package cn.nukkit.blockentity.impl;
 
-import cn.nukkit.block.BlockDaylightDetector;
 import cn.nukkit.block.BlockIds;
 import cn.nukkit.blockentity.BlockEntityType;
 import cn.nukkit.blockentity.DaylightDetector;
 import cn.nukkit.level.chunk.Chunk;
 import cn.nukkit.utils.Identifier;
 import com.nukkitx.math.vector.Vector3i;
-import com.nukkitx.nbt.CompoundTagBuilder;
-import com.nukkitx.nbt.tag.CompoundTag;
 
 public class DaylightDetectorBlockEntity extends BaseBlockEntity implements DaylightDetector {
 
-    private int redstoneSignal;
 
     public DaylightDetectorBlockEntity(BlockEntityType<?> type, Chunk chunk, Vector3i position) {
         super(type, chunk, position);
@@ -24,25 +20,4 @@ public class DaylightDetectorBlockEntity extends BaseBlockEntity implements Dayl
         return blockId == BlockIds.DAYLIGHT_DETECTOR || blockId == BlockIds.DAYLIGHT_DETECTOR_INVERTED;
     }
 
-    @Override
-    public void loadAdditionalData(CompoundTag tag) {
-        super.loadAdditionalData(tag);
-
-        tag.listenForInt("redstone_signal", this::setRedstoneSignal);
-    }
-
-    @Override
-    public void saveAdditionalData(CompoundTagBuilder tag) {
-        super.saveAdditionalData(tag);
-
-        tag.intTag("redstone_signal", this.getRedstoneSignal());
-    }
-
-    public int getRedstoneSignal() {
-        return redstoneSignal;
-    }
-
-    public void setRedstoneSignal(int redstoneSignal) {
-        this.redstoneSignal = redstoneSignal;
-    }
 }
