2347d6c46cc327b731580a9a184ae404bd1a25ad
PetteriM1 <petteri.malkavaara1@gmail.com>
2020-05-17 21:13:42 +0300

    Don't run checkInteractNearby on every tick (#1443)
    Whatever this method is required for (something with vehicle interaction on mobile I guess) it surely doesn't have to run on every tick. Because of the usage of `getEntityPlayerLookingAt` not running this on every tick will improve the server performance especially when there are many players online. I didn't find any side effects from this change.
    
    (cherry picked from commit 090684492c36dc8562c49c795d52997dbdcfd529)

--- src/main/java/cn/nukkit/Player.java
+++ src/main/java/cn/nukkit/Player.java
@@ -1786,7 +1786,10 @@ public class Player extends EntityHuman implements CommandSender, InventoryHolde
         }
 
         this.checkTeleportPosition();
-        this.checkInteractNearby();
+        
+        if (currentTick % 10 == 0) {
+            this.checkInteractNearby();
+        }
 
         if (this.spawned && this.dummyBossBars.size() > 0 && currentTick % 100 == 0) {
             this.dummyBossBars.values().forEach(DummyBossBar::updateBossEntityPosition);
