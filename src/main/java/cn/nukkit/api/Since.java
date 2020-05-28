b17c85461254c1882e7ca92b614bf5323b3a371b
joserobjr <joserobjr@gamemods.com.br>
2020-05-27 21:19:29 -0300

    PN-74 Adds new annotations for better documentation

---
+++ src/main/java/cn/nukkit/api/Since.java
@@ -0,0 +1,19 @@
+package cn.nukkit.api;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Indicates which version added the annotated element.
+ */
+@Retention(RetentionPolicy.CLASS)
+@Target({ElementType.CONSTRUCTOR, ElementType.METHOD, ElementType.ANNOTATION_TYPE, ElementType.TYPE,
+        ElementType.FIELD, ElementType.PACKAGE})
+public @interface Since {
+    /**
+     * The version which added the element.
+     */
+    String value();
+}
