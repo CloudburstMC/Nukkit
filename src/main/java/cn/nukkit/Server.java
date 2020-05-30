5b03afe9bbdbc6d429d425c742a329e1d7515b08
KKDevelop <kkdevs@mail.ru>
2020-05-30 22:06:21 +1000

    Removed duplicate suffix check from regular expressions

--- src/main/java/cn/nukkit/Server.java
+++ src/main/java/cn/nukkit/Server.java
@@ -1490,7 +1490,7 @@ public class Server {
         File[] files = dataDirectory.listFiles(file -> {
             String name = file.getName();
             Matcher matcher = UUID_PATTERN.matcher(name);
-            return !matcher.matches() && name.endsWith(".dat");
+            return !matcher.matches();
         });
 
         if (files == null) {
