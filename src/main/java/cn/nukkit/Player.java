d8d24243714ee89a4c1e9d34de5f4c463ecbe72d
PetteriM1 <petteri.malkavaara1@gmail.com>
2020-06-03 18:42:47 +0300

    Add a simple check for writable book text length

--- src/main/java/cn/nukkit/Player.java
+++ src/main/java/cn/nukkit/Player.java
@@ -3317,6 +3317,10 @@ public class Player extends EntityHuman implements CommandSender, InventoryHolde
                         return;
                     }
 
+                    if (bookEditPacket.text.length() > 256) {
+                        return;
+                    }
+
                     Item newBook = oldBook.clone();
                     boolean success;
                     switch (bookEditPacket.action) {
