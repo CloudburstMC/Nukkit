f615891eff02abf464dc6f6f3f456db32e75427f
joserobjr <joserobjr@gamemods.com.br>
2020-06-27 22:12:16 -0300

    PN-115 Makes the dump humanly sorted

--- src/test/java/cn/nukkit/RuntimeBlockStateDumper.java
+++ src/test/java/cn/nukkit/RuntimeBlockStateDumper.java
@@ -4,12 +4,14 @@ import cn.nukkit.nbt.NBTIO;
 import cn.nukkit.nbt.tag.CompoundTag;
 import cn.nukkit.nbt.tag.ListTag;
 import cn.nukkit.nbt.tag.Tag;
+import test.HumanStringComparator;
 
 import java.io.*;
 import java.nio.ByteOrder;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.TreeMap;
 import java.util.stream.Collectors;
 
 public class RuntimeBlockStateDumper {
@@ -47,14 +49,23 @@ public class RuntimeBlockStateDumper {
             throw new AssertionError(e);
         }
 
+        TreeMap<String, CompoundTag> states = new TreeMap<>(new HumanStringComparator());
+        
+        for (CompoundTag state : tags.getAll()) {
+            CompoundTag block = state.getCompound("block");
+            StringBuilder builder = new StringBuilder(block.getString("name"));
+            for (Tag tag : block.getCompound("states").getAllTags()) {
+                builder.append(';').append(tag.getName()).append('=').append(tag.parseValue());
+            }
+            states.put(builder.toString(), state);
+        }
+
         try (FileWriter fos = new FileWriter("runtime_block_states.dat.dump.txt"); BufferedWriter bos = new BufferedWriter(fos)) {
-            for (CompoundTag state : tags.getAll()) {
+            for (Map.Entry<String, CompoundTag> entry : states.entrySet()) {
+                CompoundTag state = entry.getValue();
                 CompoundTag block = state.getCompound("block");
-                StringBuilder builder = new StringBuilder(block.getString("name"));
-                for (Tag tag : block.getCompound("states").getAllTags()) {
-                    builder.append(';').append(tag.getName()).append('=').append(tag.parseValue());
-                }
-                bos.write(builder.toString());
+                
+                bos.write(entry.getKey());
                 bos.newLine();
 
                 List<String> metas = state.getList("LegacyStates", CompoundTag.class).getAll().stream()
