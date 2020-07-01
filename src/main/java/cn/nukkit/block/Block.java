dc37270d1c8a5d2677864405361385873b185f5d
Lobo Metalúrgico <hugolino1000@gmail.com>
2020-07-01 00:49:37 -0300

    Minor Bugs Fixed

--- src/main/java/cn/nukkit/block/Block.java
+++ src/main/java/cn/nukkit/block/Block.java
@@ -37,7 +37,7 @@ import java.util.function.Predicate;
  */
 public abstract class Block extends Position implements Metadatable, Cloneable, AxisAlignedBB, BlockID {
 
-    public static final int MAX_BLOCK_ID = 550;
+    public static final int MAX_BLOCK_ID = 600;
     public static final int DATA_BITS = 6;
     public static final int DATA_SIZE = 1 << DATA_BITS;
     public static final int DATA_MASK = DATA_SIZE - 1;
