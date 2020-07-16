35193125750df2fa851232a9b878272c514bf013
good777LUCKY <43497313+good777LUCKY@users.noreply.github.com>
2020-07-16 17:58:51 +0900

    Create ItemWarpedFungusOnAStick.java

---
+++ src/main/java/cn/nukkit/item/ItemWarpedFungusOnAStick.java
@@ -0,0 +1,16 @@
+package cn.nukkit.item;
+
+public class ItemWarpedFungusOnAStick extends ItemTool {
+
+    public ItemWarpedFungusOnAStick() {
+        this(0, 1);
+    }
+
+    public ItemWarpedFungusOnAStick(Integer meta) {
+        this(meta, 1);
+    }
+
+    public ItemWarpedFungusOnAStick(Integer meta, int count) {
+        super(WARPED_FUNGUS_ON_A_STICK, meta, count, "Warped Fungus on a stick");
+    }
+}
