fd401759c2b3ee4d912def3671ca7d34445b20fd
Tal <justtal@protonmail.com>
2020-08-04 18:10:47 -0500

    Update NPCRequestPacket.java

--- src/main/java/cn/nukkit/network/protocol/NPCRequestPacket.java
+++ src/main/java/cn/nukkit/network/protocol/NPCRequestPacket.java
@@ -5,12 +5,12 @@ import lombok.ToString;
 @ToString
 public class NPCRequestPacket extends DataPacket {
 
-    public static int REQUEST_SET_ACTIONS = 0;
-    public static int REQUEST_EXECUTE_ACTION = 1;
-    public static int REQUEST_EXECUTE_CLOSING_COMMANDS = 2;
-    public static int REQUEST_SET_NAME = 3;
-    public static int REQUEST_SET_SKIN = 4;
-    public static int REQUEST_SET_INTERACTION_TEXT = 5;
+    public static final int REQUEST_SET_ACTIONS = 0;
+    public static final int REQUEST_EXECUTE_ACTION = 1;
+    public static final int REQUEST_EXECUTE_CLOSING_COMMANDS = 2;
+    public static final int REQUEST_SET_NAME = 3;
+    public static final int REQUEST_SET_SKIN = 4;
+    public static final int REQUEST_SET_INTERACTION_TEXT = 5;
 
     public long entityRuntimeId;
 
