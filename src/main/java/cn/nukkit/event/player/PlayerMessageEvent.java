36cf8eba78d536a4c94aef8eb56b8b0a6ba001dc
XKazzuKX <46299532+XKazzuKX@users.noreply.github.com>
2020-08-08 23:13:19 +0100

    Add HandlerList

--- src/main/java/cn/nukkit/event/player/PlayerMessageEvent.java
+++ src/main/java/cn/nukkit/event/player/PlayerMessageEvent.java
@@ -1,10 +1,18 @@
 package cn.nukkit.event.player;
 
+import cn.nukkit.event.HandlerList;
+
 /**
  * Created on 2015/12/23 by xtypr.
  * Package cn.nukkit.event.player in project Nukkit .
  */
 public abstract class PlayerMessageEvent extends PlayerEvent {
+    private static final HandlerList handlers = new HandlerList();
+
+    public static HandlerList getHandlers() {
+        return handlers;
+    }
+
 
     protected String message;
 
