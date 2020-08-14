be45ab3e17a13bffe71e72c451a1dd126f4692d0
Gabriel Guzman <gabe@lifewaza.com>
2020-08-14 13:08:38 -0400

    Indicate potion is a level 2 potion

--- src/main/java/cn/nukkit/potion/Potion.java
+++ src/main/java/cn/nukkit/potion/Potion.java
@@ -107,7 +107,7 @@ public class Potion implements Cloneable {
         potions[Potion.TURTLE_MASTER_II] = new Potion(Potion.TURTLE_MASTER_II, 2);
         potions[Potion.SLOW_FALLING] = new Potion(Potion.SLOW_FALLING);
         potions[Potion.SLOW_FALLING_LONG] = new Potion(Potion.SLOW_FALLING_LONG);
-        potions[Potion.SLOWNESS_LONG_II] = new Potion(Potion.SLOWNESS_LONG_II);
+        potions[Potion.SLOWNESS_LONG_II] = new Potion(Potion.SLOWNESS_LONG_II, 2);
     }
 
     public static Potion getPotion(int id) {
