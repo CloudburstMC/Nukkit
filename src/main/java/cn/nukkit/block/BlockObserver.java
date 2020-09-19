d0b39c489604549105bf45f87083ff284e667fe7
joserobjr <joserobjr@powernukkit.org>
2020-09-19 17:56:56 -0300

    Upgrade Observer to the block properties system

--- src/main/java/cn/nukkit/block/BlockObserver.java
+++ src/main/java/cn/nukkit/block/BlockObserver.java
@@ -1,18 +1,28 @@
 package cn.nukkit.block;
 
 import cn.nukkit.Player;
+import cn.nukkit.api.PowerNukkitOnly;
+import cn.nukkit.api.Since;
+import cn.nukkit.blockproperty.BlockProperties;
 import cn.nukkit.item.Item;
 import cn.nukkit.item.ItemTool;
 import cn.nukkit.math.BlockFace;
 import cn.nukkit.utils.Faceable;
 
 import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+
+import static cn.nukkit.blockproperty.CommonBlockProperties.FACING_DIRECTION;
+import static cn.nukkit.blockproperty.CommonBlockProperties.POWERED;
 
 /**
- * @author Leonidius20
+ * @author Leonidius20, joserobjr
  * @since 18.08.18
  */
 public class BlockObserver extends BlockSolidMeta implements Faceable {
+    @PowerNukkitOnly
+    @Since("1.4.0.0-PN")
+    public static final BlockProperties PROPERTIES = new BlockProperties(FACING_DIRECTION, POWERED);
 
     public BlockObserver() {
         this(0);
@@ -33,23 +43,22 @@ public class BlockObserver extends BlockSolidMeta implements Faceable {
     }
 
     @Override
-    public boolean place(@Nonnull Item item, @Nonnull Block block, @Nonnull Block target, @Nonnull BlockFace face, double fx, double fy, double fz, Player player) {
+    public boolean place(@Nonnull Item item, @Nonnull Block block, @Nonnull Block target, @Nonnull BlockFace face, double fx, double fy, double fz, @Nullable Player player) {
         if (player != null) {
             if (Math.abs(player.getFloorX() - this.x) <= 1 && Math.abs(player.getFloorZ() - this.z) <= 1) {
                 double y = player.y + player.getEyeHeight();
                 if (y - this.y > 2) {
-                    this.setDamage(BlockFace.DOWN.getIndex());
+                    setBlockFace(BlockFace.DOWN);
                 } else if (this.y - y > 0) {
-                    this.setDamage(BlockFace.UP.getIndex());
+                    setBlockFace(BlockFace.UP);
                 } else {
-                    this.setDamage(player.getHorizontalFacing().getIndex());
+                    setBlockFace(player.getHorizontalFacing());
                 }
             } else {
-                this.setDamage(player.getHorizontalFacing().getIndex());
+                setBlockFace(player.getHorizontalFacing());
             }
-        } else {
-            this.setDamage(0);
         }
+        
         this.getLevel().setBlock(block, this, true, true);
         return true;
     }
@@ -79,9 +88,27 @@ public class BlockObserver extends BlockSolidMeta implements Faceable {
         return 17.5;
     }
 
+    @PowerNukkitOnly
+    @Since("1.4.0.0-PN")
+    public boolean isPowered() {
+        return getBooleanValue(POWERED);
+    }
+    
+    @PowerNukkitOnly
+    @Since("1.4.0.0-PN")
+    public void setPowered(boolean powered) {
+        setBooleanValue(POWERED, powered);
+    }
+    
     @Override
     public BlockFace getBlockFace() {
-        return BlockFace.fromHorizontalIndex(this.getDamage() & 0x07);
+        return getPropertyValue(FACING_DIRECTION);
     }
 
+    @Since("1.4.0.0-PN")
+    @PowerNukkitOnly
+    @Override
+    public void setBlockFace(BlockFace face) {
+        setPropertyValue(FACING_DIRECTION, face);
+    }
 }
