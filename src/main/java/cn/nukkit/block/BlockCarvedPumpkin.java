f17ad75a19fbae3ed3cfb7c4a785a3ee60afba3c
good777LUCKY <43497313+good777LUCKY@users.noreply.github.com>
2020-10-04 11:54:08 +0900

    Don't carve the carved pumpkin

--- src/main/java/cn/nukkit/block/BlockCarvedPumpkin.java
+++ src/main/java/cn/nukkit/block/BlockCarvedPumpkin.java
@@ -1,6 +1,10 @@
 package cn.nukkit.block;
 
+import cn.nukkit.Player;
+import cn.nukkit.item.Item;
+
 public class BlockCarvedPumpkin extends BlockPumpkin {
+
     @Override
     public int getId() {
         return CARVED_PUMPKIN;
@@ -9,5 +13,15 @@ public class BlockCarvedPumpkin extends BlockPumpkin {
     @Override
     public String getName() {
         return "Caved Pumpkin";
+    }
+    
+    @Override
+    public boolean canBeActivated() {
+        return false;
+    }
+    
+    @Override
+    public boolean onActivate(@Nonnull Item item, Player player) {
+        return false;
     }
 }
