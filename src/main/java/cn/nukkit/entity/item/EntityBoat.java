5216ef3a3d69cfcf87aba3250ed2cb2a220c3583
good777LUCKY <43497313+good777LUCKY@users.noreply.github.com>
2020-10-18 10:51:44 +0900

    No smoke

--- src/main/java/cn/nukkit/entity/item/EntityBoat.java
+++ src/main/java/cn/nukkit/entity/item/EntityBoat.java
@@ -17,7 +17,6 @@ import cn.nukkit.item.ItemBoat;
 import cn.nukkit.level.GameRule;
 import cn.nukkit.level.Location;
 import cn.nukkit.level.format.FullChunk;
-import cn.nukkit.level.particle.SmokeParticle;
 import cn.nukkit.math.AxisAlignedBB;
 import cn.nukkit.math.NukkitMath;
 import cn.nukkit.math.Vector3;
@@ -129,9 +128,6 @@ public class EntityBoat extends EntityVehicle {
         for (Entity linkedEntity : this.passengers) {
             linkedEntity.riding = null;
         }
-
-        SmokeParticle particle = new SmokeParticle(this);
-        this.level.addParticle(particle);
     }
 
     @Override
