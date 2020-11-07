d9c36488ea554e339eac391c2af321c752ae1441
Kevims <kcodeyt@gmail.com>
2020-11-07 02:17:03 +0100

    Fix end portal creation (#1667)

--- src/main/java/cn/nukkit/block/BlockEndPortalFrame.java
+++ src/main/java/cn/nukkit/block/BlockEndPortalFrame.java
@@ -9,6 +9,9 @@ import cn.nukkit.network.protocol.LevelSoundEventPacket;
 import cn.nukkit.utils.BlockColor;
 import cn.nukkit.utils.Faceable;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by Pub4Game on 26.12.2015.
  */
@@ -90,7 +93,7 @@ public class BlockEndPortalFrame extends BlockTransparentMeta implements Faceabl
     }
 
     public void createPortal() {
-        Vector3 centerSpot = this.searchCenter();
+        Vector3 centerSpot = this.searchCenter(new ArrayList<>());
         if(centerSpot != null) {
             for(int x = -2; x <= 2; x++) {
                 for(int z = -2; z <= 2; z++) {
@@ -116,15 +119,16 @@ public class BlockEndPortalFrame extends BlockTransparentMeta implements Faceabl
         }
     }
 
-    private Vector3 searchCenter() {
+    private Vector3 searchCenter(List<Block> visited) {
         for(int x = -2; x <= 2; x++) {
             if(x == 0)
                 continue;
             Block block = this.getLevel().getBlock(this.add(x, 0, 0));
             Block iBlock = this.getLevel().getBlock(this.add(x * 2, 0, 0));
-            if(this.checkFrame(block)) {
+            if(this.checkFrame(block) && !visited.contains(block)) {
+                visited.add(block);
                 if((x == -1 || x == 1) && this.checkFrame(iBlock))
-                    return ((BlockEndPortalFrame) block).searchCenter();
+                    return ((BlockEndPortalFrame) block).searchCenter(visited);
                 for(int z = -4; z <= 4; z++) {
                     if(z == 0)
                         continue;
@@ -140,9 +144,10 @@ public class BlockEndPortalFrame extends BlockTransparentMeta implements Faceabl
                 continue;
             Block block = this.getLevel().getBlock(this.add(0, 0, z));
             Block iBlock = this.getLevel().getBlock(this.add(0, 0, z * 2));
-            if(this.checkFrame(block)) {
+            if(this.checkFrame(block) && !visited.contains(block)) {
+                visited.add(block);
                 if((z == -1 || z == 1) && this.checkFrame(iBlock))
-                    return ((BlockEndPortalFrame) block).searchCenter();
+                    return ((BlockEndPortalFrame) block).searchCenter(visited);
                 for(int x = -4; x <= 4; x++) {
                     if(x == 0)
                         continue;
