3263f13a3a504d6d872bc1493a20d37611000306
Erik Miller <Meaglepvp@gmail.com>
2020-11-26 10:36:20 +0100

    implement that Haybales reduce fall damage

--- src/main/java/cn/nukkit/entity/Entity.java
+++ src/main/java/cn/nukkit/entity/Entity.java
@@ -1641,6 +1641,13 @@ public abstract class Entity extends Location implements Metadatable {
         }
 
         float damage = (float) Math.floor(fallDistance - 3 - (this.hasEffect(Effect.JUMP) ? this.getEffect(Effect.JUMP).getAmplifier() + 1 : 0));
+
+        Block down = this.level.getBlock(this.floor().down());
+
+        if(down instanceof BlockHayBale) {
+            damage -= (damage * 0.8f);
+        }
+
         if (damage > 0) {
             if (!this.isPlayer || level.getGameRules().getBoolean(GameRule.FALL_DAMAGE)) {
                 this.attack(new EntityDamageEvent(this, DamageCause.FALL, damage));
@@ -1648,7 +1655,6 @@ public abstract class Entity extends Location implements Metadatable {
         }
 
         if (fallDistance > 0.75) {
-            Block down = this.level.getBlock(this.floor().down());
 
             if (down.getId() == Item.FARMLAND) {
                 Event ev;
