d7366c81868d1d6e6543811b40a64cc596a3b1a3
good777LUCKY <43497313+good777LUCKY@users.noreply.github.com>
2020-12-08 14:46:46 +0900

    Don't sign an empty book

--- src/main/java/cn/nukkit/Player.java
+++ src/main/java/cn/nukkit/Player.java
@@ -3695,7 +3695,7 @@ public class Player extends EntityHuman implements CommandSender, InventoryHolde
                         return;
                     }
 
-                    if (bookEditPacket.text.length() > 256) {
+                    if (bookEditPacket.text == null || bookEditPacket.text.length() > 256) {
                         return;
                     }
 
