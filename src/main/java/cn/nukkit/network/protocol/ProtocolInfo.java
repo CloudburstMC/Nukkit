81a74c25d9fbf5cf3f72f0c5a41432cb4788e6ba
Erik Miller <Meaglepvp@gmail.com>
2020-12-09 11:03:04 +0100

    initial 1.16.200 support (#1707)
    * fix for skin + geometry, for entityhumans
    
    * initial 1.16.200 support
    
    * change comment

--- src/main/java/cn/nukkit/network/protocol/ProtocolInfo.java
+++ src/main/java/cn/nukkit/network/protocol/ProtocolInfo.java
@@ -14,12 +14,12 @@ public interface ProtocolInfo {
      * Actual Minecraft: PE protocol version
      */
     @SuppressWarnings("UnnecessaryBoxing")
-    int CURRENT_PROTOCOL = Integer.valueOf("419"); // DO NOT REMOVE BOXING
+    int CURRENT_PROTOCOL = Integer.valueOf("422"); // DO NOT REMOVE BOXING
 
     List<Integer> SUPPORTED_PROTOCOLS = Ints.asList(CURRENT_PROTOCOL);
 
-    String MINECRAFT_VERSION = "v1.16.100";
-    String MINECRAFT_VERSION_NETWORK = "1.16.100";
+    String MINECRAFT_VERSION = "v1.16.200";
+    String MINECRAFT_VERSION_NETWORK = "1.16.200";
 
     byte LOGIN_PACKET = 0x01;
     byte PLAY_STATUS_PACKET = 0x02;
