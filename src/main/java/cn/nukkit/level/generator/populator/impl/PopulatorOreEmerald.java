d08683178045eadf18d802baac51a025aa03ed9d
good777LUCKY <43497313+good777LUCKY@users.noreply.github.com>
2020-12-17 18:08:03 +0900

    Improve and fix

--- src/main/java/cn/nukkit/level/generator/populator/impl/PopulatorOreEmerald.java
+++ src/main/java/cn/nukkit/level/generator/populator/impl/PopulatorOreEmerald.java
@@ -3,6 +3,7 @@ package cn.nukkit.level.generator.populator.impl;
 import cn.nukkit.level.ChunkManager;
 import cn.nukkit.level.format.FullChunk;
 import cn.nukkit.level.generator.populator.type.Populator;
+import cn.nukkit.math.NukkitMath;
 import cn.nukkit.math.NukkitRandom;
 
 import static cn.nukkit.block.BlockID.EMERALD_ORE;
@@ -17,11 +18,13 @@ public class PopulatorOreEmerald extends Populator {
     public void populate(ChunkManager level, int chunkX, int chunkZ, NukkitRandom random, FullChunk chunk) {
         int sourceX = chunkX << 4;
         int sourceZ = chunkZ << 4;
+        int endX = sourceX + 15;
+        int endZ = sourceZ + 15;
         
         for (int i = 0; i < 11; i++) {
-            int x = sourceX + random.nextInt(16);
-            int z = sourceZ + random.nextInt(16);
-            int y = random.nextInt(28) + 4;
+            int x = NukkitMath.randomRange(random, sourceX, endX);
+            int z = NukkitMath.randomRange(random, sourceZ, endZ);
+            int y = NukkitMath.randomRange(random, 4, 31);
             
             if (chunk.getBlockId(x, y, z) != STONE) {
                 continue;
