a9c77b69a4a608ab0858b9913899eddf4da67479
GoodLucky777 <43497313+GoodLucky777@users.noreply.github.com>
2021-01-07 20:33:04 +0900

    Fix height

--- src/main/java/cn/nukkit/level/generator/populator/impl/PopulatorOreEmerald.java
+++ src/main/java/cn/nukkit/level/generator/populator/impl/PopulatorOreEmerald.java
@@ -21,7 +21,7 @@ public class PopulatorOreEmerald extends Populator {
         for (int i = 0; i < 11; i++) {
             int x = random.nextBoundedInt(16);
             int z = random.nextBoundedInt(16);
-            int y = NukkitMath.randomRange(random, 4, 31);
+            int y = NukkitMath.randomRange(random, 0, 32);
             
             if (chunk.getBlockState(x, y, z) != STATE_STONE) {
                 continue;
