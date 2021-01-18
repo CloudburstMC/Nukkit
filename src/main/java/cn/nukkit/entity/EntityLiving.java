383bb8b9967f52221b2a8abbb912493c28e10839
Dima Uzyanov <uzyan0acc@gmail.com>
2021-01-18 23:42:52 +0500

    EntityLiving: Water breathing repeated after reuse turtle shell helmet (while in the water)

--- src/main/java/cn/nukkit/entity/EntityLiving.java
+++ src/main/java/cn/nukkit/entity/EntityLiving.java
@@ -216,7 +216,7 @@ public abstract class EntityLiving extends Entity implements EntityDamageable {
                     isBreathing = true;
                     turtleTicks--;
                 }
-            } else {
+            } else if(isBreathing) {
                 turtleTicks = 200;
             }
         }
