cea4263a323841c747bb08e72c0ae1b9149ccfe5
PetteriM1 <26197131+PetteriM1@users.noreply.github.com>
2021-01-31 10:20:29 +0200

    Tridents don't despawn on Bedrock Edition (#1747)

--- src/main/java/cn/nukkit/entity/projectile/EntityThrownTrident.java
+++ src/main/java/cn/nukkit/entity/projectile/EntityThrownTrident.java
@@ -147,11 +147,6 @@ public class EntityThrownTrident extends EntityProjectile {
             this.setCritical(false);
         }
 
-        if (this.age > 1200) {
-            this.close();
-            hasUpdate = true;
-        }
-
         this.timing.stopTiming();
 
         return hasUpdate;
