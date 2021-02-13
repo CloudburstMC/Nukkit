1953225608f2d14162274b68e8885735fc2492ba
Will <willscullythewag@gmail.com>
2021-02-13 11:53:09 +0000

    Move nametag behaviour into EntityCreature

--- src/main/java/cn/nukkit/entity/EntityCreature.java
+++ src/main/java/cn/nukkit/entity/EntityCreature.java
@@ -1,6 +1,9 @@
 package cn.nukkit.entity;
 
+import cn.nukkit.Player;
+import cn.nukkit.item.Item;
 import cn.nukkit.level.format.FullChunk;
+import cn.nukkit.math.Vector3;
 import cn.nukkit.nbt.tag.CompoundTag;
 
 /**
@@ -10,5 +13,18 @@ import cn.nukkit.nbt.tag.CompoundTag;
 public abstract class EntityCreature extends EntityLiving {
     public EntityCreature(FullChunk chunk, CompoundTag nbt) {
         super(chunk, nbt);
+    }
+
+    @Override
+    public boolean onInteract(Player player, Item item, Vector3 clickedPos) {
+        if (item.getId() == Item.NAME_TAG) {
+            if (item.hasCustomName()) {
+                this.setNameTag(item.getCustomName());
+                this.setNameTagVisible(true);
+                player.getInventory().removeItem(item);
+                return true;
+            }
+        }
+        return false;
     }
 }
--- src/main/java/cn/nukkit/item/ItemNameTag.java
+++ src/main/java/cn/nukkit/item/ItemNameTag.java
@@ -1,9 +1,5 @@
 package cn.nukkit.item;
 
-import cn.nukkit.entity.Entity;
-import cn.nukkit.entity.EntityHuman;
-import cn.nukkit.entity.mob.EntityEnderDragon;
-
 public class ItemNameTag extends Item {
 
     public ItemNameTag() {
@@ -18,17 +14,4 @@ public class ItemNameTag extends Item {
         super(NAME_TAG, meta, count, "Name Tag");
     }
 
-    @Override
-    public boolean useOn(Entity entity) {
-        if(this.hasCustomName()) {
-            if (!(entity instanceof EntityHuman || entity instanceof EntityEnderDragon)) {
-                entity.setNameTag(this.getCustomName());
-                entity.setNameTagVisible(true);
-
-                // Set entity as persistent? Correct me if that's already implemented.
-                return true;
-            }
-        }
-        return false;
-    }
 }
