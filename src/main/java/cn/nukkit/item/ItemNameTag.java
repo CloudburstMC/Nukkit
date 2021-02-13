7aa8f0c32b7f385cca9bcd14f0ab897e4735cf00
Will <willscullythewag@gmail.com>
2021-02-13 10:07:01 +0000

    Add ItemNameTag class

---
+++ src/main/java/cn/nukkit/item/ItemNameTag.java
@@ -0,0 +1,32 @@
+package cn.nukkit.item;
+
+import cn.nukkit.entity.Entity;
+import cn.nukkit.entity.EntityHuman;
+import cn.nukkit.entity.mob.EntityEnderDragon;
+
+public class ItemNameTag extends Item {
+
+    public ItemNameTag() {
+        this(0, 1);
+    }
+
+    public ItemNameTag(Integer meta) {
+        this(meta, 1);
+    }
+
+    public ItemNameTag(Integer meta, int count) {
+        super(NAME_TAG, meta, count, "Name Tag");
+    }
+
+    @Override
+    public boolean useOn(Entity entity) {
+        if(!(entity instanceof EntityHuman || entity instanceof EntityEnderDragon)){
+            entity.setNameTag(this.getName());
+            entity.setNameTagVisible(true);
+
+            // Set entity as persistent? Correct me if that's already implemented.
+            return true;
+        }
+        return false;
+    }
+}
