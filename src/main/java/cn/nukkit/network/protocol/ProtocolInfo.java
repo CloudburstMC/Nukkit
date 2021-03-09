56e5673b6888ef1d19ba6e9da470c30ff4f4954a
Alemiz112 <alex@mizerak.eu>
2021-03-09 13:15:31 +0100

    Update entity flags

--- src/main/java/cn/nukkit/network/protocol/ProtocolInfo.java
+++ src/main/java/cn/nukkit/network/protocol/ProtocolInfo.java
@@ -178,6 +178,7 @@ public interface ProtocolInfo {
     byte CORRECT_PLAYER_MOVE_PREDICTION_PACKET = (byte) 0xa1;
     byte ITEM_COMPONENT_PACKET = (byte) 0xa2;
     byte FILTER_TEXT_PACKET = (byte) 0xa3;
+    byte CLIENTBOUND_DEBUG_RENDERER_PACKET = (byte) 0xa4;
 
     byte BATCH_PACKET = (byte) 0xff;
 }
