385aa03990d633fee62dd61c1544666278067a6d
PetteriM1 <26197131+PetteriM1@users.noreply.github.com>
2021-03-20 00:05:27 +0200

    Also allow redstone wire to be placed on hopper

--- src/main/java/cn/nukkit/block/BlockRedstoneWire.java
+++ src/main/java/cn/nukkit/block/BlockRedstoneWire.java
@@ -220,7 +220,7 @@ public class BlockRedstoneWire extends BlockFlowable {
     public boolean canBePlacedOn(Vector3 v) {
         Block b = this.level.getBlock(v);
 
-        return b.isSolid() && !b.isTransparent() && b.getId() != Block.GLOWSTONE;
+        return (b.isSolid() && !b.isTransparent() && b.getId() != Block.GLOWSTONE) || b.getId() == HOPPER_BLOCK;
     }
 
     public int getStrongPower(BlockFace side) {
