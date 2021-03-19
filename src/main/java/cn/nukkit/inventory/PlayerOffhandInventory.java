24fc514cc32dc88e7cc56a014ebee56ba0e2e455
PetteriM1 <26197131+PetteriM1@users.noreply.github.com>
2021-03-20 00:20:53 +0200

    Verify offhand items

--- src/main/java/cn/nukkit/inventory/PlayerOffhandInventory.java
+++ src/main/java/cn/nukkit/inventory/PlayerOffhandInventory.java
@@ -4,12 +4,21 @@ import cn.nukkit.Player;
 import cn.nukkit.entity.EntityHuman;
 import cn.nukkit.entity.EntityHumanType;
 import cn.nukkit.item.Item;
+import cn.nukkit.item.ItemID;
 import cn.nukkit.network.protocol.InventoryContentPacket;
 import cn.nukkit.network.protocol.InventorySlotPacket;
 import cn.nukkit.network.protocol.MobEquipmentPacket;
 import cn.nukkit.network.protocol.types.ContainerIds;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class PlayerOffhandInventory extends BaseInventory {
+
+    /**
+     * Items that can be put to offhand inventory on Bedrock Edition
+     */
+    private static final List<Integer> OFFHAND_ITEMS = Arrays.asList(ItemID.SHIELD, ItemID.ARROW, ItemID.TOTEM, ItemID.MAP, ItemID.FIREWORKS, ItemID.NAUTILUS_SHELL, 442/*ItemID.SPARKLER*/);
 
     public PlayerOffhandInventory(EntityHumanType holder) {
         super(holder, InventoryType.OFFHAND);
@@ -78,5 +87,11 @@ public class PlayerOffhandInventory extends BaseInventory {
     @Override
     public EntityHuman getHolder() {
         return (EntityHuman) super.getHolder();
+    }
+
+    @Override
+    public boolean setItem(int index, Item item, boolean send) {
+        if (!OFFHAND_ITEMS.contains(item.getId())) return false;
+        return super.setItem(index, item, send);
     }
 }
