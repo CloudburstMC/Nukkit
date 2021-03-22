28776aa230a5ac10c4f40893d2ba8b5756240d20
joserobjr <joserobjr@gamemods.com.br>
2021-03-22 04:42:12 -0300

    Encapsulate ItemComponentPacket fields
    And adds annotations

--- src/main/java/cn/nukkit/network/protocol/ItemComponentPacket.java
+++ src/main/java/cn/nukkit/network/protocol/ItemComponentPacket.java
@@ -1,5 +1,7 @@
 package cn.nukkit.network.protocol;
 
+import cn.nukkit.api.PowerNukkitOnly;
+import cn.nukkit.api.Since;
 import cn.nukkit.nbt.NBTIO;
 import cn.nukkit.nbt.tag.CompoundTag;
 import cn.nukkit.utils.MainLogger;
@@ -13,11 +15,28 @@ import lombok.ToString;
  * @author GoodLucky777
  */
 @ToString
+@PowerNukkitOnly
+@Since("1.4.0.0-PN")
 public class ItemComponentPacket extends DataPacket {
-
+    @PowerNukkitOnly
+    @Since("1.4.0.0-PN")
     public static final byte NETWORK_ID = ProtocolInfo.ITEM_COMPONENT_PACKET;
     
-    public Entry[] entries = Entry.EMPTY_ARRAY;
+    @PowerNukkitOnly
+    @Since("1.4.0.0-PN")
+    private Entry[] entries = Entry.EMPTY_ARRAY;
+    
+    @PowerNukkitOnly
+    @Since("1.4.0.0-PN")
+    public void setEntries(Entry[] entries) {
+        this.entries = entries == null? null : entries.length == 0? Entries.EMPTY_ARRAY : entries.clone();
+    }
+    
+    @PowerNukkitOnly
+    @Since("1.4.0.0-PN")
+    public Entry[] getEntries() {
+        return entries == null? null : entries.length == 0? Entries.EMPTY_ARRAY : entries.clone();
+    }
     
     @Override
     public byte pid() {
@@ -44,21 +63,31 @@ public class ItemComponentPacket extends DataPacket {
     }
     
     @ToString
+    @PowerNukkitOnly
+    @Since("1.4.0.0-PN")
     public static class Entry {
+        @PowerNukkitOnly
+        @Since("1.4.0.0-PN")
         public static final Entry[] EMPTY_ARRAY = new Entry[0];
         
         private final String name;
         private final CompoundTag data;
         
+        @PowerNukkitOnly
+        @Since("1.4.0.0-PN")
         public Entry(String name, CompoundTag data) {
             this.name = name;
             this.data = data;
         }
         
+        @PowerNukkitOnly
+        @Since("1.4.0.0-PN")
         public String getName() {
             return name;
         }
         
+        @PowerNukkitOnly
+        @Since("1.4.0.0-PN")
         public CompoundTag getData() {
             return data;
         }
