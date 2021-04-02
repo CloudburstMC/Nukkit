a1355f6d3ce1940696fc612bcbcf7ad2b28423cb
Ragnok123 <gebab.urumci.kv@gmail.com>
2021-04-02 17:23:39 +0200

    Basic villager trades

--- src/main/java/cn/nukkit/entity/passive/EntityVillager.java
+++ src/main/java/cn/nukkit/entity/passive/EntityVillager.java
@@ -1,13 +1,28 @@
 package cn.nukkit.entity.passive;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import cn.nukkit.Player;
+import cn.nukkit.entity.Entity;
 import cn.nukkit.entity.EntityAgeable;
 import cn.nukkit.entity.EntityCreature;
+import cn.nukkit.inventory.Inventory;
+import cn.nukkit.inventory.InventoryHolder;
+import cn.nukkit.inventory.TradeInventory;
+import cn.nukkit.inventory.TradeInventoryRecipe;
+import cn.nukkit.item.Item;
 import cn.nukkit.level.format.FullChunk;
 import cn.nukkit.nbt.tag.CompoundTag;
+import cn.nukkit.nbt.tag.ListTag;
 
-public class EntityVillager extends EntityCreature implements EntityNPC, EntityAgeable {
+public class EntityVillager extends EntityCreature implements InventoryHolder, EntityNPC, EntityAgeable {
 
     public static final int NETWORK_ID = 115;
+	private TradeInventory inventory;
+	private List<TradeInventoryRecipe> recipes = new ArrayList<TradeInventoryRecipe>();
+	
+	private int tradeTier = 0;
 
     public EntityVillager(FullChunk chunk, CompoundTag nbt) {
         super(chunk, nbt);
@@ -43,7 +58,54 @@ public class EntityVillager extends EntityCreature implements EntityNPC, EntityA
     public void initEntity() {
         super.initEntity();
         this.setMaxHealth(20);
+        this.inventory = new TradeInventory(this);
     }
+    
+    public void setTradeTier(int tier) {
+    	this.tradeTier = tier;
+    }
+    
+    public int getTradeTier() {
+    	return this.tradeTier;
+    }
+    
+    @Override
+    public boolean onInteract(Player player, Item item) {
+    	if(recipes.size() > 0) {
+    		player.addWindow(this.getInventory());
+    		return true;
+    	}
+    	return false;
+    }
+    
+    public void addTradeRcipe(TradeInventoryRecipe recipe) {
+    	this.recipes.add(recipe);
+    }
+    
+	public CompoundTag getOffers() {
+		CompoundTag nbt = new CompoundTag();
+		nbt.putList(recipesToNbt());
+		nbt.putList(getTierExpRequirements());
+		return nbt;
+	}
+	
+	private ListTag<CompoundTag> recipesToNbt() {
+		ListTag<CompoundTag> tag = new ListTag<CompoundTag>("Recipes");
+		for(TradeInventoryRecipe recipe : this.recipes) {
+			tag.add(recipe.toNBT());
+		}
+		return tag;
+	}
+	
+	private ListTag<CompoundTag> getTierExpRequirements() {
+		ListTag<CompoundTag> tag = new ListTag<CompoundTag>("TierExpRequirements");
+		tag.add(new CompoundTag().putInt("0", 0));
+		tag.add(new CompoundTag().putInt("1", 10));
+		tag.add(new CompoundTag().putInt("2", 60));
+		tag.add(new CompoundTag().putInt("3", 160));
+		tag.add(new CompoundTag().putInt("4", 310));
+		return tag;
+	}
 
     public boolean isBaby() {
         return this.getDataFlag(DATA_FLAGS, DATA_FLAG_BABY);
@@ -53,4 +115,9 @@ public class EntityVillager extends EntityCreature implements EntityNPC, EntityA
         this.setDataFlag(DATA_FLAGS, DATA_FLAG_BABY, baby);
         this.setScale(baby ? 0.5f : 1);
     }
+
+	@Override
+	public Inventory getInventory() {
+		return this.inventory;
+	}
 }
---
+++ src/main/java/cn/nukkit/inventory/TradeInventory.java
@@ -0,0 +1,54 @@
+package cn.nukkit.inventory;
+
+import java.io.IOException;
+import java.nio.ByteOrder;
+
+import cn.nukkit.Player;
+import cn.nukkit.entity.passive.EntityVillager;
+import cn.nukkit.item.Item;
+import cn.nukkit.nbt.NBTIO;
+import cn.nukkit.network.protocol.UpdateTradePacket;
+
+public class TradeInventory extends BaseInventory {
+
+	public TradeInventory(InventoryHolder holder) {
+		super(holder, InventoryType.TRADING);
+	}
+	
+	public void onOpen(Player who) {
+		super.onOpen(who);
+		
+		UpdateTradePacket pk = new UpdateTradePacket();
+		pk.windowId = (byte) who.getWindowId(this);
+		pk.windowType = (byte) InventoryType.TRADING.getNetworkType();
+		pk.isWilling = true;
+		pk.screen2 = true;
+		pk.trader = this.getHolder().getId();
+		pk.tradeTier = this.getHolder().getTradeTier();
+		pk.player = who.getId();
+		try {
+			pk.offers = NBTIO.write(this.getHolder().getOffers(),ByteOrder.LITTLE_ENDIAN);
+		} catch(IOException ex) {}
+		
+		who.dataPacket(pk);
+	}
+	
+	public void onClose(Player who) {
+		for(int i = 0; i <= 1; i++) {
+			Item item = getItem(i);
+			if(who.getInventory().canAddItem(item)) {
+				who.getInventory().addItem(item);
+			} else {
+				who.dropItem(item);
+			}
+			this.clear(i);
+		}
+		
+		super.onClose(who);
+	}
+	
+	public EntityVillager getHolder() {
+		return (EntityVillager) this.holder;
+	}
+
+}
