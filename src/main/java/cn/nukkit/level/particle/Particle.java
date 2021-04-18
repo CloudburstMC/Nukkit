ce09b0b03c7326867d676539beae8cbe5eb5fa52
Wodor <17339354+wode490390@users.noreply.github.com>
2021-04-18 23:36:25 +0800

    Update particle IDs

--- src/main/java/cn/nukkit/level/particle/Particle.java
+++ src/main/java/cn/nukkit/level/particle/Particle.java
@@ -12,7 +12,7 @@ import java.lang.reflect.Field;
 public abstract class Particle extends Vector3 {
 
     public static final int TYPE_BUBBLE = 1;
-    // 2 same as 1
+    public static final int TYPE_BUBBLE_MANUAL = 2;
     public static final int TYPE_CRITICAL = 3;
     public static final int TYPE_BLOCK_FORCE_FIELD = 4;
     public static final int TYPE_SMOKE = 5;
@@ -23,7 +23,6 @@ public abstract class Particle extends Vector3 {
     public static final int TYPE_LARGE_SMOKE = 10;
     public static final int TYPE_REDSTONE = 11;
     public static final int TYPE_RISING_RED_DUST = 12;
-
     public static final int TYPE_ITEM_BREAK = 13;
     public static final int TYPE_SNOWBALL_POOF = 14;
     public static final int TYPE_HUGE_EXPLODE = 15;
@@ -32,60 +31,70 @@ public abstract class Particle extends Vector3 {
     public static final int TYPE_HEART = 18;
     public static final int TYPE_TERRAIN = 19;
     public static final int TYPE_SUSPENDED_TOWN = 20, TYPE_TOWN_AURA = 20;
-    // 61 same as 20
     public static final int TYPE_PORTAL = 21;
     // 22 same as 21
     public static final int TYPE_SPLASH = 23, TYPE_WATER_SPLASH = 23;
-    // 24 same as 23
+    public static final int TYPE_WATER_SPLASH_MANUAL = 24;
     public static final int TYPE_WATER_WAKE = 25;
     public static final int TYPE_DRIP_WATER = 26;
     public static final int TYPE_DRIP_LAVA = 27;
     public static final int TYPE_DRIP_HONEY = 28;
-    public static final int TYPE_FALLING_DUST = 29, TYPE_DUST = 29;
-    public static final int TYPE_MOB_SPELL = 30;
-    public static final int TYPE_MOB_SPELL_AMBIENT = 31;
-    public static final int TYPE_MOB_SPELL_INSTANTANEOUS = 32;
-    public static final int TYPE_NOTE_AND_DUST = 33;
+    public static final int TYPE_STALACTITE_DRIP_WATER = 29;
+    public static final int TYPE_STALACTITE_DRIP_LAVA = 30;
+    public static final int TYPE_FALLING_DUST = 31, TYPE_DUST = 31;
+    public static final int TYPE_MOB_SPELL = 32;
+    public static final int TYPE_MOB_SPELL_AMBIENT = 33;
+    public static final int TYPE_MOB_SPELL_INSTANTANEOUS = 34;
+    public static final int TYPE_INK = 35;
+    public static final int TYPE_SLIME = 36;
+    public static final int TYPE_RAIN_SPLASH = 37;
+    public static final int TYPE_VILLAGER_ANGRY = 38;
+    public static final int TYPE_VILLAGER_HAPPY = 39;
+    public static final int TYPE_ENCHANTMENT_TABLE = 40;
+    public static final int TYPE_TRACKING_EMITTER = 41;
+    public static final int TYPE_NOTE = 42;
+    public static final int TYPE_WITCH_SPELL = 43;
+    public static final int TYPE_CARROT = 44;
+    public static final int TYPE_MOB_APPEARANCE  = 45;
+    public static final int TYPE_END_ROD = 46;
+    public static final int TYPE_RISING_DRAGONS_BREATH = 47;
+    public static final int TYPE_SPIT = 48;
+    public static final int TYPE_TOTEM = 49;
+    public static final int TYPE_FOOD = 50;
+    public static final int TYPE_FIREWORKS_STARTER = 51;
+    public static final int TYPE_FIREWORKS_SPARK = 52;
+    public static final int TYPE_FIREWORKS_OVERLAY = 53;
+    public static final int TYPE_BALLOON_GAS = 54;
+    public static final int TYPE_COLORED_FLAME = 55;
+    public static final int TYPE_SPARKLER = 56;
+    public static final int TYPE_CONDUIT = 57;
+    public static final int TYPE_BUBBLE_COLUMN_UP = 58;
+    public static final int TYPE_BUBBLE_COLUMN_DOWN = 59;
+    public static final int TYPE_SNEEZE = 60;
+    public static final int TYPE_SHULKER_BULLET = 61;
+    public static final int TYPE_BLEACH = 62;
+    public static final int TYPE_LARGE_EXPLOSION = 63;
+    public static final int TYPE_MYCELIUM_DUST = 64;
+    public static final int TYPE_FALLING_RED_DUST = 65;
+    public static final int TYPE_CAMPFIRE_SMOKE = 66;
+    public static final int TYPE_TALL_CAMPFIRE_SMOKE = 67;
+    public static final int TYPE_FALLING_DRAGONS_BREATH = 68;
+    public static final int TYPE_DRAGONS_BREATH = 69;
+    public static final int TYPE_BLUE_FLAME = 70;
+    public static final int TYPE_SOUL = 71;
+    public static final int TYPE_OBSIDIAN_TEAR = 72;
+    public static final int TYPE_PORTAL_REVERSE = 73;
+    public static final int TYPE_SNOWFLAKE = 74;
+    public static final int TYPE_VIBRATION_SIGNAL = 75;
+    public static final int TYPE_SCULK_SENSOR_REDSTONE = 76;
+    public static final int TYPE_SPORE_BLOSSOM_SHOWER = 77;
+    public static final int TYPE_SPORE_BLOSSOM_AMBIENT = 78;
+    public static final int TYPE_WAX = 79;
+    public static final int TYPE_ELECTRIC_SPARK = 80;
 
-    public static final int TYPE_SLIME = 34;
-    public static final int TYPE_RAIN_SPLASH = 35;
-    public static final int TYPE_VILLAGER_ANGRY = 36;
-    // 60 same as 36
-    public static final int TYPE_VILLAGER_HAPPY = 37;
-    public static final int TYPE_ENCHANTMENT_TABLE = 38;
-    public static final int TYPE_TRACKING_EMITTER = 39;
-    public static final int TYPE_NOTE = 40;
-    public static final int TYPE_WITCH_SPELL = 41;
-    public static final int TYPE_CARROT = 42;
-    // 43 unknown
-    public static final int TYPE_END_ROD = 44;
-    // 59 same as 44
-    public static final int TYPE_RISING_DRAGONS_BREATH = 45;
-    public static final int TYPE_SPIT = 46;
-    public static final int TYPE_TOTEM = 47;
-    public static final int TYPE_FOOD = 48;
-    public static final int TYPE_FIREWORKS_STARTER = 49;
-    public static final int TYPE_FIREWORKS_SPARK = 50;
-    public static final int TYPE_FIREWORKS_OVERLAY = 51;
-    public static final int TYPE_BALLOON_GAS = 52;
-    public static final int TYPE_COLORED_FLAME = 53;
-    public static final int TYPE_SPARKLER = 54;
-    public static final int TYPE_CONDUIT = 55;
-    public static final int TYPE_BUBBLE_COLUMN_UP = 56;
-    public static final int TYPE_BUBBLE_COLUMN_DOWN = 57;
-    public static final int TYPE_SNEEZE = 58;
-
-    public static final int TYPE_LARGE_EXPLOSION = 61;
-    public static final int TYPE_INK = 62;
-    public static final int TYPE_FALLING_RED_DUST = 63;
-    public static final int TYPE_CAMPFIRE_SMOKE = 64;
-    //65 same as 64
-    public static final int TYPE_FALLING_DRAGONS_BREATH = 66;
-    public static final int TYPE_DRAGONS_BREATH = 67;
-    
     public static final Integer getParticleIdByName(String name) {
         name = name.toUpperCase();
-        
+
         try {
             Field field = Particle.class.getField((name.startsWith("TYPE_") == true ? name : ("TYPE_" + name)));
 
@@ -99,11 +108,11 @@ public abstract class Particle extends Vector3 {
         }
         return null;
     }
-    
+
     public static final boolean particleExists(String name) {
         return getParticleIdByName(name) != null;   
     }
-    
+
     public Particle() {
         super(0, 0, 0);
     }
