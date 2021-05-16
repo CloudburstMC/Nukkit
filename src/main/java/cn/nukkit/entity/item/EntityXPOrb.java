ec95157df33314f19bcdead7e25dfc779266976b
AntonPPDS <60366294+AntonPPDS@users.noreply.github.com>
2021-05-16 22:19:42 +0300

    Check for player is spawned and alive

--- src/main/java/cn/nukkit/entity/item/EntityXPOrb.java
+++ src/main/java/cn/nukkit/entity/item/EntityXPOrb.java
@@ -146,7 +146,7 @@ public class EntityXPOrb extends Entity {
                 this.closestPlayer = null;
                 double closestDistance = 0.0D;
                 for (Player p : this.getViewers().values()) {
-                    if (!p.isSpectator()) {
+                    if (!p.isSpectator() && p.spawned && p.isAlive()) {
                         double d = p.distanceSquared(this);
                         if (d <= 64.0D && (this.closestPlayer == null || d < closestDistance)) {
                             this.closestPlayer = p;
@@ -156,7 +156,7 @@ public class EntityXPOrb extends Entity {
                 }
             }
 
-            if (this.closestPlayer != null && this.closestPlayer.isSpectator()) {
+            if (this.closestPlayer != null && (this.closestPlayer.isSpectator() || !this.closestPlayer.spawned || !this.closestPlayer.isAlive())) {
                 this.closestPlayer = null;
             }
 
