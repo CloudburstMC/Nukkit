f9e9c9519e07f68e4dca57b1e500127ab7159458
joserobjr <joserobjr@powernukkit.org>
2021-05-16 23:43:11 -0300

    #648 Adds more annotations

--- src/main/java/cn/nukkit/inventory/InventoryType.java
+++ src/main/java/cn/nukkit/inventory/InventoryType.java
@@ -25,7 +25,7 @@ public enum InventoryType {
     //HORSE typeId:12
     //JUKEBOX typeId:17
     UI(1, "UI", -1),
-    CURSOR(1, "Cursor", -1),
+    @PowerNukkitOnly CURSOR(1, "Cursor", -1),
     SHULKER_BOX(27, "Shulker Box", 0),
     BEACON(1, "Beacon", 13),
     // 14 STRUCTURE_EDITOR
@@ -39,19 +39,19 @@ public enum InventoryType {
     // 25 LECTERN
     // 31 HUD
     // 32 JIGSAW_EDITOR
-    GRINDSTONE(3, "Grindstone", 26),
-    BLAST_FURNACE(3, "Blast Furnace", 27),
-    SMOKER(3, "Smoker", 28),
-    STONECUTTER(2, "Stonecutter", 29), // Should be 29 but it's totally driven by the client, so setting to -1 = UI
-    CARTOGRAPHY(3, "Cartography Table", 30),
-    BARREL(27, "Barrel", 0),
-    CAMPFIRE(4, "Campfire", -9), // -9 = NONE
+    @PowerNukkitOnly GRINDSTONE(3, "Grindstone", 26),
+    @PowerNukkitOnly BLAST_FURNACE(3, "Blast Furnace", 27),
+    @PowerNukkitOnly SMOKER(3, "Smoker", 28),
+    @PowerNukkitOnly STONECUTTER(2, "Stonecutter", 29), // Should be 29 but it's totally driven by the client, so setting to -1 = UI
+    @PowerNukkitOnly CARTOGRAPHY(3, "Cartography Table", 30),
+    @PowerNukkitOnly BARREL(27, "Barrel", 0),
+    @PowerNukkitOnly CAMPFIRE(4, "Campfire", -9), // -9 = NONE
     @PowerNukkitOnly @Since("1.4.0.0-PN") ENTITY_EQUIPMENT(36, "Entity Equipment", -1), //36 CONTAINER
     @PowerNukkitOnly @Since("1.4.0.0-PN") ENTITY_ARMOR(4, "Entity Armor", -1), //4 ARMOR
     MINECART_CHEST(27, "Minecart with Chest", 0), // Should be 10
     MINECART_HOPPER(5, "Minecart with Hopper", 8), // Should be 11
     OFFHAND(1, "Offhand", -1),
-    SMITHING_TABLE(2, "Smithing Table", 33);
+    @PowerNukkitOnly @Since("1.4.0.0-PN") SMITHING_TABLE(2, "Smithing Table", 33);
 
     private final int size;
     private final String title;
