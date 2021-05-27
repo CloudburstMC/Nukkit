77c1e3a53653d6ecb7db7e69075f56bd794e784d
GoodLucky777 <43497313+GoodLucky777@users.noreply.github.com>
2021-05-27 20:55:24 +0900

    fix import

--- src/main/java/cn/nukkit/block/BlockSponge.java
+++ src/main/java/cn/nukkit/block/BlockSponge.java
@@ -1,6 +1,7 @@
 package cn.nukkit.block;
 
 import cn.nukkit.Player;
+import cn.nukkit.blockstate.BlockState;
 import cn.nukkit.item.Item;
 import cn.nukkit.item.ItemTool;
 import cn.nukkit.level.GlobalBlockPalette;
