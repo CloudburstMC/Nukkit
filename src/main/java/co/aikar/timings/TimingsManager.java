37689f047d12e4150f7f93c07380ef579d9d8038
joserobjr <joserobjr@powernukkit.org>
2021-06-24 21:45:21 -0300

    Change the timeings map

--- src/main/java/co/aikar/timings/TimingsManager.java
+++ src/main/java/co/aikar/timings/TimingsManager.java
@@ -26,9 +26,10 @@ package co.aikar.timings;
 import cn.nukkit.Server;
 
 import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class TimingsManager {
-    static final Map<TimingIdentifier, Timing> TIMING_MAP = Collections.synchronizedMap(new HashMap<>(256, 0.5f));
+    static final Map<TimingIdentifier, Timing> TIMING_MAP = new ConcurrentHashMap<>();//Collections.synchronizedMap(new HashMap<>(256, 0.5f));
 
     static final Queue<Timing> TIMINGS = new ArrayDeque<>();
     static final ArrayDeque<TimingsHistory.MinuteReport> MINUTE_REPORTS = new ArrayDeque<>();
