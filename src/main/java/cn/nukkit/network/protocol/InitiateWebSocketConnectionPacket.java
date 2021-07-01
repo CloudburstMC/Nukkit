0049d3d7451715fd2d28d9694e6936d2793bf379
IWareQ <dimonpo96@mail.ru>
2021-07-01 11:49:04 +0500

    Revert "Update InitiateWebSocketConnectionPacket"
    This reverts commit ee8c1a557551732234f049e5cd7f1803f25f0d68.

--- src/main/java/cn/nukkit/network/protocol/InitiateWebSocketConnectionPacket.java
+++ src/main/java/cn/nukkit/network/protocol/InitiateWebSocketConnectionPacket.java
@@ -5,8 +5,6 @@ import lombok.ToString;
 @ToString
 public class InitiateWebSocketConnectionPacket extends DataPacket {
 
-	public String serverUri;
-
     @Override
     public byte pid() {
         return ProtocolInfo.INITIATE_WEB_SOCKET_CONNECTION_PACKET;
@@ -14,12 +12,11 @@ public class InitiateWebSocketConnectionPacket extends DataPacket {
 
     @Override
     public void decode() {
-    	this.serverUri = this.getString();
+
     }
 
     @Override
     public void encode() {
-        this.reset();
-        this.putString(this.serverUri);
+        //TODO
     }
 }
