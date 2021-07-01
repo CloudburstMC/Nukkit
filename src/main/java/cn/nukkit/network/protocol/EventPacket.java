a9b0d66f4b06a681836abe63b404a84584bcd81f
IWareQ <dimonpo96@mail.ru>
2021-07-01 11:49:23 +0500

    Revert "Update EventPacket"
    This reverts commit 4ca014eac0bd1189c2a44490383f5f0db816cea2.

--- src/main/java/cn/nukkit/network/protocol/EventPacket.java
+++ src/main/java/cn/nukkit/network/protocol/EventPacket.java
@@ -5,31 +5,31 @@ import lombok.ToString;
 @ToString
 public class EventPacket extends DataPacket {
 
-	public static final byte TYPE_ACHIEVEMENT_AWARDED = 0;
-	public static final byte TYPE_ENTITY_INTERACT = 1;
-	public static final byte TYPE_PORTAL_BUILT = 2;
-	public static final byte TYPE_PORTAL_USED = 3;
-	public static final byte TYPE_MOB_KILLED = 4;
-	public static final byte TYPE_CAULDRON_USED = 5;
-	public static final byte TYPE_PLAYER_DEATH = 6;
-	public static final byte TYPE_BOSS_KILLED = 7;
-	public static final byte TYPE_AGENT_COMMAND = 8;
-	public static final byte TYPE_AGENT_CREATED = 9;
-	public static final byte TYPE_PATTERN_REMOVED = 10; //Idk
-	public static final byte TYPE_COMMANED_EXECUTED = 11;
-	public static final byte TYPE_FISH_BUCKETED = 12;
-	public static final byte TYPE_MOB_BORN = 13;
-	public static final byte TYPE_PET_DIED = 14;
-	public static final byte TYPE_CAULDRON_BLOCK_USED = 15;
-	public static final byte TYPE_COMPOSTER_BLOCK_USED = 16;
-	public static final byte TYPE_BELL_BLOCK_USED = 17;
-	public static final byte TYPE_ACTOR_DEFINITION = 18;
-	public static final byte TYPE_RAID_UPDATE = 19;
-	public static final byte TYPE_PLAYER_MOVEMENT_ANOMALY = 20; //Anti-cheat
-	public static final byte TYPE_PLAYER_MOVEMENT_CORRECTED = 21;
-	public static final byte TYPE_HONEY_HARVESTED = 22;
-	public static final byte TYPE_TARGET_BLOCK_HIT = 23;
-	public static final byte TYPE_PIGLIN_BARTER = 24;
+	public static final  byte TYPE_ACHIEVEMENT_AWARDED = 0;
+	public static final  byte TYPE_ENTITY_INTERACT = 1;
+	public static final  byte TYPE_PORTAL_BUILT = 2;
+	public static final  byte TYPE_PORTAL_USED = 3;
+	public static final  byte TYPE_MOB_KILLED = 4;
+	public static final  byte TYPE_CAULDRON_USED = 5;
+	public static final  byte TYPE_PLAYER_DEATH = 6;
+	public static final  byte TYPE_BOSS_KILLED = 7;
+	public static final  byte TYPE_AGENT_COMMAND = 8;
+	public static final  byte TYPE_AGENT_CREATED = 9;
+	public static final  byte TYPE_PATTERN_REMOVED = 10; //Idk
+	public static final  byte TYPE_COMMANED_EXECUTED = 11;
+	public static final  byte TYPE_FISH_BUCKETED = 12;
+	public static final  byte TYPE_MOB_BORN = 13;
+	public static final  byte TYPE_PET_DIED = 14;
+	public static final  byte TYPE_CAULDRON_BLOCK_USED = 15;
+	public static final  byte TYPE_COMPOSTER_BLOCK_USED = 16;
+	public static final  byte TYPE_BELL_BLOCK_USED = 17;
+	public static final  byte TYPE_ACTOR_DEFINITION = 18;
+	public static final  byte TYPE_RAID_UPDATE = 19;
+	public static final  byte TYPE_PLAYER_MOVEMENT_ANOMALY = 20; //Anti-cheat
+	public static final  byte TYPE_PLAYER_MOVEMENT_CORRECTED = 21;
+	public static final  byte TYPE_HONEY_HARVESTED = 22;
+	public static final  byte TYPE_TARGET_BLOCK_HIT = 23;
+	public static final  byte TYPE_PIGLIN_BARTER = 24;
 
     public long playerId;
     public int eventData;
