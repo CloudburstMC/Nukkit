5385da9f58b26162d0f5c8a1748ce2e6108b0b8e
IWareQ <dimonpo96@mail.ru>
2021-07-01 11:48:24 +0500

    Revert "Update SetCommandsEnabledPacket"
    This reverts commit f7dceb3cf5d24000c9f620f0aba38830ce60f71a.

--- src/main/java/cn/nukkit/network/protocol/SetCommandsEnabledPacket.java
+++ src/main/java/cn/nukkit/network/protocol/SetCommandsEnabledPacket.java
@@ -5,16 +5,18 @@ import lombok.ToString;
 @ToString
 public class SetCommandsEnabledPacket extends DataPacket {
 
+    public static final byte NETWORK_ID = ProtocolInfo.SET_COMMANDS_ENABLED_PACKET;
+
     public boolean enabled;
 
     @Override
     public byte pid() {
-        return ProtocolInfo.SET_COMMANDS_ENABLED_PACKET;
+        return NETWORK_ID;
     }
 
     @Override
     public void decode() {
-    	this.enabled = this.getBoolean();
+
     }
 
     @Override
