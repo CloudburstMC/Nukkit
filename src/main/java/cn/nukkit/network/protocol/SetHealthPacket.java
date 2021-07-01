a1779b164821b9345e253e6891290bfc48425172
IWareQ <dimonpo96@mail.ru>
2021-07-01 11:48:27 +0500

    Revert "Update SetHealthPacket"
    This reverts commit 5580b0bff10577f4c1fd2b5a2aa1f0262a0c50b9.

--- src/main/java/cn/nukkit/network/protocol/SetHealthPacket.java
+++ src/main/java/cn/nukkit/network/protocol/SetHealthPacket.java
@@ -5,21 +5,23 @@ import lombok.ToString;
 @ToString
 public class SetHealthPacket extends DataPacket {
 
+    public static final byte NETWORK_ID = ProtocolInfo.SET_HEALTH_PACKET;
+
     public int health;
 
     @Override
     public byte pid() {
-        return ProtocolInfo.SET_HEALTH_PACKET;
+        return NETWORK_ID;
     }
 
     @Override
     public void decode() {
-    	this.health = this.getVarInt();
+
     }
 
     @Override
     public void encode() {
         this.reset();
-        this.putVarInt(this.health);
+        this.putUnsignedVarInt(this.health);
     }
 }
