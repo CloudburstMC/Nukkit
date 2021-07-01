fcf5f6b1b8e47165e69c3ee7c599b764490e6433
IWareQ <dimonpo96@mail.ru>
2021-07-01 11:49:27 +0500

    Revert "Update StopSoundPacket"
    This reverts commit 31b4f836852ae18d4dea18dc28ce88c4f70a7b03.

--- src/main/java/cn/nukkit/network/protocol/StopSoundPacket.java
+++ src/main/java/cn/nukkit/network/protocol/StopSoundPacket.java
@@ -5,24 +5,25 @@ import lombok.ToString;
 @ToString
 public class StopSoundPacket extends DataPacket {
 
-    public String soundName;
+    public static final byte NETWORK_ID = ProtocolInfo.STOP_SOUND_PACKET;
+
+    public String name;
     public boolean stopAll;
 
     @Override
     public byte pid() {
-        return ProtocolInfo.STOP_SOUND_PACKET;
+        return NETWORK_ID;
     }
 
     @Override
     public void decode() {
-    	this.soundName = this.getString();
-        this.stopAll = this.getBoolean();
+
     }
 
     @Override
     public void encode() {
         this.reset();
-        this.putString(this.soundName);
+        this.putString(this.name);
         this.putBoolean(this.stopAll);
     }
 }
