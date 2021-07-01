ad8adafb163819d8382b206fdb4509a4f8ee81d0
IWareQ <dimonpo96@mail.ru>
2021-07-01 11:49:35 +0500

    Revert "Update SimpleEventPacket"
    This reverts commit f709e4a34c0ece25360130fca970d0383c0c723f.

--- src/main/java/cn/nukkit/network/protocol/SimpleEventPacket.java
+++ src/main/java/cn/nukkit/network/protocol/SimpleEventPacket.java
@@ -5,11 +5,7 @@ import lombok.ToString;
 @ToString
 public class SimpleEventPacket extends DataPacket {
 
-	public static final short TYPE_ENABLE_COMMANDS = 1;
-	public static final short TYPE_DISABLE_COMMANDS = 2;
-	public static final short TYPE_UNLOCK_WORLD_TEMPLATE_SETTINGS = 3;
-
-    public short eventId;
+    public short unknown;
 
     @Override
     public byte pid() {
@@ -18,12 +14,12 @@ public class SimpleEventPacket extends DataPacket {
 
     @Override
     public void decode() {
-    	this.eventId = this.getShort();
+
     }
 
     @Override
     public void encode() {
         this.reset();
-        this.putShort(this.eventId);
+        this.putShort(this.unknown);
     }
 }
