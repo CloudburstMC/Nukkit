0d0eb2d7e5153a2b1e3b5f1590319eddd82c93af
IWareQ <dimonpo96@mail.ru>
2021-07-02 15:08:13 +0500

    Update ResourcePackClientResponsePacket

--- src/main/java/cn/nukkit/network/protocol/ResourcePackClientResponsePacket.java
+++ src/main/java/cn/nukkit/network/protocol/ResourcePackClientResponsePacket.java
@@ -9,19 +9,20 @@ public class ResourcePackClientResponsePacket extends DataPacket {
 
     public static final byte NETWORK_ID = ProtocolInfo.RESOURCE_PACK_CLIENT_RESPONSE_PACKET;
 
-    public static final byte STATUS_REFUSED = 1;
-    public static final byte STATUS_SEND_PACKS = 2;
-    public static final byte STATUS_HAVE_ALL_PACKS = 3;
-    public static final byte STATUS_COMPLETED = 4;
-
-    public byte responseStatus;
+    public ResponseStatus responseStatus;
     public Entry[] packEntries;
 
     @Override
+    public byte pid() {
+        return NETWORK_ID;
+    }
+
+    @Override
     public void decode() {
-        this.responseStatus = (byte) this.getByte();
-        this.packEntries = new Entry[this.getLShort()];
-        for (int i = 0; i < this.packEntries.length; i++) {
+        this.responseStatus = ResponseStatus.values()[this.getByte() - 1];
+        int count = this.getLShort();
+        this.packEntries = new Entry[count];
+        for (int i = 0; i < count; i++) {
             String[] entry = this.getString().split("_");
             this.packEntries[i] = new Entry(UUID.fromString(entry[0]), entry[1]);
         }
@@ -30,20 +31,16 @@ public class ResourcePackClientResponsePacket extends DataPacket {
     @Override
     public void encode() {
         this.reset();
-        this.putByte(this.responseStatus);
+        this.putByte((byte) (this.responseStatus.ordinal() + 1));
         this.putLShort(this.packEntries.length);
         for (Entry entry : this.packEntries) {
             this.putString(entry.uuid.toString() + '_' + entry.version);
         }
     }
 
-    @Override
-    public byte pid() {
-        return NETWORK_ID;
-    }
-
     @ToString
     public static class Entry {
+
         public final UUID uuid;
         public final String version;
 
@@ -51,5 +48,13 @@ public class ResourcePackClientResponsePacket extends DataPacket {
             this.uuid = uuid;
             this.version = version;
         }
+    }
+
+    public static enum ResponseStatus {
+
+        REFUSED,
+        SEND_PACKS,
+        HAVE_ALL_PACKS,
+        COMPLETED
     }
 }
