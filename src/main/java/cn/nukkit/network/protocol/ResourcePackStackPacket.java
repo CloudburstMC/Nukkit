ac82104a79918071d1670661e1e25ba01609d8cb
IWareQ <dimonpo96@mail.ru>
2021-07-02 15:01:22 +0500

    Update ResourcePackStackPacket

--- src/main/java/cn/nukkit/network/protocol/ResourcePackStackPacket.java
+++ src/main/java/cn/nukkit/network/protocol/ResourcePackStackPacket.java
@@ -8,43 +8,77 @@ public class ResourcePackStackPacket extends DataPacket {
 
     public static final byte NETWORK_ID = ProtocolInfo.RESOURCE_PACK_STACK_PACKET;
 
-    public boolean mustAccept = false;
+    public boolean mustAccept;
     public ResourcePack[] behaviourPackStack = new ResourcePack[0];
     public ResourcePack[] resourcePackStack = new ResourcePack[0];
-    public boolean isExperimental = false;
     public String gameVersion = ProtocolInfo.MINECRAFT_VERSION_NETWORK;
+    public Experiment[] experiments = new Experiment[0];
+    public boolean hasPreviouslyUsedExperiments;
+
+    @Override
+    public byte pid() {
+        return NETWORK_ID;
+    }
 
     @Override
     public void decode() {
-
+        this.mustAccept = this.getBoolean();
+        int count = (int) this.getUnsignedVarInt();
+        this.behaviourPackStack = new ResourcePack[count];
+        for (int i = 0; i < count; i++) {
+            this.getString();
+            this.getString();
+            this.getString();
+            //TODO
+        }
+        count = (int) this.getUnsignedVarInt();
+        this.resourcePackStack = new ResourcePack[count];
+        for (int i = 0; i < count; i++) {
+            this.getString();
+            this.getString();
+            this.getString();
+            //TODO
+        }
+        this.gameVersion = this.getString();
+        count = this.getLInt();
+        this.experiments = new Experiment[count];
+        for (int i = 0; i < count; i++) {
+            Experiment experiment = new Experiment();
+            experiment.experimentName = this.getString();
+            experiment.enabled = this.getBoolean();
+            this.experiments[i] = experiment;
+        }
+        this.hasPreviouslyUsedExperiments = this.getBoolean();
     }
 
     @Override
     public void encode() {
         this.reset();
         this.putBoolean(this.mustAccept);
-
         this.putUnsignedVarInt(this.behaviourPackStack.length);
-        for (ResourcePack entry : this.behaviourPackStack) {
-            this.putString(entry.getPackId().toString());
-            this.putString(entry.getPackVersion());
+        for (ResourcePack resourcePack : this.behaviourPackStack) {
+            this.putString(resourcePack.getPackId().toString());
+            this.putString(resourcePack.getPackVersion());
             this.putString(""); //TODO: subpack name
         }
-
         this.putUnsignedVarInt(this.resourcePackStack.length);
-        for (ResourcePack entry : this.resourcePackStack) {
-            this.putString(entry.getPackId().toString());
-            this.putString(entry.getPackVersion());
+        for (ResourcePack resourcePack : this.resourcePackStack) {
+            this.putString(resourcePack.getPackId().toString());
+            this.putString(resourcePack.getPackVersion());
             this.putString(""); //TODO: subpack name
         }
-
         this.putString(this.gameVersion);
-        this.putLInt(0); // Experiments length
-        this.putBoolean(false); // Were experiments previously toggled
+        this.putLInt(this.experiments.length);
+        for (Experiment experiment : this.experiments) {
+            this.putString(experiment.experimentName);
+            this.putBoolean(experiment.enabled);
+        }
+        this.putBoolean(hasPreviouslyUsedExperiments);
     }
 
-    @Override
-    public byte pid() {
-        return NETWORK_ID;
+    public static class Experiment {
+
+        public String experimentName;
+        public boolean enabled;
     }
 }
