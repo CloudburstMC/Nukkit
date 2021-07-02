a175991d3c85f93baaaedff3bcd1d547683ca955
IWareQ <dimonpo96@mail.ru>
2021-07-02 14:49:25 +0500

    Update ServerToClientHandshakePacket

--- src/main/java/cn/nukkit/network/protocol/ServerToClientHandshakePacket.java
+++ src/main/java/cn/nukkit/network/protocol/ServerToClientHandshakePacket.java
@@ -5,22 +5,23 @@ import lombok.ToString;
 @ToString
 public class ServerToClientHandshakePacket extends DataPacket {
 
+    public static final byte NETWORK_ID = ProtocolInfo.SERVER_TO_CLIENT_HANDSHAKE_PACKET;
+
+    public String jwt;
+
     @Override
     public byte pid() {
-        return ProtocolInfo.SERVER_TO_CLIENT_HANDSHAKE_PACKET;
+        return NETWORK_ID;
     }
-
-    public String publicKey;
-    public String serverToken;
-    public String privateKey;
 
     @Override
     public void decode() {
-
+        this.jwt = this.getString();
     }
 
     @Override
     public void encode() {
-        //TODO
+        this.reset();
+        this.putString(this.jwt);
     }
 }
