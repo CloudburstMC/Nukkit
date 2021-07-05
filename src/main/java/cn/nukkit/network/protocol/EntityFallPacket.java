2ecf329865bbed5c7ee34a9891985579eeed4e2e
IWareQ <dimonpo96@mail.ru>
2021-07-05 10:57:30 +0500

    Update EntityFallPacket

--- src/main/java/cn/nukkit/network/protocol/EntityFallPacket.java
+++ src/main/java/cn/nukkit/network/protocol/EntityFallPacket.java
@@ -4,26 +4,30 @@ import lombok.ToString;
 
 @ToString
 public class EntityFallPacket extends DataPacket {
+
     public static final byte NETWORK_ID = ProtocolInfo.ENTITY_FALL_PACKET;
 
-    public long eid;
+    public long entityRuntimeId;
     public float fallDistance;
     public boolean unknown;
 
     @Override
+    public byte pid() {
+        return NETWORK_ID;
+    }
+
+    @Override
     public void decode() {
-        this.eid = this.getEntityRuntimeId();
+        this.entityRuntimeId = this.getEntityRuntimeId();
         this.fallDistance = this.getLFloat();
         this.unknown = this.getBoolean();
     }
 
     @Override
     public void encode() {
-
-    }
-
-    @Override
-    public byte pid() {
-        return NETWORK_ID;
+        this.reset();
+        this.putEntityRuntimeId(this.entityRuntimeId);
+        this.putLFloat(this.fallDistance);
+        this.putBoolean(this.unknown);
     }
 }
