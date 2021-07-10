1f6c92e484f52b85e0649431d94034efa50a442e
IWareQ <dimonpo96@mail.ru>
2021-07-10 22:23:16 +0500

    Update PlayerInputPacket and LevelChunkPacket

--- src/main/java/cn/nukkit/network/Network.java
+++ src/main/java/cn/nukkit/network/Network.java
@@ -391,8 +391,8 @@ public class Network {
         this.registerPacket(ProtocolInfo.ADVENTURE_SETTINGS_PACKET, AdventureSettingsPacket.class);
         this.registerPacket(ProtocolInfo.BLOCK_ENTITY_DATA_PACKET, BlockEntityDataPacket.class);
         this.registerPacket(ProtocolInfo.PLAYER_INPUT_PACKET, PlayerInputPacket.class);
-        /*this.registerPacket(ProtocolInfo.LEVEL_CHUNK_PACKET, LevelChunkPacket.class);
-        this.registerPacket(ProtocolInfo.SET_COMMANDS_ENABLED_PACKET, SetCommandsEnabledPacket.class);
+        this.registerPacket(ProtocolInfo.LEVEL_CHUNK_PACKET, LevelChunkPacket.class);
+        /*this.registerPacket(ProtocolInfo.SET_COMMANDS_ENABLED_PACKET, SetCommandsEnabledPacket.class);
         this.registerPacket(ProtocolInfo.SET_DIFFICULTY_PACKET, SetDifficultyPacket.class);
         this.registerPacket(ProtocolInfo.CHANGE_DIMENSION_PACKET, ChangeDimensionPacket.class);
         this.registerPacket(ProtocolInfo.SET_PLAYER_GAME_TYPE_PACKET, SetPlayerGameTypePacket.class);
--- src/main/java/cn/nukkit/network/protocol/LevelChunkPacket.java
+++ src/main/java/cn/nukkit/network/protocol/LevelChunkPacket.java
@@ -8,23 +8,35 @@ import lombok.ToString;
  */
 @ToString(exclude = "data")
 public class LevelChunkPacket extends DataPacket {
+
     public static final byte NETWORK_ID = ProtocolInfo.FULL_CHUNK_DATA_PACKET;
+
+    public int chunkX;
+    public int chunkZ;
+    public int subChunkCount;
+    public boolean cacheEnabled;
+    public long[] blobIds = new long[0];
+    public byte[] data;
 
     @Override
     public byte pid() {
         return NETWORK_ID;
     }
 
-    public int chunkX;
-    public int chunkZ;
-    public int subChunkCount;
-    public boolean cacheEnabled;
-    public long[] blobIds;
-    public byte[] data;
-
     @Override
     public void decode() {
-
+        this.chunkX = this.getVarInt();
+        this.chunkZ = this.getVarInt();
+        this.subChunkCount = (int) this.getUnsignedVarInt();
+        this.cacheEnabled = this.getBoolean();
+        if (this.cacheEnabled) {
+            int count = (int) this.getUnsignedVarInt();
+            this.blobIds = new long[count];
+            for (int i = 0; i < count; i++) {
+                this.blobIds[i] = this.getLLong();
+            }
+        }
+        this.data = this.getByteArray();
     }
 
     @Override
@@ -33,11 +45,10 @@ public class LevelChunkPacket extends DataPacket {
         this.putVarInt(this.chunkX);
         this.putVarInt(this.chunkZ);
         this.putUnsignedVarInt(this.subChunkCount);
-        this.putBoolean(cacheEnabled);
+        this.putBoolean(this.cacheEnabled);
         if (this.cacheEnabled) {
-            this.putUnsignedVarInt(blobIds.length);
-
-            for (long blobId : blobIds) {
+            this.putUnsignedVarInt(this.blobIds.length);
+            for (long blobId : this.blobIds) {
                 this.putLLong(blobId);
             }
         }
