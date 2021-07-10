3fc8153d4f96ce230502b0b7dfaf0f495896a73c
IWareQ <dimonpo96@mail.ru>
2021-07-10 22:16:54 +0500

    Update PlayerInputPacket

--- src/main/java/cn/nukkit/network/Network.java
+++ src/main/java/cn/nukkit/network/Network.java
@@ -390,8 +390,8 @@ public class Network {
         this.registerPacket(ProtocolInfo.GUI_DATA_PICK_ITEM_PACKET, GUIDataPickItemPacket.class);
         this.registerPacket(ProtocolInfo.ADVENTURE_SETTINGS_PACKET, AdventureSettingsPacket.class);
         this.registerPacket(ProtocolInfo.BLOCK_ENTITY_DATA_PACKET, BlockEntityDataPacket.class);
-        /*this.registerPacket(ProtocolInfo.PLAYER_INPUT_PACKET, PlayerInputPacket.class);
-        this.registerPacket(ProtocolInfo.LEVEL_CHUNK_PACKET, LevelChunkPacket.class);
+        this.registerPacket(ProtocolInfo.PLAYER_INPUT_PACKET, PlayerInputPacket.class);
+        /*this.registerPacket(ProtocolInfo.LEVEL_CHUNK_PACKET, LevelChunkPacket.class);
         this.registerPacket(ProtocolInfo.SET_COMMANDS_ENABLED_PACKET, SetCommandsEnabledPacket.class);
         this.registerPacket(ProtocolInfo.SET_DIFFICULTY_PACKET, SetDifficultyPacket.class);
         this.registerPacket(ProtocolInfo.CHANGE_DIMENSION_PACKET, ChangeDimensionPacket.class);
--- src/main/java/cn/nukkit/network/protocol/PlayerInputPacket.java
+++ src/main/java/cn/nukkit/network/protocol/PlayerInputPacket.java
@@ -8,30 +8,32 @@ import lombok.ToString;
 @ToString
 public class PlayerInputPacket extends DataPacket {
 
-    public static final byte NETWORK_ID = ProtocolInfo.PLAYER_INPUT_PACKET;
+public static final byte NETWORK_ID = ProtocolInfo.PLAYER_INPUT_PACKET;
 
-    public float motionX;
-    public float motionY;
+public float motionX;
+public float motionY;
+public boolean jumping;
+public boolean sneaking;
 
-    public boolean jumping;
-    public boolean sneaking;
+@Override
+public byte pid() {
+return NETWORK_ID;
+}
 
-    @Override
-    public void decode() {
-        this.motionX = this.getLFloat();
-        this.motionY = this.getLFloat();
-        this.jumping = this.getBoolean();
-        this.sneaking = this.getBoolean();
-    }
+@Override
+public void decode() {
+this.motionX = this.getLFloat();
+this.motionY = this.getLFloat();
+this.jumping = this.getBoolean();
+this.sneaking = this.getBoolean();
+}
 
-    @Override
-    public void encode() {
-
-    }
-
-    @Override
-    public byte pid() {
-        return NETWORK_ID;
-    }
-
+@Override
+public void encode() {
+this.reset();
+this.putLFloat(this.motionX);
+this.putLFloat(this.motionY);
+this.putBoolean(this.jumping);
+this.putBoolean(this.sneaking);
+}
 }
