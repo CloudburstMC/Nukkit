f9144543fad03cd8dd7ab248c61512fe0ad28795
IWareQ <dimonpo96@mail.ru>
2021-07-11 21:58:33 +0500

    Update EventPacket

--- src/main/java/cn/nukkit/network/protocol/EventPacket.java
+++ src/main/java/cn/nukkit/network/protocol/EventPacket.java
@@ -5,25 +5,58 @@ import lombok.ToString;
 @ToString
 public class EventPacket extends DataPacket {
 
-    public long eid;
-    public int unknown1;
-    public byte unknown2;
+    public static final byte NETWORK_ID = ProtocolInfo.EVENT_PACKET;
+
+    public long entityRuntimeId;
+    public int eventData;
+    public Event event;
 
     @Override
     public byte pid() {
-        return ProtocolInfo.EVENT_PACKET;
+        return NETWORK_ID;
     }
 
     @Override
     public void decode() {
-
+        this.entityRuntimeId = this.getEntityRuntimeId();
+        this.eventData = this.getVarInt();
+        this.event = Event.values()[this.getByte()];
     }
 
     @Override
     public void encode() {
         this.reset();
-        this.putVarLong(this.eid);
-        this.putVarInt(this.unknown1);
-        this.putByte(this.unknown2);
+        this.putEntityRuntimeId(this.entityRuntimeId);
+        this.putVarInt(this.eventData);
+        this.putByte(this.event.ordinal());
+    }
+
+    public static enum Event {
+
+        ACHIEVEMENT_AWARDED,
+        ENTITY_INTERACT,
+        PORTAL_BUILT,
+        PORTAL_USED,
+        MOB_KILLED,
+        CAULDRON_USED,
+        PLAYER_DEATH,
+        BOSS_KILLED,
+        AGENT_COMMAND,
+        AGENT_CREATED,
+        PATTERN_REMOVED,
+        COMMANED_EXECUTED,
+        FISH_BUCKETED,
+        MOB_BORN,
+        PET_DIED,
+        CAULDRON_BLOCK_USED,
+        COMPOSTER_BLOCK_USED,
+        BELL_BLOCK_USED,
+        ACTOR_DEFINITION,
+        RAID_UPDATE,
+        PLAYER_MOVEMENT_ANOMALY,
+        PLAYER_MOVEMENT_CORRECTED,
+        HONEY_HARVESTED,
+        TARGET_BLOCK_HIT,
+        PIGLIN_BARTER
     }
 }
