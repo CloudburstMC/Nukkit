5f2585347176370073658f1c0414ad42a3b2e957
IWareQ <dimonpo96@mail.ru>
2021-07-11 22:05:17 +0500

    Update SpawnExperienceOrbPacket

--- src/main/java/cn/nukkit/network/Network.java
+++ src/main/java/cn/nukkit/network/Network.java
@@ -399,8 +399,8 @@ public class Network {
         this.registerPacket(ProtocolInfo.PLAYER_LIST_PACKET, PlayerListPacket.class);
         this.registerPacket(ProtocolInfo.SIMPLE_EVENT_PACKET, SimpleEventPacket.class);
         this.registerPacket(ProtocolInfo.EVENT_PACKET, EventPacket.class);
-        /*this.registerPacket(ProtocolInfo.SPAWN_EXPERIENCE_ORB_PACKET, SpawnExperienceOrbPacket.class);
-        this.registerPacket(ProtocolInfo.CLIENTBOUND_MAP_ITEM_DATA_PACKET, ClientboundMapItemDataPacket.class);
+        this.registerPacket(ProtocolInfo.SPAWN_EXPERIENCE_ORB_PACKET, SpawnExperienceOrbPacket.class);
+        /*this.registerPacket(ProtocolInfo.CLIENTBOUND_MAP_ITEM_DATA_PACKET, ClientboundMapItemDataPacket.class);
         this.registerPacket(ProtocolInfo.MAP_INFO_REQUEST_PACKET, MapInfoRequestPacket.class);
         this.registerPacket(ProtocolInfo.REQUEST_CHUNK_RADIUS_PACKET, RequestChunkRadiusPacket.class);
         this.registerPacket(ProtocolInfo.CHUNK_RADIUS_UPDATED_PACKET, ChunkRadiusUpdatedPacket.class);
--- src/main/java/cn/nukkit/network/protocol/SpawnExperienceOrbPacket.java
+++ src/main/java/cn/nukkit/network/protocol/SpawnExperienceOrbPacket.java
@@ -1,5 +1,6 @@
 package cn.nukkit.network.protocol;
 
+import cn.nukkit.math.Vector3f;
 import lombok.ToString;
 
 @ToString
@@ -7,25 +8,24 @@ public class SpawnExperienceOrbPacket extends DataPacket {
 
     public static final byte NETWORK_ID = ProtocolInfo.SPAWN_EXPERIENCE_ORB_PACKET;
 
-    public float x;
-    public float y;
-    public float z;
+    public Vector3f position;
     public int amount;
 
     @Override
-    public void decode() {
+    public byte pid() {
+        return NETWORK_ID;
+    }
 
+    @Override
+    public void decode() {
+        this.position = this.getVector3f();
+        this.amount = this.getVarInt();
     }
 
     @Override
     public void encode() {
         this.reset();
-        this.putVector3f(this.x, this.y, this.z);
-        this.putUnsignedVarInt(this.amount);
-    }
-
-    @Override
-    public byte pid() {
-        return NETWORK_ID;
+        this.putVector3f(this.position);
+        this.putVarInt(this.amount);
     }
 }
