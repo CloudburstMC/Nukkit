21fc67b202eba686f1e0a49a728ed8f2c5b7d796
IWareQ <dimonpo96@mail.ru>
2021-07-19 11:25:27 +0500

    Add SubClientLoginPacket

--- src/main/java/cn/nukkit/network/protocol/SubClientLoginPacket.java
+++ src/main/java/cn/nukkit/network/protocol/SubClientLoginPacket.java
@@ -5,18 +5,23 @@ import lombok.ToString;
 @ToString
 public class SubClientLoginPacket extends DataPacket {
 
+    public static final byte NETWORK_ID = ProtocolInfo.SUB_CLIENT_LOGIN_PACKET;
+
+    public String connectionRequestData;
+
     @Override
     public byte pid() {
-        return ProtocolInfo.SUB_CLIENT_LOGIN_PACKET;
+        return NETWORK_ID;
     }
 
     @Override
     public void decode() {
-
+        this.connectionRequestData = this.getString();
     }
 
     @Override
     public void encode() {
-        //TODO
+        this.reset();
+        this.putString(this.connectionRequestData);
     }
 }
