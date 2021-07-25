bd6b5a2e4dbc3b2d3f828734c2d7390efbe836f8
zzz <1173572640@qq.com>
2021-07-26 02:28:18 +0800

    Update MoveEntityDeltaPacket.java

--- src/main/java/cn/nukkit/network/protocol/MoveEntityDeltaPacket.java
+++ src/main/java/cn/nukkit/network/protocol/MoveEntityDeltaPacket.java
@@ -6,20 +6,30 @@ import lombok.ToString;
 public class MoveEntityDeltaPacket extends DataPacket {
     public static final byte NETWORK_ID = ProtocolInfo.MOVE_ENTITY_DELTA_PACKET;
 
-    public static final int FLAG_HAS_X = 0b1;
-    public static final int FLAG_HAS_Y = 0b10;
-    public static final int FLAG_HAS_Z = 0b100;
-    public static final int FLAG_HAS_YAW = 0b1000;
-    public static final int FLAG_HAS_HEAD_YAW = 0b10000;
-    public static final int FLAG_HAS_PITCH = 0b100000;
+    public static final int FLAG_HAS_X = 0B1;
+    public static final int FLAG_HAS_Y = 0B10;
+    public static final int FLAG_HAS_Z = 0B100;
+    public static final int FLAG_HAS_PITCH = 0B1000;
+    public static final int FLAG_HAS_YAW = 0B10000;
+    public static final int FLAG_HAS_HEAD_YAW = 0B100000;
+    public static final int FLAG_ON_GROUND = 0B1000000;
+    public static final int FLAG_TELEPORTING = 0B10000000;
+    public static final int FLAG_FORCE_MOVE_LOCAL_ENTITY = 0B100000000;
 
+    public long runtimeEntityId;
     public int flags = 0;
     public float x = 0;
     public float y = 0;
     public float z = 0;
-    public double yawDelta = 0;
-    public double headYawDelta = 0;
-    public double pitchDelta = 0;
+    public float pitch = 0;
+    public float yaw = 0;
+    public float headYaw = 0;
+    @Deprecated
+    private int deltaX;
+    @Deprecated
+    private int deltaY;
+    @Deprecated
+    private int deltaZ;
 
     @Override
     public byte pid() {
@@ -28,24 +38,63 @@ public class MoveEntityDeltaPacket extends DataPacket {
 
     @Override
     public void decode() {
+        this.runtimeEntityId = this.getEntityRuntimeId();
         this.flags = this.getByte();
-        this.x = getCoordinate(FLAG_HAS_X);
-        this.y = getCoordinate(FLAG_HAS_Y);
-        this.z = getCoordinate(FLAG_HAS_Z);
-        this.yawDelta = getRotation(FLAG_HAS_YAW);
-        this.headYawDelta = getRotation(FLAG_HAS_HEAD_YAW);
-        this.pitchDelta = getRotation(FLAG_HAS_PITCH);
+        if ((flags & FLAG_HAS_X) != 0) {
+            this.x = this.getLFloat();
+        }
+        if ((flags & FLAG_HAS_Y) != 0) {
+            this.y = this.getLFloat();
+        }
+        if ((flags & FLAG_HAS_Z) != 0) {
+            this.z = this.getLFloat();
+        }
+        if ((flags & FLAG_HAS_PITCH) != 0) {
+            this.pitch = this.getByte() * (360F / 256F);
+        }
+        if ((flags & FLAG_HAS_YAW) != 0) {
+            this.yaw = this.getByte() * (360F / 256F);
+        }
+        if ((flags & FLAG_HAS_HEAD_YAW) != 0) {
+            this.headYaw = this.getByte() * (360F / 256F);
+        }
     }
 
     @Override
     public void encode() {
+        this.reset();
+        this.putEntityRuntimeId(runtimeEntityId);
+        //int flagsIndex = buffer.writerIndex();
+        this.putLShort(0);
+        int flags = 0;
+        if ((this.flags & FLAG_HAS_X) != 0) {
+            flags |= FLAG_HAS_X;
+            this.putLFloat(this.x);
+        }
+        if ((this.flags & FLAG_HAS_Y) != 0) {
+            flags |= FLAG_HAS_Y;
+            this.putLFloat(this.y);
+        }
+        if ((this.flags & FLAG_HAS_Z) != 0) {
+            flags |= FLAG_HAS_Z;
+            this.putLFloat(this.z);
+        }
+        if ((this.flags & FLAG_HAS_PITCH) != 0) {
+            flags |= FLAG_HAS_PITCH;
+            this.putByte((byte) (pitch / (360F / 256F)));
+        }
+        if ((this.flags & FLAG_HAS_YAW) != 0) {
+            flags |= FLAG_HAS_YAW;
+            this.putByte((byte) (yaw / (360F / 256F)));
+        }
+        if ((this.flags & FLAG_HAS_HEAD_YAW) != 0) {
+            flags |= FLAG_HAS_HEAD_YAW;
+            this.putByte((byte) (headYaw / (360F / 256F)));
+        }
+        //int currentIndex = buffer.writerIndex();
+        //buffer.writerIndex(flagsIndex);
         this.putByte((byte) flags);
-        putCoordinate(FLAG_HAS_X, this.x);
-        putCoordinate(FLAG_HAS_Y, this.y);
-        putCoordinate(FLAG_HAS_Z, this.z);
-        putRotation(FLAG_HAS_YAW, this.yawDelta);
-        putRotation(FLAG_HAS_HEAD_YAW, this.headYawDelta);
-        putRotation(FLAG_HAS_PITCH, this.pitchDelta);
+        //buffer.writerIndex(currentIndex);
     }
 
     private float getCoordinate(int flag) {
@@ -57,9 +106,9 @@ public class MoveEntityDeltaPacket extends DataPacket {
 
     private double getRotation(int flag) {
         if ((flags & flag) != 0) {
-            return this.getByte() * (360d / 256d);
+            return this.getByte() * (360F / 256F);
         }
-        return 0d;
+        return 0;
     }
 
     private void putCoordinate(int flag, float value) {
@@ -70,7 +119,11 @@ public class MoveEntityDeltaPacket extends DataPacket {
 
     private void putRotation(int flag, double value) {
         if ((flags & flag) != 0) {
-            this.putByte((byte) (value / (360d / 256d)));
+            this.putByte((byte) (value / (360F / 256F)));
         }
+    }
+
+    public boolean hasFlag(int flag) {
+        return (flags & flag) != 0;
     }
 }
