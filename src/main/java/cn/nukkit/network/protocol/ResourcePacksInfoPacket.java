d6f5d27e9825003336b982f764096b1c4ac587dc
DasEtwas <18222134+DasEtwas@users.noreply.github.com>
2021-08-04 18:01:48 +0200

    Fix behaviour pack list encoding

--- src/main/java/cn/nukkit/network/protocol/ResourcePacksInfoPacket.java
+++ src/main/java/cn/nukkit/network/protocol/ResourcePacksInfoPacket.java
@@ -25,11 +25,11 @@ public class ResourcePacksInfoPacket extends DataPacket {
         this.putBoolean(this.mustAccept);
         this.putBoolean(this.scripting);
         this.putBoolean(this.forceServerPacks);
-        this.encodePacks(this.behaviourPackEntries);
-        this.encodePacks(this.resourcePackEntries);
+        this.encodeBehaviourPacks(this.behaviourPackEntries);
+        this.encodeResourcePacks(this.resourcePackEntries);
     }
 
-    private void encodePacks(ResourcePack[] packs) {
+    private void encodeBehaviourPacks(ResourcePack[] packs) {
         this.putLShort(packs.length);
         for (ResourcePack entry : packs) {
             this.putString(entry.getPackId().toString());
@@ -39,6 +39,19 @@ public class ResourcePacksInfoPacket extends DataPacket {
             this.putString(""); // sub-pack name
             this.putString(""); // content identity
             this.putBoolean(entry.requiresScripting()); // scripting
+        }
+    }
+
+    private void encodeResourcePacks(ResourcePack[] packs) {
+        this.putLShort(packs.length);
+        for (ResourcePack entry : packs) {
+            this.putString(entry.getPackId().toString());
+            this.putString(entry.getPackVersion());
+            this.putLLong(entry.getPackSize());
+            this.putString(""); // encryption key
+            this.putString(""); // sub-pack name
+            this.putString(""); // content identity
+            this.putBoolean(false); // scripting
             this.putBoolean(false); // raytracing capable
         }
     }
