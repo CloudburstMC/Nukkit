46f267a6bd0a7382327091fb46cf465443d67b42
Woder <17339354+wode490390@users.noreply.github.com>
2021-12-03 00:10:46 +0800

    Fix EntityLink desync issues

--- src/main/java/cn/nukkit/Player.java
+++ src/main/java/cn/nukkit/Player.java
@@ -2642,7 +2642,7 @@ public class Player extends EntityHuman implements CommandSender, InventoryHolde
                                 break;
                             }
 
-                            ((EntityRideable) riding).mountEntity(this);
+                            ((EntityRideable) riding).dismountEntity(this);
                             break;
                         case InteractPacket.ACTION_OPEN_INVENTORY:
                             if (targetEntity.getId() != this.getId()) break;
