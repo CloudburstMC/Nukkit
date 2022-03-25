357709ab010ef5a3bbfa3075674881d2943fc6d1
Kevims <kcodeyt@gmail.com>
2022-03-25 16:11:12 +0100

    Replace hacky gson code and implement FormResponseHandler#withoutPlayer

--- src/main/java/cn/nukkit/form/handler/FormResponseHandler.java
+++ src/main/java/cn/nukkit/form/handler/FormResponseHandler.java
@@ -2,7 +2,13 @@ package cn.nukkit.form.handler;
 
 import cn.nukkit.Player;
 
+import java.util.function.IntConsumer;
+
 public interface FormResponseHandler {
+
+    static FormResponseHandler withoutPlayer(IntConsumer formIDConsumer) {
+        return (player, formID) -> formIDConsumer.accept(formID);
+    }
 
     void handle(Player player, int formID);
 
--- src/main/java/cn/nukkit/form/window/FormWindow.java
+++ src/main/java/cn/nukkit/form/window/FormWindow.java
@@ -2,31 +2,17 @@ package cn.nukkit.form.window;
 
 import cn.nukkit.form.handler.FormResponseHandler;
 import cn.nukkit.form.response.FormResponse;
-import com.google.gson.ExclusionStrategy;
-import com.google.gson.FieldAttributes;
 import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
 import it.unimi.dsi.fastutil.objects.ObjectArrayList;
 
-import java.lang.reflect.Modifier;
 import java.util.List;
 
 public abstract class FormWindow {
 
-    private static final Gson GSON = new GsonBuilder().addSerializationExclusionStrategy(new ExclusionStrategy() {
-        @Override
-        public boolean shouldSkipField(FieldAttributes f) {
-            return f.getName().equals("handlers");
-        }
-
-        @Override
-        public boolean shouldSkipClass(Class<?> clazz) {
-            return false;
-        }
-    }).create();
+    private static final Gson GSON = new Gson();
 
     protected boolean closed = false;
-    protected final List<FormResponseHandler> handlers = new ObjectArrayList<>();
+    protected final transient List<FormResponseHandler> handlers = new ObjectArrayList<>();
 
     public String getJSONData() {
         return FormWindow.GSON.toJson(this);
