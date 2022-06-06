0f2d0d4a16f5500bd6cc8b22262f0b505ca8d1e2
PetteriM1 <26197131+PetteriM1@users.noreply.github.com>
2022-06-06 12:06:11 +0300

    Improve redstone wire placement (#1994)

--- src/main/java/cn/nukkit/block/BlockRedstoneWire.java
+++ src/main/java/cn/nukkit/block/BlockRedstoneWire.java
@@ -41,7 +41,7 @@ public class BlockRedstoneWire extends BlockFlowable {
 
     @Override
     public boolean place(Item item, Block block, Block target, BlockFace face, double fx, double fy, double fz, Player player) {
-        if (face != BlockFace.UP || !canBePlacedOn(target)) {
+        if (!canBePlacedOn(block.down())) {
             return false;
         }
 
@@ -203,7 +203,7 @@ public class BlockRedstoneWire extends BlockFlowable {
             return 0;
         }
 
-        if (type == Level.BLOCK_UPDATE_NORMAL && !this.canBePlacedOn(this.getLocation().down())) {
+        if (type == Level.BLOCK_UPDATE_NORMAL && !this.canBePlacedOn(this.down())) {
             this.getLevel().useBreakOn(this);
             return Level.BLOCK_UPDATE_NORMAL;
         }
@@ -214,9 +214,11 @@ public class BlockRedstoneWire extends BlockFlowable {
     }
 
     public boolean canBePlacedOn(Vector3 v) {
-        Block b = this.level.getBlock(v);
+        return this.canBePlacedOn(this.level.getBlock(v));
+    }
 
-        return (b.isSolid() && !b.isTransparent() && b.getId() != Block.GLOWSTONE) || b.getId() == HOPPER_BLOCK;
+    private boolean canBePlacedOn(Block b) {
+        return (b.isSolid() && !b.isTransparent() && b.getId() != GLOWSTONE) || b.getId() == HOPPER_BLOCK;
     }
 
     public int getStrongPower(BlockFace side) {
