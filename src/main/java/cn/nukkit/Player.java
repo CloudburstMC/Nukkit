648a4292969635c149c25b5be85abb44d89398bd
PetteriM1 <26197131+PetteriM1@users.noreply.github.com>
2022-10-26 11:27:10 +0300

    Make sure updateFoodExpLevel calculations won't affect nextChunkOrderRun (#2065)

--- src/main/java/cn/nukkit/Player.java
+++ src/main/java/cn/nukkit/Player.java
@@ -1552,12 +1552,13 @@ public class Player extends EntityHuman implements CommandSender, InventoryHolde
         if ((this.isFoodEnabled() || this.getServer().getDifficulty() == 0) && distance >= 0.05) {
             double jump = 0;
             double swimming = this.isInsideOfWater() ? 0.015 * distance : 0;
-            if (swimming != 0) distance = 0;
+            double distance2 = distance;
+            if (swimming != 0) distance2 = 0;
             if (this.isSprinting()) {
                 if (this.inAirTicks == 3 && swimming == 0) {
                     jump = 0.2;
                 }
-                this.getFoodData().updateFoodExpLevel(0.1 * distance + jump + swimming);
+                this.getFoodData().updateFoodExpLevel(0.1 * distance2 + jump + swimming);
             } else {
                 if (this.inAirTicks == 3 && swimming == 0) {
                     jump = 0.05;
