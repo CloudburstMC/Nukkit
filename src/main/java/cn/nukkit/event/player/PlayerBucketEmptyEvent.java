0d4e1c2e850467206b1b72dca3616d2261610b24
PetteriM1 <26197131+PetteriM1@users.noreply.github.com>
2023-01-04 17:44:50 +0000

    Bucket of Axolotl + Bucket of Tadpoles + Option to disable fish spawning (#2057)

--- src/main/java/cn/nukkit/event/player/PlayerBucketEmptyEvent.java
+++ src/main/java/cn/nukkit/event/player/PlayerBucketEmptyEvent.java
@@ -7,14 +7,39 @@ import cn.nukkit.item.Item;
 import cn.nukkit.math.BlockFace;
 
 public class PlayerBucketEmptyEvent extends PlayerBucketEvent {
+
     private static final HandlerList handlers = new HandlerList();
+
+    private boolean mobSpawningAllowed;
 
     public static HandlerList getHandlers() {
         return handlers;
     }
 
     public PlayerBucketEmptyEvent(Player who, Block blockClicked, BlockFace blockFace, Item bucket, Item itemInHand) {
-        super(who, blockClicked, blockFace, bucket, itemInHand);
+        this(who, blockClicked, blockFace, bucket, itemInHand, true);
     }
 
+    public PlayerBucketEmptyEvent(Player who, Block blockClicked, BlockFace blockFace, Item bucket, Item itemInHand, boolean mobSpawningAllowed) {
+        super(who, blockClicked, blockFace, bucket, itemInHand);
+        this.mobSpawningAllowed = mobSpawningAllowed;
+    }
+
+    /**
+     * Whether a fish can be spawned when a fish bucket is emptied.
+     *
+     * @return can spawn a fish
+     */
+    public boolean isMobSpawningAllowed() {
+        return this.mobSpawningAllowed;
+    }
+
+    /**
+     * Set whether a fish can be spawned when a fish bucket is emptied.
+     *
+     * @param mobSpawningAllowed can spawn a fish
+     */
+    public void setMobSpawningAllowed(boolean mobSpawningAllowed) {
+        this.mobSpawningAllowed = mobSpawningAllowed;
+    }
 }
--- src/main/java/cn/nukkit/item/ItemBucket.java
+++ src/main/java/cn/nukkit/item/ItemBucket.java
@@ -48,6 +48,10 @@ public class ItemBucket extends Item {
                 return "Water Bucket";
             case 10:
                 return "Lava Bucket";
+            case 12:
+                return "Bucket of Axolotl";
+            case 13:
+                return "Bucket of Tadpoles";
             default:
                 return "Bucket";
         }
@@ -61,6 +65,8 @@ public class ItemBucket extends Item {
             case 5:
             case 8:
             case 9:
+            case 12:
+            case 13:
                 return 8;
             case 10:
             case 11:
@@ -133,7 +139,7 @@ public class ItemBucket extends Item {
             }
         } else if (targetBlock instanceof BlockLiquid) {
             Item result = Item.get(BUCKET, 0, 1);
-            PlayerBucketEmptyEvent ev = new PlayerBucketEmptyEvent(player, block, face, this, result);
+            PlayerBucketEmptyEvent ev = new PlayerBucketEmptyEvent(player, block, face, this, result, true);
             if (!block.canBeFlowedInto()) {
                 ev.setCancelled(true);
             }
@@ -169,23 +175,33 @@ public class ItemBucket extends Item {
                     level.addLevelSoundEvent(block, LevelSoundEventPacket.SOUND_BUCKET_EMPTY_WATER);
                 }
 
-                switch (this.getDamage()) {
-                    case 2:
-                        Entity e2 = Entity.createEntity("Cod", block);
-                        if (e2 != null) e2.spawnToAll();
-                        break;
-                    case 3:
-                        Entity e3 = Entity.createEntity("Salmon", block);
-                        if (e3 != null) e3.spawnToAll();
-                        break;
-                    case 4:
-                        Entity e4 = Entity.createEntity("TropicalFish", block);
-                        if (e4 != null) e4.spawnToAll();
-                        break;
-                    case 5:
-                        Entity e5 = Entity.createEntity("Pufferfish", block);
-                        if (e5 != null) e5.spawnToAll();
-                        break;
+                if (ev.isMobSpawningAllowed()) {
+                    switch (this.getDamage()) {
+                        case 2:
+                            Entity e2 = Entity.createEntity("Cod", block);
+                            if (e2 != null) e2.spawnToAll();
+                            break;
+                        case 3:
+                            Entity e3 = Entity.createEntity("Salmon", block);
+                            if (e3 != null) e3.spawnToAll();
+                            break;
+                        case 4:
+                            Entity e4 = Entity.createEntity("TropicalFish", block);
+                            if (e4 != null) e4.spawnToAll();
+                            break;
+                        case 5:
+                            Entity e5 = Entity.createEntity("Pufferfish", block);
+                            if (e5 != null) e5.spawnToAll();
+                            break;
+                        case 12:
+                            Entity e12 = Entity.createEntity("Axolotl", block);
+                            if (e12 != null) e12.spawnToAll();
+                            break;
+                        case 13:
+                            Entity e13 = Entity.createEntity("Tadpole", block);
+                            if (e13 != null) e13.spawnToAll();
+                            break;
+                    }
                 }
 
                 return true;
@@ -225,7 +241,7 @@ public class ItemBucket extends Item {
         }
 
         if (!player.isCreative()) {
-            player.getInventory().setItemInHand(new ItemBucket());
+            player.getInventory().setItemInHand(Item.get(Item.BUCKET));
         }
 
         player.removeAllEffects();
