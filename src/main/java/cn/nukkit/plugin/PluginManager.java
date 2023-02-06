4addec74df1078b5f185800aafe5f4af81556bd1
RobaCZ <robachannel@gmail.com>
2023-02-06 17:59:21 +0100

    Fix typo in deprecated verbose

--- src/main/java/cn/nukkit/plugin/PluginManager.java
+++ src/main/java/cn/nukkit/plugin/PluginManager.java
@@ -588,7 +588,7 @@ public class PluginManager {
             for (Class<?> clazz = eventClass; Event.class.isAssignableFrom(clazz); clazz = clazz.getSuperclass()) {
                 // This loop checks for extending deprecated events
                 if (clazz.getAnnotation(Deprecated.class) != null) {
-                    if (Boolean.parseBoolean(String.valueOf(this.server.getConfig("settings.deprecated-verbpse", true)))) {
+                    if (Boolean.parseBoolean(String.valueOf(this.server.getConfig("settings.deprecated-verbose", true)))) {
                         this.server.getLogger().warning(this.server.getLanguage().translateString("nukkit.plugin.deprecatedEvent", plugin.getName(), clazz.getName(), listener.getClass().getName() + "." + method.getName() + "()"));
                     }
                     break;
