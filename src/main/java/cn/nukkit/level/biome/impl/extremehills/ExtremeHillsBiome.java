57a1468f81607d9c11d7a2ae9ca46299bafff0db
PetteriM1 <26197131+PetteriM1@users.noreply.github.com>
2023-02-20 17:06:31 +0200

    Add monster egg stone & emerald to extreme hills generator (#2084)

--- src/main/java/cn/nukkit/level/biome/impl/extremehills/ExtremeHillsBiome.java
+++ src/main/java/cn/nukkit/level/biome/impl/extremehills/ExtremeHillsBiome.java
@@ -1,7 +1,11 @@
 package cn.nukkit.level.biome.impl.extremehills;
 
+import cn.nukkit.block.Block;
+import cn.nukkit.block.BlockID;
 import cn.nukkit.block.BlockSapling;
 import cn.nukkit.level.biome.type.GrassyBiome;
+import cn.nukkit.level.generator.object.ore.OreType;
+import cn.nukkit.level.generator.populator.impl.PopulatorOre;
 import cn.nukkit.level.generator.populator.impl.PopulatorTree;
 
 /**
@@ -26,6 +30,11 @@ public class ExtremeHillsBiome extends GrassyBiome {
             trees.setRandomAmount(2);
             this.addPopulator(trees);
         }
+
+        this.addPopulator(new PopulatorOre(STONE, new OreType[]{
+                new OreType(Block.get(BlockID.EMERALD_ORE), 11, 1, 0, 32),
+                new OreType(Block.get(BlockID.MONSTER_EGG), 7, 9, 0, 63)
+        }));
 
         this.setBaseHeight(1f);
         this.setHeightVariation(0.5f);
