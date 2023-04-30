a3becadfd4b05ac9fa51f04e40fb9f4747f9de20
PetteriM1 <26197131+PetteriM1@users.noreply.github.com>
2023-04-30 20:37:22 +0300

    1.19.80 (#2110)
    * 1.19.80
    
    * Update item_mappings.json
    
    * Update RuntimeItemMapping.java
    
    * Fix signs

---
+++ src/main/java/cn/nukkit/network/protocol/OpenSignPacket.java
@@ -0,0 +1,26 @@
+package cn.nukkit.network.protocol;
+
+import cn.nukkit.math.BlockVector3;
+
+public class OpenSignPacket extends DataPacket {
+
+    public BlockVector3 position;
+    public boolean frontSide;
+
+    @Override
+    public byte pid() {
+        return ProtocolInfo.__INTERNAL__OPEN_SIGN_PACKET;
+    }
+
+    @Override
+    public void decode() {
+
+    }
+
+    @Override
+    public void encode() {
+        this.reset();
+        this.putBlockVector3(this.position);
+        this.putBoolean(this.frontSide);
+    }
+}
