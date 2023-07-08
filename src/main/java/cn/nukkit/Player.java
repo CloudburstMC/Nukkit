a466e88229b41e95a244260fcbac5429fb15fadd
PetteriM1 <26197131+PetteriM1@users.noreply.github.com>
2023-07-08 21:30:49 +0300

    Ignore isResponse in removeWindow for now (#2124)

--- src/main/java/cn/nukkit/Player.java
+++ src/main/java/cn/nukkit/Player.java
@@ -4966,8 +4966,11 @@ public class Player extends EntityHuman implements CommandSender, InventoryHolde
 
     protected void removeWindow(Inventory inventory, boolean isResponse) {
         inventory.close(this);
-        if (isResponse && !this.permanentWindows.contains(this.getWindowId(inventory)))
+        // TODO: This needs a proper fix
+        // Requiring isResponse here causes issues with inventory events and an item duplication glitch
+        if (/*isResponse &&*/ !this.permanentWindows.contains(this.getWindowId(inventory))) {
             this.windows.remove(inventory);
+        }
     }
 
     public void sendAllInventories() {
