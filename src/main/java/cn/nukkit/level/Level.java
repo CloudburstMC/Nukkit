20e20ed5ff870ba07173f132f87c851aac0d87ef
Petteri <26197131+PetteriM1@users.noreply.github.com>
2024-01-14 15:09:24 +0200

    regenerateChunk: no unnecessary save + force new chunk generation

--- src/main/java/cn/nukkit/level/Level.java
+++ src/main/java/cn/nukkit/level/Level.java
@@ -3072,14 +3072,14 @@ public class Level implements ChunkManager, Metadatable {
     }
 
     public void regenerateChunk(int x, int z) {
-        this.unloadChunk(x, z, false);
+        this.unloadChunk(x, z, false, false);
 
         this.cancelUnloadChunkRequest(x, z);
 
         BaseFullChunk chunk = provider.getEmptyChunk(x, z);
         provider.setChunk(x, z, chunk);
 
-        this.generateChunk(x, z);
+        this.generateChunk(x, z, true);
     }
 
     public void doChunkGarbageCollection() {
